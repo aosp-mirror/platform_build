3caa7cfa04804479481e5042bfe9b57d1478a9b6
Rod Whitby <rod@whitby.id.au>
2008-10-22 11:49:14 +1030

    Detect a missing javac and provide a slightly more helpful error message.
    Note the hyphens in the error message are required because the output of
    this scripts is used directly in a Makefile target.
    
    Signed-off-by: Rod Whitby <rod@whitby.id.au>

--- core/find-jdk-tools-jar.sh
+++ core/find-jdk-tools-jar.sh
@@ -2,6 +2,10 @@ if [[ "x$ANDROID_JAVA_HOME" != x && -e $ANDROID_JAVA_HOME/lib/tools.jar ]] ; the
     echo $ANDROID_JAVA_HOME/lib/tools.jar
 else
     JAVAC=$(which javac)
+    if [ -z "$JAVAC" ] ; then
+	echo "Please-install-JDK-5.0,-update-12-or-higher,-which-you-can-download-from-java.sun.com"
+	exit 1
+    fi
     while [ -L $JAVAC ] ; do
         LSLINE=$(ls -l $JAVAC)
         JAVAC=$(echo -n $LSLINE | sed -e "s/.* -> //")
