991a6b57776e710e946f55bbc62766c39ed88f52
Patrick Scott <>
2009-04-13 09:57:37 -0700

    AI 145870: Add a build property for the default alarm alert. Update the various   framework classes to deal with the new property. Also update various   documentation that mentions the default ringtones.   Use the build property as the default alert when the user has not chosen   an alert for an alarm. This is also used if the alarm alert is null when   the alarm fires.   BUG=1723684
    Automated import of CL 145870

--- target/product/core.mk
+++ target/product/core.mk
@@ -3,7 +3,8 @@ PRODUCT_NAME :=
 PRODUCT_DEVICE :=
 PRODUCT_POLICY := android.policy_phone
 PRODUCT_PROPERTY_OVERRIDES := \
-    ro.config.notification_sound=F1_New_SMS.ogg
+    ro.config.notification_sound=F1_New_SMS.ogg \
+    ro.config.alarm_alert=Alarm_Classic.ogg
 
 PRODUCT_PACKAGES := \
     framework-res \
