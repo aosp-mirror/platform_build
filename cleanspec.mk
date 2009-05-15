50ca1cf796d867d6f378d55fc98a3bb5f69630df
Ed Heyl <edheyl@google.com>
2009-05-15 15:02:03 -0700

    cherry-pick bd215ed9749f8199085b27ab4f21e687c07e9633

--- cleanspec.mk
+++ cleanspec.mk
@@ -77,6 +77,7 @@ $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/JAVA_LIBRARIES/com.an
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/PinyinIMEGoogleService_intermediates)
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/JAVA_LIBRARIES/framework_intermediates/src/telephony)
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/product/*/obj/SHARED_LIBRARIES/libwebcore_intermediates)
+$(call add-clean-step, rm -rf $(OUT_DIR)/target/product/*/obj)
 
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
