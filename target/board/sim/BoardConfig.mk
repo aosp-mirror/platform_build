ecc70d705ab8ad6461f23d8be97791227f4219ea
Dianne Hackborn <hackbod@google.com>
2009-05-21 15:45:30 -0700

    Define TARGET_CPU_ABI for finding native code in .apks

--- target/board/sim/BoardConfig.mk
+++ target/board/sim/BoardConfig.mk
@@ -17,6 +17,9 @@ TARGET_NO_BOOTLOADER := true
 # Don't bother with a kernel
 TARGET_NO_KERNEL := true
 
+# The simulator does not support native code at all
+TARGET_CPU_ABI := none
+
 #the simulator partially emulates the original HTC /dev/eac audio interface
 HAVE_HTC_AUDIO_DRIVER := true
 BOARD_USES_GENERIC_AUDIO := true
