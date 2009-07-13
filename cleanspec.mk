56c7c9d59848fdd835200d36539937ed74f714cd
Mike Lockwood <lockwood@android.com>
2009-07-13 19:26:34 -0400

    cleanspec.mk: Add clean step needed for switching sholes to real GPS.
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- cleanspec.mk
+++ cleanspec.mk
@@ -61,6 +61,7 @@ $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/STATIC_LIBRARIES/libwebcore_int
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/SHARED_LIBRARIES/libwebcore_intermediates)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/STATIC_LIBRARIES/libwebcore_intermediates)
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/JAVA_LIBRARIES/com.google.android.datamessaging_intermediates)
+$(call add-clean-step, rm -rf $(OUT_DIR)/target/product/sholes/obj/SHARED_LIBRARIES/libhardware_legacy_intermediates)
 
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
