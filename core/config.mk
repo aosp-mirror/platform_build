e4aaa368b3c58c72a771c23de81e46b729bc736c
Marco Nelissen <marcone@google.com>
2009-07-13 20:42:49 -0700

    Apparently the build server uses a different compiler, one that doesn't understand '-fdiagnostics-show-option'

--- core/config.mk
+++ core/config.mk
@@ -75,7 +75,7 @@ SHOW_COMMANDS:= $(filter showcommands,$(MAKECMDGOALS))
 # ###############################################################
 
 # These can be changed to modify both host and device modules.
-COMMON_GLOBAL_CFLAGS:= -DANDROID -fmessage-length=0 -W -Wall -Wno-unused -fdiagnostics-show-option -Winit-self -Wpointer-arith
+COMMON_GLOBAL_CFLAGS:= -DANDROID -fmessage-length=0 -W -Wall -Wno-unused -Winit-self -Wpointer-arith
 COMMON_RELEASE_CFLAGS:= -DNDEBUG -UDEBUG
 
 COMMON_GLOBAL_CPPFLAGS:= $(COMMON_GLOBAL_CFLAGS) -Wsign-promo -Wctor-dtor-privacy 
