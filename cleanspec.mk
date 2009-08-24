40ea53098cc0829abee3cfa7abfafc735f27e93b
Scott Su <scott.su@myriadgroup.com>
2009-08-24 13:44:05 +0800

    Remove frameworks/base/im from build path

--- cleanspec.mk
+++ cleanspec.mk
@@ -75,6 +75,7 @@ $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/EXECUTABLES/openssl_intermediat
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/product/sholes/system/build.prop)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/SHARED_LIBRARIES/libwebcore_intermediates)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/STATIC_LIBRARIES/libwebcore_intermediates)
+$(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/JAVA_LIBRARIES/framework_intermediates)
 
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
