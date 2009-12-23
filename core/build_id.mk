bffc879d879095a1abde88f752c61043e9153b1d
Ed Heyl <edheyl@google.com>
2009-12-22 20:42:05 -0800

    FRD81

--- core/build_id.mk
+++ core/build_id.mk
@@ -18,15 +18,4 @@
 # Defines branch-specific values.
 #
 
-# BUILD_ID is usually used to specify the branch name
-# (like "MAIN") or a branch name and a release candidate
-# (like "TC1-RC5").  It must be a single word, and is
-# capitalized by convention.
-#
-BUILD_ID := MASTER
-
-# DISPLAY_BUILD_NUMBER should only be set for development branches,
-# If set, the BUILD_NUMBER (cl) is appended to the BUILD_ID for
-# a more descriptive BUILD_ID_DISPLAY, otherwise BUILD_ID_DISPLAY
-# is the same as BUILD_ID
-DISPLAY_BUILD_NUMBER := true
+export BUILD_ID=FRD81
