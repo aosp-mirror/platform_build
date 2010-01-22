6bc8ddcc48ce45584328d1b5b2a1820df6316d77
android-build SharedAccount <android-build@sekiwake.mtv.corp.google.com>
2010-01-22 08:34:06 -0800

    clean step for JIT switch

--- cleanspec.mk
+++ cleanspec.mk
@@ -140,6 +140,7 @@ $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/Email_intermedia
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/Email_intermediates)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/vendor/google_voiceime)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/QuickSearchBox.apk)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/SHARED_LIBRARIES/libdvm_intermediates)
 
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
