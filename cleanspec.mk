6323bd93cc00a55beb44b971216b5dd42cd89203
Marco Nelissen <marcone@google.com>
2010-03-02 13:44:32 -0800

    Cleanspec for switching from Music2 back to Music

--- cleanspec.mk
+++ cleanspec.mk
@@ -153,6 +153,8 @@ $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/STATIC_LIBRARIES/libgtest_main_
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/STATIC_LIBRARIES/libgtest_intermediates)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/APPS/Music*)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/Music*)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/APPS/Music*)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/Music*)
 
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
