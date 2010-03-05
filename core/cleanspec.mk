221cdbb17dba524c95b4c0a48c663843f5f739a2
Andrew Stadler <stadler@android.com>
2010-03-05 12:45:52 -0800

    Fix builds due to Music2 moving around.

--- core/cleanspec.mk
+++ core/cleanspec.mk
@@ -174,6 +174,8 @@ $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/soundback.apk)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/talkback.apk)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/APPS/Launcher*)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/Launcher*.apk)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/APPS/Music*)
+$(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/Music*)
 
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
