5720bb91a4e93071539a2ddf2d377284e3afba0a
Colin Cross <ccross@android.com>
2010-03-08 19:07:26 -0800

    Modify the armv7-a target arch to not use NEON
    To build for a target with NEON, use armv7-a-neon
    
    Change-Id: I70d509287b04c0faebe44faf6c8ebb28a7360ff9

--- core/combo/arch/arm/armv7-a.mk
+++ core/combo/arch/arm/armv7-a.mk
@@ -7,8 +7,8 @@ ARCH_ARM_HAVE_64BIT_DATA        := true
 ARCH_ARM_HAVE_HALFWORD_MULTIPLY := true
 ARCH_ARM_HAVE_CLZ               := true
 ARCH_ARM_HAVE_FFS               := true
+ARCH_ARM_HAVE_ARMV7A            := true
 ARCH_ARM_HAVE_VFP               := true
-ARCH_ARM_HAVE_NEON              := true
 
 # Note: Hard coding the 'tune' value here is probably not ideal,
 # and a better solution should be found in the future.
@@ -16,7 +16,7 @@ ARCH_ARM_HAVE_NEON              := true
 arch_variant_cflags := \
     -march=armv7-a \
     -mfloat-abi=softfp \
-    -mfpu=neon
+    -mfpu=vfpv3-d16
 
 arch_variant_ldflags := \
 	-Wl,--fix-cortex-a8
