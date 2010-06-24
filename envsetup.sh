6f8410b97670283d9b5e346ba9ea3a3054158026
Bruce Beare <brucex.j.beare@intel.com>
2010-06-24 13:51:35 -0700

    Vendorsetup needs to look in additional places.
    Change-Id: I698856f8e02487322540e804c038bba28cc23532
    Signed-off-by: Bruce Beare <brucex.j.beare@intel.com>

--- envsetup.sh
+++ envsetup.sh
@@ -1091,7 +1091,7 @@ case `ps -o command -p $$` in
 esac
 
 # Execute the contents of any vendorsetup.sh files we can find.
-for f in `/bin/ls vendor/*/vendorsetup.sh vendor/*/build/vendorsetup.sh device/*/*/vendorsetup.sh 2> /dev/null`
+for f in `/bin/ls vendor/*/vendorsetup.sh vendor/*/*/vendorsetup.sh device/*/*/vendorsetup.sh 2> /dev/null`
 do
     echo "including $f"
     . $f
