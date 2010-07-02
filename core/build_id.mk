e99698b93fb9524bf0694fccadf14f8e33e139bf
The Android Automerger <android-build@android.com>
2010-07-02 08:38:05 -0700

    GRG02
    Change-Id: I6017bbc4fc47a649f7b58bc45dcebe2e82a31f6d

--- core/build_id.mk
+++ core/build_id.mk
@@ -18,6 +18,5 @@
 # (like "MAIN") or a branch name and a release candidate
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
-#
 
-export BUILD_ID=GRF91
+export BUILD_ID=GRG02
