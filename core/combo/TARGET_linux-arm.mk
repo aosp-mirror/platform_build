6d98f5f4d6a25b03c7322ed596ab377ef65cf1fd
Dima Zavin <dima@android.com>
2010-08-05 17:46:47 -0700

    core: fix paths to libm arch-specific include dirs
    Change-Id: I17a1ed13af6d4955a636ad429917b683ac59a947
    Signed-off-by: Dima Zavin <dima@android.com>

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -208,7 +208,7 @@ TARGET_C_INCLUDES := \
 	$(libstdc++_root)/include \
 	$(KERNEL_HEADERS) \
 	$(libm_root)/include \
-	$(libm_root)/include/arch/arm \
+	$(libm_root)/include/arm \
 	$(libthread_db_root)/include
 
 TARGET_CRTBEGIN_STATIC_O := $(TARGET_OUT_STATIC_LIBRARIES)/crtbegin_static.o
