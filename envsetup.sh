0332f0ac1ec69082d337e9990b964b883e5cf689
Jean-Baptiste Queru <jbq@google.com>
2010-10-22 09:52:09 -0700

    Truly default to the full config.
    It is much better than generic for emulator-based work.
    
    Change-Id: I8b69b208bc4969d3f95c3467ca6737de8fd9460d

--- envsetup.sh
+++ envsetup.sh
@@ -311,7 +311,7 @@ function chooseproduct()
         if [ "$TARGET_SIMULATOR" = true ] ; then
             default_value=sim
         else
-            default_value=generic
+            default_value=full
         fi
     fi
 
@@ -465,7 +465,7 @@ function lunch()
         answer=$1
     else
         print_lunch_menu
-        echo -n "Which would you like? [generic-eng] "
+        echo -n "Which would you like? [full-eng] "
         read answer
     fi
 
@@ -473,7 +473,7 @@ function lunch()
 
     if [ -z "$answer" ]
     then
-        selection=generic-eng
+        selection=full-eng
     elif [ "$answer" = "simulator" ]
     then
         selection=simulator
@@ -561,7 +561,7 @@ function tapas()
         apps=all
     fi
 
-    export TARGET_PRODUCT=generic
+    export TARGET_PRODUCT=full
     export TARGET_BUILD_VARIANT=$variant
     export TARGET_SIMULATOR=false
     export TARGET_BUILD_TYPE=release
