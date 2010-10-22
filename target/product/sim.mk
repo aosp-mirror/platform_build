e050219f1f9b22eff8023bb937aef123e7a8c7f2
Jean-Baptiste Queru <jbq@google.com>
2010-10-22 10:22:42 -0700

    Make a full list of locales available
    Use it for the sim build and full build.
    
    Change-Id: I0050f9ee10aafa96cb89cad5d3f492bb1839e331
    Orig-Change-Id: I64dfa7ac1b374529ef5f0c1460dd35171311115e

--- target/product/sim.mk
+++ target/product/sim.mk
@@ -1,6 +1,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
 
 # Overrides
 PRODUCT_NAME := sim
 PRODUCT_DEVICE := sim
-PRODUCT_LOCALES := en_US
