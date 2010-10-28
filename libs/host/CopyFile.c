e6add13d712111b96806911b0330b0de42d68b94
Raphael Moll <ralf@android.com>
2010-10-27 19:06:12 -0700

    Fix "make acp" under Cygwin. DO NOT MERGE.
    Change-Id: I52ae8d4835b27a712d4795e7f41fa9ca4f3a822d

--- libs/host/CopyFile.c
+++ libs/host/CopyFile.c
@@ -69,7 +69,7 @@ static bool isSourceNewer(const struct stat* pSrcStat, const struct stat* pDstSt
  */
 static bool isHiresMtime(const struct stat* pSrcStat)
 {
-#if defined(WIN32_EXE) || defined(USE_MINGW)
+#if defined(WIN32) || defined(USE_MINGW)
     return 0;
 #elif defined(MACOSX_RSRC)
     return pSrcStat->st_mtimespec.tv_nsec > 0;
