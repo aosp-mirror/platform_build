266f48148c60a464eeaac3c0aad303862f4d839b
android-merger <android-merger@google.com>
2010-12-09 10:50:19 -0800

    2.3.1
    Change-Id: I3f1b2a63684f29afdffb246c2d329fa8aac0008a

--- core/version_defaults.mk
+++ core/version_defaults.mk
@@ -41,7 +41,7 @@ ifeq "" "$(PLATFORM_VERSION)"
   # which is the version that we reveal to the end user.
   # Update this value when the platform version changes (rather
   # than overriding it somewhere else).  Can be an arbitrary string.
-  PLATFORM_VERSION := 2.3
+  PLATFORM_VERSION := 2.3.1
 endif
 
 ifeq "" "$(PLATFORM_SDK_VERSION)"
