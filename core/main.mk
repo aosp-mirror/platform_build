bd0c91c8b9d36fd3ed8d8ac7e66b3e1eee3bb0bb
Raphael <raphael@google.com>
2011-02-15 16:09:36 -0800

    Merge c4d4731b4dd306b74b9e2245b4983b6b3c5b866e: 'Build SDK repository'. Do not merge.
    When the build is invoked with the fake target "sdk_repo" and
    a main target of sdk, win_sdk or sdk_addon, we now create
    packages in DIST_DIR that can directly be used to populate the
    SDK Repository.
    This is quite close to how we actually distribute the SDK.
    
    Change-Id: I863ac8baf159432d7c95df46cba6e4f3b9122629

--- core/main.mk
+++ core/main.mk
@@ -190,13 +190,8 @@ endif
 ###
 
 is_sdk_build :=
-ifneq ($(filter sdk,$(MAKECMDGOALS)),)
-is_sdk_build := true
-endif
-ifneq ($(filter win_sdk,$(MAKECMDGOALS)),)
-is_sdk_build := true
-endif
-ifneq ($(filter sdk_addon,$(MAKECMDGOALS)),)
+
+ifneq ($(filter sdk win_sdk sdk_addon,$(MAKECMDGOALS)),)
 is_sdk_build := true
 endif
 
@@ -271,6 +266,11 @@ endif
 ## sdk ##
 
 ifdef is_sdk_build
+
+# Detect if we want to build a repository for the SDK
+sdk_repo_goal := $(strip $(filter sdk_repo,$(MAKECMDGOALS)))
+MAKECMDGOALS := $(strip $(filter-out sdk_repo,$(MAKECMDGOALS)))
+
 ifneq ($(words $(filter-out $(INTERNAL_MODIFIER_TARGETS),$(MAKECMDGOALS))),1)
 $(error The 'sdk' target may not be specified with any other targets)
 endif
@@ -762,11 +762,13 @@ docs: $(ALL_DOCS)
 .PHONY: sdk
 ALL_SDK_TARGETS := $(INTERNAL_SDK_TARGET)
 sdk: $(ALL_SDK_TARGETS)
-$(call dist-for-goals,sdk, \
-    $(ALL_SDK_TARGETS) \
-    $(SYMBOLS_ZIP) \
-    $(INSTALLED_BUILD_PROP_TARGET) \
-)
+ifneq ($(filter sdk win_sdk,$(MAKECMDGOALS)),)
+$(call dist-for-goals,sdk win_sdk, \
+	$(ALL_SDK_TARGETS) \
+	$(SYMBOLS_ZIP) \
+	$(INSTALLED_BUILD_PROP_TARGET) \
+ )
+endif
 
 .PHONY: findbugs
 findbugs: $(INTERNAL_FINDBUGS_HTML_TARGET) $(INTERNAL_FINDBUGS_XML_TARGET)
