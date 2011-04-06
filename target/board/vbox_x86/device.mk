2aced2dc945bddfa94a6fabc7ddec9aa4dac05f0
Vladimir Chtchetkine <vchtchetkine@google.com>
2011-04-05 17:28:43 -0700

    Fix ADB, and a typo in BoardConfig.mk
    Added an instruction to copy init.vbox_x86.rc to the output's root.
    This is required to properly setup networking and other stuff inside
    the emulator.
    
    Change-Id: I84b11b048120a59109aa95d4f18d496a3c874911

--- target/board/vbox_x86/device.mk
+++ target/board/vbox_x86/device.mk
@@ -29,4 +29,5 @@ PRODUCT_COPY_FILES := \
     development/data/etc/apns-conf.xml:system/etc/apns-conf.xml \
     development/data/etc/vold.conf:system/etc/vold.conf \
     build/target/board/vbox_x86/init.rc:root/init.rc \
+    build/target/board/vbox_x86/init.vbox_x86.rc:root/init.vbox_x86.rc \
     $(LOCAL_KERNEL):kernel
