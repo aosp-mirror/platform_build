c9ef69c32c2522a75529157e2889bf19f7a8b23c
Joe Onorato <joeo@google.com>
2011-04-06 12:09:45 -0700

    Remove app that's been removed.
    Change-Id: Ie91629e3d44ee9b16ec2d0ea93978c738b682fb9

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -86,6 +86,7 @@ $(call add-clean-step, rm -rf $(PRODUCT_OUT)/symbols/system/lib/*.so)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/*)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/APPS/*)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/build.prop)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/YouTube*)
 
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
