02da210a5cb4f9cdc7320885484499856236e5a4
Doug Zongker <dougz@android.com>
2011-04-12 15:50:17 -0700

    pass blobs to write_raw_image() instead of using temp file
    Change-Id: I73b71541596612552d52a8f7b1ccdd74451ac6fc

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -200,9 +200,8 @@ class EdifyGenerator(object):
       args = {'device': p.device, 'fn': fn}
       if partition_type == "MTD":
         self.script.append(
-            ('assert(package_extract_file("%(fn)s", "/tmp/%(device)s.img"),\n'
-             '       write_raw_image("/tmp/%(device)s.img", "%(device)s"),\n'
-             '       delete("/tmp/%(device)s.img"));') % args)
+            'write_raw_image(package_extract_file("%(fn)s"), "%(device)s");'
+            % args)
       elif partition_type == "EMMC":
         self.script.append(
             'package_extract_file("%(fn)s", "%(device)s");' % args)
