174c15d067a54ad056d4cc368f790b89fc71dfe9
Jean-Baptiste Queru <jbq@google.com>
2011-05-10 08:29:49 -0700

    Use actual numbers in version numbers.
    This string is used in the browser user agent, and some
    sites get confused when this isn't a dotted-number version
    number into thinking that they should serve stripped down HTML.
    This clears the confusion.
    
    Change-Id: I6c959ff17c540a1f3808f19238b9eb93ba170d2e

--- core/version_defaults.mk
+++ core/version_defaults.mk
@@ -41,7 +41,7 @@ ifeq "" "$(PLATFORM_VERSION)"
   # which is the version that we reveal to the end user.
   # Update this value when the platform version changes (rather
   # than overriding it somewhere else).  Can be an arbitrary string.
-  PLATFORM_VERSION := AOSP
+  PLATFORM_VERSION := 3.1.4.1.5.9.2.6.5
 endif
 
 ifeq "" "$(PLATFORM_SDK_VERSION)"
