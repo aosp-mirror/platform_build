93b4c25f00083dc936d6200b814000299513a9a6
Bruce Beare <bruce.j.beare@intel.com>
2011-05-31 12:47:58 -0700

    Vendorsetup needs to look in additional places.
    Orig-Change-Id: I698856f8e02487322540e804c038bba28cc23532
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>

--- envsetup.sh
+++ envsetup.sh
@@ -1096,7 +1096,7 @@ fi
 unset _xarray
 
 # Execute the contents of any vendorsetup.sh files we can find.
-for f in `/bin/ls vendor/*/vendorsetup.sh vendor/*/build/vendorsetup.sh device/*/*/vendorsetup.sh 2> /dev/null`
+for f in `/bin/ls vendor/*/vendorsetup.sh vendor/*/*/vendorsetup.sh device/*/*/vendorsetup.sh 2> /dev/null`
 do
     echo "including $f"
     . $f
