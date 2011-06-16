742968bda2cda1bcab874a95af371abab5f38e30
Dan Morrill <morrildl@google.com>
2011-06-16 13:43:50 -0700

    Adding a cleanspec to pick up version change. Do not merge.
    Change-Id: I4853ae86488164400d7a0203614b9e87515f6e61

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -54,6 +54,10 @@ $(call add-clean-step, find $(PRODUCT_OUT) -name "*.apk" | xargs rm)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/APPS/*)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/build.prop)
 $(call add-clean-step, rm -rf $(OUT_DIR))
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/build.prop)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/*)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/APPS/*)
+
 
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
