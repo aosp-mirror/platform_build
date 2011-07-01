40b5a67ecd70eee8583b6b12775d18255868d89f
Mike Lockwood <lockwood@android.com>
2011-07-01 12:31:47 -0400

    Remove obsolete persist.service.adb.enable property from default.prop
    Change-Id: I59a2da01eddfc1fa775e027a86f7f818a146a6b0
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- core/main.mk
+++ core/main.mk
@@ -256,12 +256,12 @@ endif # !user_variant
 
 ifeq (true,$(strip $(enable_target_debugging)))
   # Target is more debuggable and adbd is on by default
-  ADDITIONAL_DEFAULT_PROPERTIES += ro.debuggable=1 persist.service.adb.enable=1
+  ADDITIONAL_DEFAULT_PROPERTIES += ro.debuggable=1
   # Include the debugging/testing OTA keys in this build.
   INCLUDE_TEST_OTA_KEYS := true
 else # !enable_target_debugging
   # Target is less debuggable and adbd is off by default
-  ADDITIONAL_DEFAULT_PROPERTIES += ro.debuggable=0 persist.service.adb.enable=0
+  ADDITIONAL_DEFAULT_PROPERTIES += ro.debuggable=0
 endif # !enable_target_debugging
 
 ## eng ##
