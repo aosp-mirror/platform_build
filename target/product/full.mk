2c6a62f8fbfdc4db9cf0a20aab005b1b99bb0988
Jean-Baptiste Queru <jbq@google.com>
2011-07-15 17:47:14 -0700

    Remove Camera app from the generic build.
    That's because a camera isn't required for compatibility and some
    devices don't have a camera.
    
    Add the Camera app in the emulator builds, since the emulator does
    have a camera.
    
    Change-Id: I56a1fe9f703c3b05dec99200a9353d75338c5f40

--- target/product/full.mk
+++ target/product/full.mk
@@ -19,6 +19,9 @@
 # build quite specifically for the emulator, and might not be
 # entirely appropriate to inherit from for on-device configurations.
 
+PRODUCT_PACKAGES := \
+    Camera
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/board/generic/device.mk)
 
