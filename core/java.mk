76a7d82fac1e94617a97393a60f1a2877d841105
Eino-Ville Talvala <etalvala@google.com>
2011-08-22 10:42:44 -0700

    Add filterfw.jar to standard list of java libraries to include.
    Since classes from this jar are part of the public API, it should be
    included by default.
    
    Also add the jar into the dex_preopt list.
    
    Bug: 5098987
    Change-Id: Ib39deffea43714eb56630ad1d65411cfde5878fd

--- core/java.mk
+++ core/java.mk
@@ -29,7 +29,7 @@ ifneq ($(LOCAL_SDK_VERSION),)
   endif
 else
   ifneq ($(LOCAL_NO_STANDARD_LIBRARIES),true)
-    LOCAL_JAVA_LIBRARIES := core core-junit ext framework $(LOCAL_JAVA_LIBRARIES)
+    LOCAL_JAVA_LIBRARIES := core core-junit ext framework filterfw $(LOCAL_JAVA_LIBRARIES)
   endif
 endif
 
