7766ea28843bb3cb223c65b1752055288ad245d8
Dianne Hackborn <hackbod@google.com>
2011-09-21 15:25:37 -0700

    Let's call MR0 4.0.1.
    Change-Id: Ie55012702e2cfb1eb247941e8b73519ad9b5c547

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -102,6 +102,7 @@ $(call add-clean-step, rm -rf $(PRODUCT_OUT)/data/app/*)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/build.prop)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/JAVA_LIBRARIES/*)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/framework/*)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/build.prop)
 
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
