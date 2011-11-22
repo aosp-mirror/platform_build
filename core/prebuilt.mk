7d4e450ebc6144e7a74263cadd1e9e77e5b9ca63
Ying Wang <wangying@google.com>
2011-11-22 14:34:39 -0800

    Fix OVERRIDE_BUILT_MODULE_PATH for prebuilt shared libraries.
    So that prebuilt shared libraries can be referenced with the module name
    in other module's Android.mk.
    This is a clean cherrypick from later branches.
    Bug: 5652242
    
    Change-Id: I2d27e9b7109129bdd3f61e0aef56472a13418d54

--- core/prebuilt.mk
+++ core/prebuilt.mk
@@ -16,6 +16,17 @@ ifneq ($(LOCAL_PREBUILT_JAVA_LIBRARIES),)
 $(error dont use LOCAL_PREBUILT_JAVA_LIBRARIES anymore LOCAL_PATH=$(LOCAL_PATH))
 endif
 
+ifdef LOCAL_IS_HOST_MODULE
+  my_prefix:=HOST_
+else
+  my_prefix:=TARGET_
+endif
+ifeq (SHARED_LIBRARIES,$(LOCAL_MODULE_CLASS))
+  # Put the built targets of all shared libraries in a common directory
+  # to simplify the link line.
+  OVERRIDE_BUILT_MODULE_PATH := $($(my_prefix)OUT_INTERMEDIATE_LIBRARIES)
+endif
+
 include $(BUILD_SYSTEM)/base_rules.mk
 
 # Deal with the OSX library timestamp issue when installing
