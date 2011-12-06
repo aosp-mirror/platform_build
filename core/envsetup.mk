ffe03c984b2836a9b4e4ddb5a17ec0dd26ba686c
Jean-Baptiste Queru <jbq@google.com>
2011-12-06 10:29:46 -0800

    Set OUT_DIR based on a common base path
    Change-Id: I2c4ba8b101d4d186ed4b371b9d07b3bcf75bbcc9
    Author: Jean-Baptiste Queru<jbq@google.com>
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>

--- core/envsetup.mk
+++ core/envsetup.mk
@@ -133,7 +133,11 @@ endif
 # figure out the output directories
 
 ifeq (,$(strip $(OUT_DIR)))
+ifeq (,$(strip $(OUT_DIR_COMMON_BASE)))
 OUT_DIR := $(TOPDIR)out
+else
+OUT_DIR := $(OUT_DIR_COMMON_BASE)/$(notdir $(PWD))
+endif
 endif
 
 DEBUG_OUT_DIR := $(OUT_DIR)/debug
