d38c870d2e168b051692b20ade0fc966c9e2daf7
Conley Owens <cco3@android.com>
2011-12-22 09:46:19 -0800

    Get build system to work with Make 3.82
    Note: `sort` can't seem to handle newlines in 3.82.
    
    Change-Id: Ieeaee1d19688688a02e396bbc5b4dea26e7f6943

--- core/main.mk
+++ core/main.mk
@@ -39,12 +39,14 @@ endif
 
 # check for broken versions of make
 ifeq (0,$(shell expr $$(echo $(MAKE_VERSION) | sed "s/[^0-9\.].*//") = 3.81))
+ifeq (0,$(shell expr $$(echo $(MAKE_VERSION) | sed "s/[^0-9\.].*//") = 3.82))
 $(warning ********************************************************************************)
 $(warning *  You are using version $(MAKE_VERSION) of make.)
-$(warning *  Android can only be built by version 3.81.)
+$(warning *  Android can only be built by versions 3.81 and 3.82.)
 $(warning *  see http://source.android.com/source/download.html)
 $(warning ********************************************************************************)
 $(error stopping)
+endif
 endif
 
 TOP := .
