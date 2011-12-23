29b6918fddf7eb4eef4a1defa322f294fbd8fc5c
Ying Wang <wangying@google.com>
2011-12-22 17:40:05 -0800

    Remove the restriction that PHONY_PACKAGE needs LOCAL_REQUIRED_MODULES
    Change-Id: I33e776ea80368e0f0290169e680689a680186ed6

--- core/phony_package.mk
+++ core/phony_package.mk
@@ -2,10 +2,6 @@ ifneq ($(strip $(LOCAL_SRC_FILES)),)
 $(error LOCAL_SRC_FILES are not allowed for phony packages)
 endif
 
-ifeq ($(strip $(LOCAL_REQUIRED_MODULES)),)
-$(error LOCAL_REQUIRED_MODULES is required for phony packages)
-endif
-
 LOCAL_MODULE_CLASS := FAKE
 LOCAL_MODULE_SUFFIX := -timestamp
 
