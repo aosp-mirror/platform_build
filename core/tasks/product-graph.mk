f5debbfee630f0d49b9dcb8a4eb0bfa83822ecc6
Joe Onorato <joeo@google.com>
2012-05-07 17:15:15 -0700

    make product-graph now filtered
    Change-Id: I6bd93a87902e20a24c1c58152fb578ef1f4cb208

--- core/tasks/product-graph.mk
+++ core/tasks/product-graph.mk
@@ -16,9 +16,20 @@
 
 products_pdf := $(OUT_DIR)/products.pdf
 products_graph := $(products_pdf:%.pdf=%.dot)
+ifeq ($(strip $(ANDROID_PRODUCT_GRAPH)),)
+products_list := $(INTERNAL_PRODUCT)
+else
+ifeq ($(strip $(ANDROID_PRODUCT_GRAPH)),--all)
+products_list := --all
+else
+products_list := $(foreach prod,$(ANDROID_PRODUCT_GRAPH),$(call resolve-short-product-name,$(prod)))
+endif
+endif
+
+$(products_graph): PRIVATE_PRODUCTS := $(products_list)
 
 $(products_graph):
-	@echo Product graph DOT: $@
+	@echo Product graph DOT: $@ for $(PRIVATE_PRODUCTS)
 	$(hide) ( \
 		echo 'digraph {'; \
 		echo 'graph [ ratio=.5 ];'; \
@@ -27,12 +38,17 @@ $(products_graph):
 			echo \"$(d)\" -\> \"$(p)\";)) \
 		$(foreach prod, \
 			$(sort $(foreach p,$(ALL_PRODUCTS), \
-				$(foreach d,$(PRODUCTS.$(strip $(p)).INHERITS_FROM), \
-					$(d))) \
+				$(foreach d,$(PRODUCTS.$(strip $(p)).INHERITS_FROM), $(d))) \
 				$(foreach p,$(ALL_PRODUCTS),$(p))), \
-			echo \"$(prod)\" [ label=\"$(dir $(prod))\\n$(notdir $(prod))\"];) \
+			echo \"$(prod)\" [ \
+					label=\"$(dir $(prod))\\n$(notdir $(prod))\\n\\n$(PRODUCTS.$(strip $(prod)).PRODUCT_MODEL)\\n$(PRODUCTS.$(strip $(prod)).PRODUCT_DEVICE)\" \
+					$(if $(filter $(prod),$(PRIVATE_PRODUCTS)), \
+						style=\"filled\" fillcolor=\"#FFFDB0\",) \
+				];) \
 		echo '}' \
-	) > $@
+	) \
+	| ./build/tools/filter-product-graph.py $(PRIVATE_PRODUCTS) \
+	> $@
 
 # This rule doesn't include any nodes that don't inherit from
 # anything or don't have anything inherit from them, to make the
