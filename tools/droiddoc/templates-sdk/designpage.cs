e4d8f1b1f5a6e2308bfeff04d7fa51ff7394c644
Scott Main <smain@google.com>
2012-06-21 18:03:05 -0700

    Massive clobber of all template files in developer site for new design
    Change-Id: I5f243dcb377a8b2e573c404a9e685cc64e0e06d2

--- tools/droiddoc/templates-sdk/designpage.cs
+++ tools/droiddoc/templates-sdk/designpage.cs
@@ -14,7 +14,12 @@
     <script>var SITE_ROOT = '<?cs var:toroot ?>design';</script>
     <script src="<?cs var:toroot ?>assets/design/default.js"></script>
   </head>
-  <body itemscope itemtype="http://schema.org/Article">
+  <body class="gc-documentation 
+    <?cs if:(guide||develop||training||reference||tools||sdk) ?>develop<?cs
+    elif:design ?>design<?cs
+    elif:distribute ?>distribute<?cs
+    /if ?>" itemscope itemtype="http://schema.org/Article">
+    <a name="top"></a>
 
     <div id="page-container">
 
