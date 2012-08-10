e4ba06c35c2874b6d15afb6c23c57a07ee5df587
Ying Wang <wangying@google.com>
2012-08-10 15:12:15 -0700

    Allow full_x86 and full_mips in unbundled build.
    So we can do unbundled builds for x86 and mips devices.
    
    Change-Id: Ibd8c3b0371c316b713a748a6e6ce14fe98fdc4aa

--- target/product/AndroidProducts.mk
+++ target/product/AndroidProducts.mk
@@ -25,12 +25,12 @@
 # it includes.
 #
 
-# Unbundled apps will be built with the default product config.
+# Unbundled apps will be built with the most generic product config.
 ifneq ($(TARGET_BUILD_APPS),)
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/core.mk \
-    $(LOCAL_DIR)/generic.mk \
-    $(LOCAL_DIR)/full.mk
+    $(LOCAL_DIR)/full.mk \
+    $(LOCAL_DIR)/full_x86.mk \
+    $(LOCAL_DIR)/full_mips.mk
 else
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/core.mk \
