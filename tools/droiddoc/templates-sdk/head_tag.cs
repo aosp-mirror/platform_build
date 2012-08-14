f5089846a1876f8e976d4bd1ddf383d3ed68d558
Scott Main <smain@google.com>
2012-08-14 16:31:07 -0700

    docs: combine all possible js files into one to reduce the number of requests Also make change to doc template and css to fix footer layout bug introduced by change 9edfa6db8c53ef9515aaeed734bea02c5d65e1e5
    Change-Id: Icfb1b499abfca6a39b5bff8fab576b14a1cf960a

--- tools/droiddoc/templates-sdk/head_tag.cs
+++ tools/droiddoc/templates-sdk/head_tag.cs
@@ -9,42 +9,24 @@
     var:page.title ?> | <?cs
   /if ?>Android Developers</title>
 
-<!-- BASIC STYLESHEETS -->
+<!-- STYLESHEETS -->
 <link rel="stylesheet"
 href="<?cs if:android.whichdoc != 'online' ?>http:<?cs /if ?>//fonts.googleapis.com/css?family=Roboto:regular,medium,thin,italic,mediumitalic,bold" title="roboto">
 <link href="<?cs var:toroot ?>assets/css/default.css" rel="stylesheet" type="text/css">
 
 <?cs if:reference ?>
-<!-- alternative FULLSCREEN styles -->
+<!-- FULLSCREEN STYLESHEET -->
 <link href="<?cs var:toroot ?>assets/css/fullscreen.css" rel="stylesheet" class="fullscreen"
 type="text/css">
 <?cs /if ?>
 
-<!-- BASIC JAVASCRIPT -->
-<script src="<?cs var:toroot ?>assets/js/global-libraries-combined.js"
-        type="text/javascript"></script>
+<!-- JAVASCRIPT -->
+<script src="<?cs if:android.whichdoc != 'online' ?>http:<?cs /if ?>//www.google.com/jsapi" type="text/javascript"></script>
+<script src="<?cs var:toroot ?>assets/js/global-libraries-combined.js" type="text/javascript"></script>
 <script type="text/javascript">
   var toRoot = "<?cs var:toroot ?>";
 </script>
 <script src="<?cs var:toroot ?>assets/js/docs.js" type="text/javascript"></script>
-
-<?cs if:carousel ?>
-<!-- homepage carousel -->
-<script type="text/javascript" src="<?cs var:toroot ?>assets/js/carousel.js"></script>
-<?cs /if ?>
-
-<!-- search -->
-<script src="<?cs if:android.whichdoc != 'online' ?>http:<?cs /if ?>//www.google.com/jsapi" type="text/javascript"></script>
-<script src="<?cs var:toroot ?>assets/js/search_autocomplete.js" type="text/javascript"></script>
-
-<?cs if:tabbedList ?>
-<!-- tabbed lists -->
-<script src="<?cs var:toroot ?>assets/js/tabbedlist.js" type="text/javascript"></script>
-<?cs /if ?>
-
-<?cs if:reference ?>
-<script src="<?cs var:toroot ?>assets/js/droiddoc.js" type="text/javascript"></script>
-<?cs /if ?>
 <script src="<?cs var:toroot ?>navtree_data.js" type="text/javascript"></script>
 
 </head>
