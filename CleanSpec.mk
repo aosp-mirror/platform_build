7d862475fd63d005f3c28a7a2407ddf45d645add
Ying Wang <wangying@google.com>
2012-09-05 15:58:29 -0700

    Clean step for libc.
    (cherry-pick of ab1e93d4d4f4a46fa922df5ba7ccd41dde1221ac.)
    
    Change-Id: I304312d6eb0346c050d02bd834565e0d43d8b4dd

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -127,6 +127,11 @@ $(call add-clean-step, rm -rf $(OUT_DIR)/host/darwin-x86/obj)
 
 $(call add-clean-step, rm -f $(OUT_DIR)/versions_checked.mk)
 
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/STATIC_LIBRARIES)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/SHARED_LIBRARIES)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/EXECUTABLES)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/lib/*.o)
+
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
