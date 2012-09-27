0c4eb4188553fc06049834111416067da53ac605
Ying Wang <wangying@google.com>
2012-09-27 13:26:25 -0700

    Load only the current product config makefile.
    To reduce the start time.
    With the change and previous change of doing clean step in only given paths,
    start time of mm/mmm is reduced from ~5s to about 1s;
    lunch time is reduced from ~40s to 3.5s.
    
    (cherry-pick of 157a5e1695593f935c3223430f4530e21f990378.)
    
    Bug: 7186768,7169854
    Change-Id: I0be5e2721efa66a80f112acf8f95e177ebc2f65b

--- core/product.mk
+++ core/product.mk
@@ -16,7 +16,12 @@
 
 #
 # Functions for including AndroidProducts.mk files
-#
+# PRODUCT_MAKEFILES is set up in AndroidProducts.mks.
+# Format of PRODUCT_MAKEFILES:
+# <product_name>:<path_to_the_product_makefile>
+# If the <product_name> is the same as the base file name (without dir
+# and the .mk suffix) of the product makefile, "<product_name>:" can be
+# omitted.
 
 #
 # Returns the list of all AndroidProducts.mk files.
