478a3808f84061495b1ad84f1a6445a23b4695b5
Ulas Kirazci <ulas@google.com>
2012-09-26 17:19:01 -0700

    Rely only on NDK libs if built against the NDK.
    Change-Id: Icf0b690181669830d3b2d19aa5b7dd6815b44bfc

--- core/native_test.mk
+++ core/native_test.mk
@@ -4,12 +4,16 @@
 ###########################################
 
 LOCAL_CFLAGS += -DGTEST_OS_LINUX_ANDROID -DGTEST_HAS_STD_STRING
+
+LOCAL_C_INCLUDES += external/gtest/include
+LOCAL_STATIC_LIBRARIES += libgtest libgtest_main
+
+ifndef LOCAL_SDK_VERSION
 LOCAL_C_INCLUDES += bionic \
                     bionic/libstdc++/include \
-                    external/gtest/include \
                     external/stlport/stlport
-LOCAL_STATIC_LIBRARIES += libgtest libgtest_main
 LOCAL_SHARED_LIBRARIES += libstlport
+endif
 
 ifndef LOCAL_MODULE_PATH
 LOCAL_MODULE_PATH := $(TARGET_OUT_DATA_NATIVE_TESTS)/$(LOCAL_MODULE)
