4f2f16da143fede94a1124ee79efccee78442b90
Nick Kralevich <nnk@google.com>
2012-10-19 13:36:49 -0700

    x86: add --warn-shared-textrel
    Emit a warning when the static linker detects a shared library
    has text relocations. Text relocations make it harder to
    share pages across processes, and make it harder to use
    certain memory protection features in, for example, SELinux.
    
    This warning will turn into an error in a future change
    (via --fatal-warnings)
    
    Change-Id: I2d169c7ce3600d02e25591f3ec15aba363730298
    References: http://www.akkadia.org/drepper/textrelocs.html

--- core/combo/TARGET_linux-x86.mk
+++ core/combo/TARGET_linux-x86.mk
@@ -141,6 +141,7 @@ TARGET_GLOBAL_LDFLAGS += -m32
 
 TARGET_GLOBAL_LDFLAGS += -Wl,-z,noexecstack
 TARGET_GLOBAL_LDFLAGS += -Wl,-z,relro -Wl,-z,now
+TARGET_GLOBAL_LDFLAGS += -Wl,--warn-shared-textrel
 TARGET_GLOBAL_LDFLAGS += -Wl,--gc-sections
 
 TARGET_C_INCLUDES := \
