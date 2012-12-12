cced518b1e09734c90f2fbe5cb0bf85ae9c5fccc
Nick Kralevich <nnk@google.com>
2012-12-11 16:35:06 -0800

    It's a new version!
    Change-Id: I97ba954c8726ae3b64c43260ce221642d5865c6d

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -152,6 +152,11 @@ $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/APPS/*)
 
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/build.prop)
 
+# Start of "K" development!
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/build.prop)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/*)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/APPS/*)
+
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
