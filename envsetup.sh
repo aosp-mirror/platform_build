fa72a60d0ca8d373127a844010871392c6749ef4
Joe Onorato <joeo@google.com>
2013-01-11 12:49:04 -0800

    Add -f option to make for m, mm and mmm
    It lets these shell functions work even if you have some
    other makefile at the root of your tree.
    
    Change-Id: Ic84688abd89cc70c062e156f1397296837fb137e

--- envsetup.sh
+++ envsetup.sh
@@ -603,7 +603,7 @@ function m()
 {
     T=$(gettop)
     if [ "$T" ]; then
-        make -C $T $@
+        make -C $T -f build/core/main.mk $@
     else
         echo "Couldn't locate the top of the tree.  Try setting TOP."
     fi
@@ -643,7 +643,7 @@ function mm()
         elif [ ! "$M" ]; then
             echo "Couldn't locate a makefile from the current directory."
         else
-            ONE_SHOT_MAKEFILE=$M make -C $T all_modules $@
+            ONE_SHOT_MAKEFILE=$M make -C $T -f build/core/main.mk all_modules $@
         fi
     fi
 }
@@ -690,7 +690,7 @@ function mmm()
                 fi
             fi
         done
-        ONE_SHOT_MAKEFILE="$MAKEFILE" make -C $T $DASH_ARGS $MODULES $ARGS
+        ONE_SHOT_MAKEFILE="$MAKEFILE" make -C $T -f build/core/main.mk $DASH_ARGS $MODULES $ARGS
     else
         echo "Couldn't locate the top of the tree.  Try setting TOP."
     fi
