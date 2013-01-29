98f23e7af0480b70515552195e359633191f83d8
Scott Main <smain@google.com>
2013-01-29 15:12:52 -0800

    docs: delete obsolete asset files
    Change-Id: I1aa8a8fa757dbc81180c0585eacb68df5fe0ceb1

--- tools/droiddoc/templates-sdk/trailer.cs
+++ tools/droiddoc/templates-sdk/trailer.cs
@@ -1,30 +1,5 @@
 </div> <!-- end body-content --> <?cs # normally opened by header.cs ?>
 
-
-
-
-<!-- Grid - for dev 
-<script type="text/javascript">
-
-window.gOverride = {
-		gColor: '#FF0000',
-		pColor: '#EEEEEE',
-		gOpacity: 0.10,
-		pOpacity: 0.40,
-		pHeight: 16,
-		pOffset: 2,
-		gColumns:16,
-		gEnabled:false,
-		pEnabled:false
-	}
-</script>
-
-<link href="<?cs var:toroot ?>assets/js/grid/960.gridder.css" rel="stylesheet" type="text/css">
-<script src="<?cs var:toroot ?>assets/js/grid/960.gridder.js" type="text/javascript"></script>
--->
-
-
-
 <?cs if:carousel ?>
 <script type="text/javascript">
 $('.slideshow-container').dacSlideshow({
