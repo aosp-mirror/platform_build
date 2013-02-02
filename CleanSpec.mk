aa8c2956c2014984f7fed3a4c911934a2161b2b2
Dianne Hackborn <hackbod@google.com>
2013-02-01 16:51:45 -0800

    You would not believe how much we all love jelly beans.
    Seriously.
    
    We looooooove them.
    
    Especially Ryan.
    
    Change-Id: I7ef04f1113147cb86a660c566b0d95d8c55eafd4

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -163,6 +163,11 @@ $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/SHARED_LIBRARIES)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/EXECUTABLES)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/lib/*.o)
 
+# Wait, back to some JB development!
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/build.prop)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/*)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/APPS/*)
+
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
