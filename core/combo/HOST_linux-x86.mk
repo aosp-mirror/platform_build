79e3f7799648f6de27087f17b2b50cc79870805e
Ying Wang <wangying@google.com>
2013-02-12 11:48:37 -0800

    Allow user to override the linux host toolchain.
    Bug: 8181840
    Change-Id: Ib2a90d33414758e95b1a3c60db62c4188944dfdd

--- core/combo/HOST_linux-x86.mk
+++ core/combo/HOST_linux-x86.mk
@@ -24,7 +24,9 @@ endef
 
 # Previously the prebiult host toolchain is used only for the sdk build,
 # that's why we have "sdk" in the path name.
+ifeq ($(strip $(HOST_TOOLCHAIN_PREFIX)),)
 HOST_TOOLCHAIN_PREFIX := prebuilts/tools/gcc-sdk
+endif
 # Don't do anything if the toolchain is not there
 ifneq (,$(strip $(wildcard $(HOST_TOOLCHAIN_PREFIX)/gcc)))
 HOST_CC  := $(HOST_TOOLCHAIN_PREFIX)/gcc
