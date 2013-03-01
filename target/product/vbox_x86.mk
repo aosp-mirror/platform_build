962d06ef65c12ac6c059ea4b6c1140b96e1e8ffe
Ying Wang <wangying@google.com>
2013-02-28 16:39:32 -0800

    Add module names to emulator PRODUCT_PACKAGES.
    Instead of using "LOCAL_MODULE_TAGS := debug" to install them.
    
    Bug: 8276818
    Change-Id: I7eba3c4e6c2f07ebe661522cb0e14afcc3ef7e92

--- target/product/vbox_x86.mk
+++ target/product/vbox_x86.mk
@@ -25,6 +25,12 @@ endif
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/board/vbox_x86/device.mk)
 
+PRODUCT_PACKAGES += \
+       camera.vbox_x86 \
+       lights.vbox_x86 \
+       gps.vbox_x86 \
+       sensors.vbox_x86
+
 PRODUCT_NAME := vbox_x86
 PRODUCT_DEVICE := vbox_x86
 PRODUCT_MODEL := Full Android on x86 VirtualBox
