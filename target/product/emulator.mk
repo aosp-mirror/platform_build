c3eb68c4ba383e7de2dd9b761d96dbe646586845
keunyoung <keunyoung@google.com>
2013-03-05 11:07:52 -0800

    pull goldfish specific stuffs from emulator.mk
    - init.goldfish.rc, init.goldfish.sh, and ueventd.goldfish.rc
    
    Change-Id: Ia4a12477faf727c4e2276313d6d9dcf7ea430b22

--- target/product/emulator.mk
+++ target/product/emulator.mk
@@ -36,4 +36,8 @@ PRODUCT_PACKAGES += \
 	lib64EGL_translator
 
 PRODUCT_COPY_FILES += \
-    device/generic/goldfish/fstab.goldfish:root/fstab.goldfish
+    device/generic/goldfish/fstab.goldfish:root/fstab.goldfish \
+    device/generic/goldfish/init.goldfish.rc:root/init.goldfish.rc \
+    device/generic/goldfish/init.goldfish.sh:system/etc/init.goldfish.sh \
+    device/generic/goldfish/ueventd.goldfish.rc:root/ueventd.goldfish.rc
+
