8805930b6f2022f7f3050220866dce76f61008ab
Rom Lemarchand <romlem@google.com>
2013-05-03 05:41:01 -0700

    core: combo: arm: add cortex-a8 target
    Change-Id: I83e409dd048762acbd2e2dec9b0095933141cff0

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -9,10 +9,14 @@ ARCH_ARM_HAVE_NEON              := true
 ifeq ($(strip $(TARGET_CPU_VARIANT)), cortex-a15)
 	arch_variant_cflags := -mcpu=cortex-a15
 else
+ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a8)
+	arch_variant_cflags := -mcpu=cortex-a8
+else
 ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a7)
 	arch_variant_cflags := -mcpu=cortex-a7
 else
 	arch_variant_cflags := -march=armv7-a
+endif
 endif
 endif
 
