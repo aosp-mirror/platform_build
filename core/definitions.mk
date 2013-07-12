1390cac3cea58f1c5c738bf875b4de5ba4d68fa4
JP Abgrall <jpa@google.com>
2013-07-11 19:08:06 -0700

    core: don't hardcode "out" during findleaves.py invocations.
    "out" can be $OUT_DIR
    
    Bug: 7183057
    Change-Id: I7b52d40de4398f481e47f6d6d719fb550d874f73

--- core/definitions.mk
+++ core/definitions.mk
@@ -147,7 +147,7 @@ endef
 # $(1): directory to search under
 # Ignores $(1)/Android.mk
 define first-makefiles-under
-$(shell build/tools/findleaves.py --prune=out --prune=.repo --prune=.git \
+$(shell build/tools/findleaves.py --prune=$(OUT_DIR) --prune=.repo --prune=.git \
         --mindepth=2 $(1) Android.mk)
 endef
 
