850de1995cf026a4c9a8809303ddba5fe4cd3e13
Michael Runge <mrunge@google.com>
2013-07-22 20:42:44 +0000

    Revert "Update OTA to understand SELinux filesystem labels"
    This reverts commit fbbd79530adc6ddd6bbfb3c5fc60ba5ec0ce5f2d.
    
    All incremental auto OTAs broken b/9964074
    
    Change-Id: I500d2ac194804abd20a0e01d9862fd42e8f5d2de

--- tools/fs_config/Android.mk
+++ tools/fs_config/Android.mk
@@ -17,7 +17,6 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := fs_config.c
 LOCAL_MODULE := fs_config
-LOCAL_STATIC_LIBRARIES := libselinux
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 
 include $(BUILD_HOST_EXECUTABLE)
