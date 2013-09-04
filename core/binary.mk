JAOSP/aosp_platform_build

Add "WITH_SYNTAX_CHECK=1 make ..."
The new option WITH_SYNTAX_CHECK=1 instructs build system to invoke
"clang -fsyntax-only" to utilize clang's better diagnostics before calling
LOCAL_CC/LOCAL_CXX for code generation.  The compilation time is slightly
longer, and the generated object file should be the same as w/o WITH_SYNTAX_CHECK

Project use lots of GCC extensions (eg. nested function) not supported
by clang may opt out by adding LOCAL_NO_SYNTAX_CHECK:=true

Change-Id: I5689586788ef049bd967364f71f31f1e359bd121

core/binary.mk
@@ -170,12 +170,32 @@ $(LOCAL_INTERMEDIATE_TARGETS): PRIVATE_TARGET_GLOBAL_CPPFLAGS := $(TARGET_GLOBAL
 $(LOCAL_INTERMEDIATE_TARGETS): PRIVATE_NO_DEFAULT_COMPILER_FLAGS := \
     $(strip $(LOCAL_NO_DEFAULT_COMPILER_FLAGS))
 
+my_syntax_prefix :=
+ifneq ($(strip $(WITH_SYNTAX_CHECK)),)
+  ifneq ($(strip $(LOCAL_NO_SYNTAX_CHECK)),true)
+    my_syntax_prefix := prebuilts/clang/$(HOST_PREBUILT_TAG)/host/3.3/bin
+    ifeq ($(wildcard $(my_syntax_prefix)/ccc-syntax),)
+      $(warning *** Disable checksyntax because $(my_syntax_prefix)/ccc-syntax does not exist)
+      my_syntax_prefix :=
+    endif
+  endif
+endif
+
+ifneq ($(strip $(LOCAL_IS_HOST_MODULE)),)
+  my_syntax_arch := host
+else
+  my_syntax_arch := $(TARGET_ARCH)
+endif
+
 ifeq ($(strip $(LOCAL_CC)),)
   ifeq ($(strip $(LOCAL_CLANG)),true)
     LOCAL_CC := $(CLANG)
   else
     LOCAL_CC := $($(my_prefix)CC)
   endif
+endif
+ifneq ($(my_syntax_prefix),)
+  LOCAL_CC := $(my_syntax_prefix)/ccc-syntax $(my_syntax_arch) "$(LOCAL_CC)"
 endif
 $(LOCAL_INTERMEDIATE_TARGETS): PRIVATE_CC := $(LOCAL_CC)
 
@@ -185,6 +205,9 @@ ifeq ($(strip $(LOCAL_CXX)),)
   else
     LOCAL_CXX := $($(my_prefix)CXX)
   endif
+endif
+ifneq ($(my_syntax_prefix),)
+  LOCAL_CXX := $(my_syntax_prefix)/cxx-syntax $(my_syntax_arch) "$(LOCAL_CXX)"
 endif
 $(LOCAL_INTERMEDIATE_TARGETS): PRIVATE_CXX := $(LOCAL_CXX)
 

aosp-mirror/platform_build

core/binary.mk
@@ -170,12 +170,25 @@ $(LOCAL_INTERMEDIATE_TARGETS): PRIVATE_TARGET_GLOBAL_CPPFLAGS := $(TARGET_GLOBAL
 $(LOCAL_INTERMEDIATE_TARGETS): PRIVATE_NO_DEFAULT_COMPILER_FLAGS := \
     $(strip $(LOCAL_NO_DEFAULT_COMPILER_FLAGS))
 
+ifeq ($(strip $(WITH_SYNTAX_CHECK)),)
+  LOCAL_NO_SYNTAX_CHECK := true
+endif
+
+ifneq ($(strip $(LOCAL_IS_HOST_MODULE)),)
+  my_syntax_arch := host
+else
+  my_syntax_arch := $(TARGET_ARCH)
+endif
+
 ifeq ($(strip $(LOCAL_CC)),)
   ifeq ($(strip $(LOCAL_CLANG)),true)
     LOCAL_CC := $(CLANG)
   else
     LOCAL_CC := $($(my_prefix)CC)
   endif
+endif
+ifneq ($(LOCAL_NO_SYNTAX_CHECK),true)
+  LOCAL_CC := $(SYNTAX_TOOLS_PREFIX)/ccc-syntax $(my_syntax_arch) "$(LOCAL_CC)"
 endif
 $(LOCAL_INTERMEDIATE_TARGETS): PRIVATE_CC := $(LOCAL_CC)
 
@@ -185,6 +198,9 @@ ifeq ($(strip $(LOCAL_CXX)),)
   else
     LOCAL_CXX := $($(my_prefix)CXX)
   endif
+endif
+ifneq ($(LOCAL_NO_SYNTAX_CHECK),true)
+  LOCAL_CXX := $(SYNTAX_TOOLS_PREFIX)/cxx-syntax $(my_syntax_arch) "$(LOCAL_CXX)"
 endif
 $(LOCAL_INTERMEDIATE_TARGETS): PRIVATE_CXX := $(LOCAL_CXX)
 