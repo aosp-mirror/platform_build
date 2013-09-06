fd1327256e698a7b5dd677bea45947fd20b23208
Victoria Lease <violets@google.com>
2013-09-06 14:00:38 -0700

    include external/sil-fonts
    Bug: 10625246
    Change-Id: I15c655030557e91663635b70848da86c9e0221e7

--- target/product/generic_no_telephony.mk
+++ target/product/generic_no_telephony.mk
@@ -83,6 +83,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 $(call inherit-product-if-exists, frameworks/base/data/fonts/fonts.mk)
 $(call inherit-product-if-exists, external/noto-fonts/fonts.mk)
 $(call inherit-product-if-exists, external/naver-fonts/fonts.mk)
+$(call inherit-product-if-exists, external/sil-fonts/fonts.mk)
 $(call inherit-product-if-exists, frameworks/base/data/keyboards/keyboards.mk)
 $(call inherit-product-if-exists, frameworks/webview/chromium/chromium.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core.mk)
