906cb78168718dafea120b0a0a6cf239b90a2a71
Andrew Hsieh <andrewhsieh@google.com>
2013-09-10 17:37:14 +0800

    Add "WITH_STATIC_ANALYZER=1 m/mm/mmm/mma/mmma ..."
    The new option WITH_STATIC_ANALYZER=1 instructs build system to
    run static analyzer via "clang --analyze" on a successful build.
    If analyzer finds any issue, instruction to open report is displayed.
    See http://clang-analyzer.llvm.org/scan-build.html for details.
    
    WITH_STATIC_ANALYZER trumps WITH_SYNTAX_CHECK if both exist.
    
    Project use lots of GCC extensions (eg. nested function) not supported
    by clang may opt out by adding LOCAL_NO_STATIC_ANALYZER:=true
    
    Change-Id: I9970560560bd52ce5f0fd7129c3488629627c735

--- core/clear_vars.mk
+++ core/clear_vars.mk
@@ -138,6 +138,7 @@ LOCAL_PREBUILT_MODULE_FILE:=
 LOCAL_POST_INSTALL_CMD:=
 LOCAL_DIST_BUNDLED_BINARIES:=
 LOCAL_NO_SYNTAX_CHECK:=
+LOCAL_NO_STATIC_ANALYZER:=
 
 # Trim MAKEFILE_LIST so that $(call my-dir) doesn't need to
 # iterate over thousands of entries every time.
