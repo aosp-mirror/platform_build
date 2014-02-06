c92f1407a1cc32e81971d89086f84a6dc643ef9d
Colin Cross <ccross@android.com>
2014-02-06 14:45:37 -0800

    add support for module supported or unsupported target architectures
    Add four new variables for module makefiles:
    
    LOCAL_MODULE_TARGET_ARCH specifies that a module is only supported for
    one or more architectures.  Any architecture not in the list will be
    not attempt to build the module.  The expected use case is prebuilts
    that are only suitable for a single architecture, or modules like llvm
    that need per-architecture support.
    
    LOCAL_MODULE_UNSUPPORTED_TARGET_ARCH specifies that a module cannot be
    built for one or more architectures.
    
    LOCAL_MODULE_TARGET_ARCH_WARN and LOCAL_MODULE_UNSUPPORTED_TARGET_ARCH_WARN
    are the same, but warn that the arch is not supported, which is useful
    for modules that are critical but not yet working.
    
    The logic for whether or not to build an architecture is fairly
    complicated, so this patch consolidates it into module_arch_supported.mk
    
    Change-Id: I120caf4a375f484e1fd6017b60c2f53882ae01e6

--- core/shared_library.mk
+++ core/shared_library.mk
@@ -10,14 +10,19 @@ $(warning $(LOCAL_MODULE): LOCAL_UNSTRIPPED_PATH for shared libraries is unsuppo
 endif
 endif
 
-ifneq ($(TARGET_IS_64_BIT)|$(LOCAL_32_BIT_ONLY),true|true)
-# Build for TARGET_ARCH
 LOCAL_2ND_ARCH_VAR_PREFIX :=
+include $(BUILD_SYSTEM)/module_arch_supported.mk
+
+ifeq ($(my_module_arch_supported),true)
 include $(BUILD_SYSTEM)/shared_library_internal.mk
 endif
 
 ifdef TARGET_2ND_ARCH
-ifneq ($(LOCAL_NO_2ND_ARCH),true)
+
+LOCAL_2ND_ARCH_VAR_PREFIX := $(TARGET_2ND_ARCH_VAR_PREFIX)
+include $(BUILD_SYSTEM)/module_arch_supported.mk
+
+ifeq ($(my_module_arch_supported),true)
 # Build for TARGET_2ND_ARCH
 OVERRIDE_BUILT_MODULE_PATH :=
 LOCAL_BUILT_MODULE :=
@@ -27,11 +32,12 @@ LOCAL_BUILT_MODULE_STEM :=
 LOCAL_INSTALLED_MODULE_STEM :=
 LOCAL_INTERMEDIATE_TARGETS :=
 
-LOCAL_2ND_ARCH_VAR_PREFIX := $(TARGET_2ND_ARCH_VAR_PREFIX)
 include $(BUILD_SYSTEM)/shared_library_internal.mk
+
+endif
+
 LOCAL_2ND_ARCH_VAR_PREFIX :=
 
-endif # LOCAL_NO_2ND_ARCH
 endif # TARGET_2ND_ARCH
 
 my_module_arch_supported :=
