e6e48f67d8fac2b5a29dbfeb1ad40610a451b792
Colin Cross <ccross@android.com>
2014-03-21 12:29:32 -0700

    add support for LOCAL_MULTILIB
    LOCAL_MULTILIB replaces LOCAL_32_BIT_ONLY and
    LOCAL_NO_2ND_ARCH, although both are still supported.
    
    Set LOCAL_MULTILIB := 32 to always build a module 32-bit.
    This is the same as specifying LOCAL_32_BIT_ONLY.
    
    Set LOCAL_MULTILIB := first to always build a module for
    the first architecture (64-bit on a 64-bit target, 32-bit on a
    32-bit target).  This is the same as specifying LOCAL_NO_2ND_ARCH.
    
    Set LOCAL_MULTILIB := both to build for both architectures
    on a mulitlib (64-bit) target.
    
    If LOCAL_MULTILIB is not set libraries will default to "both",
    and executables, packages, and prebuilts will default to building
    for the first architecture if supported by the module, otherwise
    the second.
    
    Executables that set LOCAL_MULTILIB := both must set either
    LOCAL_MODULE_STEM_32 and LOCAL_MODULE_STEM_64 or
    LOCAL_MODULE_PATH_32 and LOCAL_MODULE_PATH_64 to specify how to
    differentiate the install locations of the two versions.
    
    Change-Id: I22ab6aa342b231c307b1d8a86cea4fd91eea39f5

--- core/shared_library.mk
+++ core/shared_library.mk
@@ -10,6 +10,13 @@ $(warning $(LOCAL_MODULE): LOCAL_UNSTRIPPED_PATH for shared libraries is unsuppo
 endif
 endif
 
+include $(BUILD_SYSTEM)/multilib.mk
+
+ifndef my_module_multilib
+# libraries default to building for both architecturess
+my_module_multilib := both
+endif
+
 LOCAL_2ND_ARCH_VAR_PREFIX :=
 include $(BUILD_SYSTEM)/module_arch_supported.mk
 
