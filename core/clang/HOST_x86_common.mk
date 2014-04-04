92d79cbb41d8757ffa165e26e8ae42f4335e85f6
Tim Murray <timmurray@google.com>
2014-04-04 14:38:29 -0700

    Switch host builds to use Clang by default.
    If LOCAL_CLANG is not set to false for a host module, clang will be used instead of gcc.
    
    This also enables the integrated assembler by default for Darwin host builds.
    
    bug 16172793
    
    Change-Id: If7484c5dbcccce7d925bec97bff0a3e4c30e9434

--- core/clang/HOST_x86_common.mk
+++ core/clang/HOST_x86_common.mk
@@ -1,7 +1,11 @@
 # Shared by HOST_x86.mk and HOST_x86_64.mk.
 
 ifeq ($(HOST_OS),darwin)
-# nothing required here yet
+CLANG_CONFIG_x86_DARWIN_HOST_EXTRA_ASFLAGS := \
+  -integrated-as
+
+CLANG_CONFIG_x86_DARWIN_HOST_EXTRA_CFLAGS := \
+  -integrated-as
 endif
 
 ifeq ($(HOST_OS),linux)
