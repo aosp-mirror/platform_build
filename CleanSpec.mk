7e127ebe0bf8753fcbeeac6140916d512322e308
Brian Carlstrom <bdc@google.com>
2014-05-28 14:31:46 -0700

    Move to dalvik.vm.lib.2 to force default to libart
    Bug: 14298175
    Change-Id: Ie4a716dddddc4d339fd4f47033f2c546a1ca118a

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -249,6 +249,9 @@ $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/build.prop)
 # Adding dalvik.vm.dex2oat-flags to eng builds
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/build.prop)
 
+# Rename persist.sys.dalvik.vm.lib.1 to allow new default
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/build.prop)
+
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
