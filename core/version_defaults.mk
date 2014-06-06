7f83b7c75b9e2a2602158065e2aeca91cc0864d4
Bart Sears <bsears@google.com>
2014-06-06 13:34:42 -0700

    MR2.1 - Version 4.4.4.  Here we go!  DO NOT MERGE
    Make is so.
    
    Change-Id: I06b8c8c491e6e355124b6fea41893155ea4c07d0

--- core/version_defaults.mk
+++ core/version_defaults.mk
@@ -41,7 +41,7 @@ ifeq "" "$(PLATFORM_VERSION)"
   # which is the version that we reveal to the end user.
   # Update this value when the platform version changes (rather
   # than overriding it somewhere else).  Can be an arbitrary string.
-  PLATFORM_VERSION := 4.4.3
+  PLATFORM_VERSION := 4.4.4
 endif
 
 ifeq "" "$(PLATFORM_SDK_VERSION)"
