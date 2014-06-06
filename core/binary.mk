7b913ce6fa5030519121757bf5b258efba51cc06
Ying Wang <wangying@google.com>
2014-06-05 19:05:47 -0700

    Support .asm being compiled by yasm targeted for x86.
    Change-Id: Icd6626a082facf920b0e49e2fbe8861e94400552

--- core/binary.mk
+++ core/binary.mk
@@ -744,6 +744,19 @@ endif
 asm_objects := $(asm_objects_S) $(asm_objects_s)
 
 
+# .asm for x86 needs to be compiled with yasm.
+ifeq (x86,$($(my_prefix)$(LOCAL_2ND_ARCH_VAR_PREFIX)ARCH))
+asm_sources_asm := $(filter %.asm,$(my_src_files))
+ifneq ($(strip $(asm_sources_asm)),)
+asm_objects_asm := $(addprefix $(intermediates)/,$(asm_sources_asm:.asm=.o))
+$(asm_objects_asm): $(intermediates)/%.o: $(TOPDIR)$(LOCAL_PATH)/%.asm \
+    $(LOCAL_ADDITIONAL_DEPENDENCIES)
+	$(transform-asm-to-o)
+
+asm_objects += $(asm_objects_asm)
+endif
+endif
+
 ####################################################
 ## Import includes
 ####################################################
