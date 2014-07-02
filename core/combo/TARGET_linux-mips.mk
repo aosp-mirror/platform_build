e68ec719039406bfcc6cd8b1cab4890afbeb66a8
Ying Wang <wangying@google.com>
2014-07-02 14:26:49 -0700

    [DO NOT MERGE] Workaround to fix klp-modular-dev-plus-aosp build.
    Change-Id: I8042b045f20b870de84ea7c4c060c3cbe1cdfe69

--- core/combo/TARGET_linux-mips.mk
+++ core/combo/TARGET_linux-mips.mk
@@ -236,8 +236,7 @@ $(hide) $(PRIVATE_CXX) \
 	$(PRIVATE_LDFLAGS) \
 	$(PRIVATE_TARGET_LIBATOMIC) \
 	$(if $(PRIVATE_LIBCXX),,$(PRIVATE_TARGET_LIBGCC)) \
-	$(if $(filter true,$(PRIVATE_NO_CRT)),,$(PRIVATE_TARGET_CRTEND_SO_O)) \
-	$(PRIVATE_LDLIBS)
+	$(if $(filter true,$(PRIVATE_NO_CRT)),,$(PRIVATE_TARGET_CRTEND_SO_O))
 endef
 
 define $(combo_2nd_arch_prefix)transform-o-to-executable-inner
@@ -263,8 +262,7 @@ $(hide) $(PRIVATE_CXX) -nostdlib -Bdynamic -fPIE -pie \
 	$(PRIVATE_LDFLAGS) \
 	$(PRIVATE_TARGET_LIBATOMIC) \
 	$(if $(PRIVATE_LIBCXX),,$(PRIVATE_TARGET_LIBGCC)) \
-	$(if $(filter true,$(PRIVATE_NO_CRT)),,$(PRIVATE_TARGET_CRTEND_O)) \
-	$(PRIVATE_LDLIBS)
+	$(if $(filter true,$(PRIVATE_NO_CRT)),,$(PRIVATE_TARGET_CRTEND_O))
 endef
 
 define $(combo_2nd_arch_prefix)transform-o-to-static-executable-inner
