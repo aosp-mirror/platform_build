d28feb006605a791c01afe82544a756f90c5f9e9
Ying Wang <wangying@android.com>
2014-07-15 01:22:50 +0000

    Revert "Fix Java detection on some Linux distributions"
    This reverts commit 09040dd8232f8fe588d938e04581ba1648f452f4.
    
    Change-Id: Iafb126a6200e259f4176d903bacf8d3de28c6d97

--- core/find-jdk-tools-jar.sh
+++ core/find-jdk-tools-jar.sh
@@ -2,9 +2,13 @@
 if [ "x$ANDROID_JAVA_HOME" != x ] && [ -e "$ANDROID_JAVA_HOME/lib/tools.jar" ] ; then
     echo $ANDROID_JAVA_HOME/lib/tools.jar
 else
-    JAVAC=$(readlink -f $(which javac))
+    JAVAC=$(which javac)
     if [ -z "$JAVAC" ] ; then
         exit 1
     fi
+    while [ -L "$JAVAC" ] ; do
+        LSLINE=$(ls -l "$JAVAC")
+        JAVAC=$(echo -n "$LSLINE" | sed -e "s/.* -> //")
+    done
     echo $JAVAC | sed -e "s:\(.*\)/bin/javac.*:\\1/lib/tools.jar:"
 fi
