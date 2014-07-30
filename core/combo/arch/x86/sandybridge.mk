2d645ab323deee44ae88271358c36c14eb602551
Varvara Rainchik <varvara.rainchik@intel.com>
2014-07-30 18:21:03 +0400

    Add missing flags to x86 (both 32- and 64-bit) arch variants. Delete x86_64-atom.mk as we don't support 64-bit on old Atom.
    Change-Id: I0b9ab61cd9b840f32c30059cb3ba9704c733c42a
    Signed-off-by: Varvara Rainchik <varvara.rainchik@intel.com>

--- core/combo/arch/x86/sandybridge.mk
+++ core/combo/arch/x86/sandybridge.mk
@@ -2,9 +2,13 @@
 # Generating binaries for SandyBridge processors.
 #
 ARCH_X86_HAVE_SSSE3  := true
+ARCH_X86_HAVE_SSE4   := true
 ARCH_X86_HAVE_SSE4_1 := true
 ARCH_X86_HAVE_SSE4_2 := true
+ARCH_X86_HAVE_AES_NI := true
 ARCH_X86_HAVE_AVX    := true
+ARCH_X86_HAVE_POPCNT := true
+ARCH_X86_HAVE_MOVBE  := false
 
 # CFLAGS for this arch
 arch_variant_cflags := \
