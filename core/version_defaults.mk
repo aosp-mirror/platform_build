e2ff12bf2a6892ec56676b6a845a02e81b372e8c
Justin Koh <justinkoh@google.com>
2014-08-12 13:55:57 -0700

    DO NOT MERGE Bump version string.
    Bump version string for KKWT MR0
    Bug: 16982332
    
    Change-Id: I8636169362ec45891257c39fb47574a6e021a3f0

--- core/version_defaults.mk
+++ core/version_defaults.mk
@@ -41,7 +41,7 @@ ifeq "" "$(PLATFORM_VERSION)"
   # which is the version that we reveal to the end user.
   # Update this value when the platform version changes (rather
   # than overriding it somewhere else).  Can be an arbitrary string.
-  PLATFORM_VERSION := 4.4W
+  PLATFORM_VERSION := 4.4W.1
 endif
 
 ifeq "" "$(PLATFORM_SDK_VERSION)"
