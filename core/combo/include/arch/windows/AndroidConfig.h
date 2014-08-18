032ac32bdbffe0ec7bb75674ce57e15666024bc4
Elliott Hughes <enh@google.com>
2014-08-18 12:18:59 -0700

    Remove unused entries from AndroidConfig.h.
    We should probably try to remove these files completely, but this at
    least takes care of the stuff that's completely obsolete.
    
    Change-Id: Ic71b7b491c119963068294e258dc6afe5a45b40d

--- core/combo/include/arch/windows/AndroidConfig.h
+++ core/combo/include/arch/windows/AndroidConfig.h
@@ -119,13 +119,6 @@
 #endif
 
 /*
- * Define this if you have <sys/sendfile.h>
- */
-#ifdef __CYGWIN__
-#  define  HAVE_SYS_SENDFILE_H 1
-#endif
-
-/*
  * Define this if you build against MSVCRT.DLL
  */
 #ifndef __CYGWIN__
@@ -149,11 +142,6 @@
  * Define this if we have gethostbyname_r().
  */
 /* #define HAVE_GETHOSTBYNAME_R */
-
-/*
- * Define this if we have ioctl().
- */
-/* #define HAVE_IOCTL */
 
 /*
  * Define this if we want to use WinSock.
@@ -207,12 +195,6 @@
 #define HAVE_BACKTRACE 0
 
 /*
- * Defined if we have the cxxabi.h header for demangling C++ symbols.  If
- * not defined, stack crawls will be displayed with raw mangled symbols
- */
-#define HAVE_CXXABI 0
-
-/*
  * Define if tm struct has tm_gmtoff field
  */
 /* #define HAVE_TM_GMTOFF 1 */
@@ -256,11 +238,6 @@
  * sprintf() format string for shared library naming.
  */
 #define OS_SHARED_LIB_FORMAT_STR    "lib%s.dll"
-
-/*
- * type for the third argument to mincore().
- */
-#define MINCORE_POINTER_TYPE unsigned char *
 
 /*
  * The default path separator for the platform
