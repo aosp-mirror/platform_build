781094fa4347eb10d2d55fb75232e19417bda831
Stephen Hines <srhines@google.com>
2014-10-17 17:36:42 -0700

    Remove unnecessary 10.8-specific build rules
    We no longer need gcc for host builds, since those all run through clang. This
    header include, however, triggers errors about SSE intrinsics by replacing
    the more relevant include dirs that we should be using.
    
    Change-Id: I26a949f0109de8e6e2d1f09cb8127be927549cc4

--- core/combo/HOST_darwin-x86_64.mk
+++ core/combo/HOST_darwin-x86_64.mk
@@ -35,11 +35,6 @@ HOST_TOOLCHAIN_ROOT := prebuilts/gcc/darwin-x86/host/i686-apple-darwin-4.2.1
 HOST_TOOLCHAIN_PREFIX := $(HOST_TOOLCHAIN_ROOT)/bin/i686-apple-darwin$(gcc_darwin_version)
 HOST_CC  := $(HOST_TOOLCHAIN_PREFIX)-gcc
 HOST_CXX := $(HOST_TOOLCHAIN_PREFIX)-g++
-ifeq ($(mac_sdk_version),10.8)
-# Mac SDK 10.8 no longer has stdarg.h, etc
-host_toolchain_header := $(HOST_TOOLCHAIN_ROOT)/lib/gcc/i686-apple-darwin$(gcc_darwin_version)/4.2.1/include
-HOST_GLOBAL_CFLAGS += -isystem $(host_toolchain_header)
-endif
 
 # gcc location for clang; to be updated when clang is updated
 # HOST_TOOLCHAIN_ROOT is a Darwin-specific define
