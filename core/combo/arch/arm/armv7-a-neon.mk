e270873407ef32351458263d56504c6ff0c0f5eb
Ian Rogers <irogers@google.com>
2014-10-28 15:50:02 -0700

    Specify -mcpu=krait for clang.
    Change-Id: I64682cddf57246341c1727ca16c56f7ce0c6fd0a

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -9,7 +9,8 @@ ARCH_ARM_HAVE_NEON              := true
 ifneq (,$(filter cortex-a15 krait denver,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
 	# TODO: krait is not a cortex-a15, we set the variant to cortex-a15 so that
 	#       hardware divide operations are generated. This should be removed and a
-	#       krait CPU variant added to GCC/clang.
+	#       krait CPU variant added to GCC. For clang we specify -mcpu for krait in
+	#       core/clang/arm.mk.
 	arch_variant_cflags := -mcpu=cortex-a15
 
 	# Fake an ARM compiler flag as these processors support LPAE which GCC/clang
