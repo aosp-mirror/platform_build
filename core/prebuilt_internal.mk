2902d4585f2a60a91084e96d00f2ee8d8301bf65
Ying Wang <wangying@google.com>
2014-10-29 15:48:32 -0700

    Add support for prebuilt AARs.
    - You can give a .aar as source file to a prebuilt static Java library
      module. The build system will set up dependencies and rules to extract
      classes.jar and other resource files.
    - To build against a prebuilt AAR module, use:
      LOCAL_STATIC_JAVA_AAR_LIBRARIES := <module names of aar prebuilt AARs>
      The build system will set up rules to merge the library's
      AndroidManifest.xml with the main AndroidManifest.xml, add the AAR's
      resource dirs and link/merge the AAR's classes.jar.
    
    Bug: 18168693
    Change-Id: Ic2c1d20572a93bd98dbc72f8a39e26b459e442c2
    (cherry picked from commit e9dd9f2bfceed3b5f630b0edbe3feb7f34548d8b)

--- core/prebuilt_internal.mk
+++ core/prebuilt_internal.mk
@@ -260,10 +260,26 @@ ifeq ($(LOCAL_IS_HOST_MODULE)$(LOCAL_MODULE_CLASS),JAVA_LIBRARIES)
 # while the deps should be in the common dir, so we make a copy in the common dir.
 # For nonstatic library, $(common_javalib_jar) is the dependency file,
 # while $(common_classes_jar) is used to link.
-common_classes_jar := $(call intermediates-dir-for,JAVA_LIBRARIES,$(LOCAL_MODULE),,COMMON)/classes.jar
-common_javalib_jar := $(dir $(common_classes_jar))javalib.jar
+common_classes_jar := $(intermediates.COMMON)/classes.jar
+common_javalib_jar := $(intermediates.COMMON)/javalib.jar
 
-$(common_classes_jar) : $(my_prebuilt_src_file) | $(ACP)
+$(common_classes_jar) $(common_javalib_jar): PRIVATE_MODULE := $(LOCAL_MODULE)
+
+ifneq ($(filter %.aar, $(my_prebuilt_src_file)),)
+# This is .aar file, archive of classes.jar and Android resources.
+my_src_jar := $(intermediates.COMMON)/aar/classes.jar
+
+$(my_src_jar) : $(my_prebuilt_src_file)
+	$(hide) rm -rf $(dir $@) && mkdir -p $(dir $@)
+	$(hide) unzip -qo -d $(dir $@) $<
+	# Make sure the extracted classes.jar has a new timestamp.
+	$(hide) touch $@
+
+else
+# This is jar file.
+my_src_jar := $(my_prebuilt_src_file)
+endif
+$(common_classes_jar) : $(my_src_jar) | $(ACP)
 	$(transform-prebuilt-to-target)
 
 $(common_javalib_jar) : $(common_classes_jar) | $(ACP)
