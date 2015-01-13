5877c21436816ced9dfc1abade6a9e81c9edb230
Elliott Hughes <enh@google.com>
2015-01-12 16:20:24 -0800

    Remove HAVE_SYMLINKS.
    Change-Id: I685357292af45a048439f0df0ae45f513479841a

--- core/combo/include/arch/target_linux-x86/AndroidConfig.h
+++ core/combo/include/arch/target_linux-x86/AndroidConfig.h
@@ -28,12 +28,6 @@
 #define HAVE_PTHREADS
 
 /*
- * Define this if your platforms implements symbolic links
- * in its filesystems
- */
-#define HAVE_SYMLINKS 1
-
-/*
  * Define if we have <malloc.h> header
  */
 #define HAVE_MALLOC_H
