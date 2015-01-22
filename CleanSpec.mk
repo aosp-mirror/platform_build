c421abfcd549d45bd42f54afa5ec84be0379c8cf
Dianne Hackborn <hackbod@google.com>
2015-01-22 14:40:41 -0800

    Update version to MNC.
    Change-Id: I67a69efa90babcfbc3e83907e46be0833c0ddcdd

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -329,6 +329,11 @@ $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/APPS/*)
 # instead.
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/build.prop)
 
+# On to MNC
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/build.prop)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/*)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/APPS/*)
+
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
