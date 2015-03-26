9ef1170e93314ccb33a09ae4eb93d669c6073cb6
Bart Sears <bsears@google.com>
2015-03-25 21:04:38 -0700

    DO NOT MERGE - Bump version to 5.1.1
    Bug: 19939307
    Change-Id: I069f514fe6d3759aa7dde38627c54f7bed38c1bb

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -309,6 +309,11 @@ $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/build.prop)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/*)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/APPS/*)
 
+# 5.1.1!
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/build.prop)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/*)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/APPS/*)
+
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
