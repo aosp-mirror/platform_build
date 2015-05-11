d760d4676bb50ce74e4cc9a2813c592c189d31c9
Andrew Hsieh <andrewhsieh@google.com>
2015-05-11 06:03:07 +0000

    Revert "Revert "Enable linker -fix-cortex-a53-843419""
    This reverts commit 32e1689684682aadf1a5d5af523ccafae5907572.
    
    Now that prebuilts/gcc/darwin-x86/aarch64/aarch64-linux-android-4.9 is also updated
    
    Change-Id: Id95c1c4cc651c434461655e62b8f23afd56b53e4

--- core/combo/TARGET_linux-arm64.mk
+++ core/combo/TARGET_linux-arm64.mk
@@ -114,6 +114,7 @@ TARGET_GLOBAL_LDFLAGS += \
 			-Wl,--fatal-warnings \
 			-Wl,-maarch64linux \
 			-Wl,--hash-style=gnu \
+			-Wl,--fix-cortex-a53-843419 \
 			$(arch_variant_ldflags)
 
 # Disable transitive dependency library symbol resolving.
