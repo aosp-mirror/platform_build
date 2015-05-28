7607786843d9bb2eb7d29d120adef14e4e7838d3
Clay Murphy <claym@google.com>
2015-05-27 18:37:02 -0700

    Docs: Update link to new Community page location
    Bug: 21277510
    
    Change-Id: I527e4efe264cbcf61bce997bed9086f1cb060c83

--- tools/droiddoc/templates-sac/customizations.cs
+++ tools/droiddoc/templates-sac/customizations.cs
@@ -355,7 +355,7 @@ def:custom_copyright() ?>
 def:custom_footerlinks() ?>
   <p>
     <a href="<?cs var:toroot ?>source/index.html">About Android</a>&nbsp;&nbsp;|&nbsp;
-    <a href="<?cs var:toroot ?>source/community/index.html">Community</a>&nbsp;&nbsp;|&nbsp;
+    <a href="<?cs var:toroot ?>source/community.html">Community</a>&nbsp;&nbsp;|&nbsp;
     <a href="<?cs var:toroot ?>legal.html">Legal</a>&nbsp;&nbsp;|&nbsp;
   </p><?cs 
 /def ?>
