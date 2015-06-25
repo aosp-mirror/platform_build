2a04839aecb1cbdcf5ca9e76ebfce9448eec0e13
Ying Wang <wangying@google.com>
2015-06-25 13:56:53 -0700

    Create userdata.img with real data when SANITIZE_TARGET=address.
    Bug: 21785137
    Change-Id: Ie0c36988759fe07419ad29bab5a71cdd0d992d2a

--- tools/releasetools/build_image.py
+++ tools/releasetools/build_image.py
@@ -31,7 +31,7 @@ import tempfile
 FIXED_SALT = "aee087a5be3b982978c923f566a94613496b417f2af592639bc80d141e34dfe7"
 
 def RunCommand(cmd):
-  """ Echo and run the given command
+  """ Echo and run the given command.
 
   Args:
     cmd: the command represented as a list of strings.
