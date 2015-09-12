2064ebcd580df1665834284c867ffb9ea4107607
Zach Jang <zachjang@google.com>
2015-09-11 17:27:09 -0700

    DO NOT MERGE - Backport of ag/748221 - Security Patch Level in Settings CL#2/3
    b/23946860
    
    Change-Id: I42a2aaf26cf458d1775b11029789330cdd85b3a3

--- core/version_defaults.mk
+++ core/version_defaults.mk
@@ -24,6 +24,7 @@
 #     DEFAULT_APP_TARGET_SDK
 #     BUILD_ID
 #     BUILD_NUMBER
+#     SECURITY_PATCH
 #
 
 # Look for an optional file containing overrides of the defaults,
@@ -65,6 +66,22 @@ ifeq "" "$(PLATFORM_VERSION_CODENAME)"
   # the same as PLATFORM_VERSION_CODENAME or a comma-separated list of additional
   # codenames after PLATFORM_VERSION_CODENAME.
   PLATFORM_VERSION_ALL_CODENAMES := $(PLATFORM_VERSION_CODENAME)
+endif
+
+ifeq "" "$(PLATFORM_SECURITY_PATCH)"
+  # Used to indicate the security patch that has been applied to the device.
+  # Can be an arbitrary string, but must be a single word.
+  #
+  # If there is no $PLATFORM_SECURITY_PATCH set, keep it empty.
+  PLATFORM_SECURITY_PATCH := 2015-11-01
+endif
+
+ifeq "" "$(PLATFORM_BASE_OS)"
+  # Used to indicate the base os applied to the device.
+  # Can be an arbitrary string, but must be a single word.
+  #
+  # If there is no $PLATFORM_BASE_OS set, keep it empty.
+  PLATFORM_BASE_OS :=
 endif
 
 ifeq "" "$(DEFAULT_APP_TARGET_SDK)"
