47ef4c3467e4f895b776fb0e8dbcba8f90a9398c
Zach Jang <zachjang@google.com>
2015-09-11 17:27:09 -0700

    DO NOT MERGE - Backport of ag/748221 - Security Patch Level in Settings CL#2/3
    b/23946860
    
    Change-Id: I42a2aaf26cf458d1775b11029789330cdd85b3a3

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -305,6 +305,9 @@ $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/build.prop)
 # 5.0.2
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/build.prop)
 
+# Added new build props
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/build.prop)
+
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
