63e3b0294f3e4d453bf93d5e437848024dee938a
Colin Cross <ccross@android.com>
2015-09-24 22:14:25 -0700

    Enable color output from gcc and clang
    Force gcc and clang to always output color diagnostics.  Ninja will
    strip the ANSI color codes if it is not running in a terminal.
    
    Bug: 24273983
    Change-Id: Ic9252549bfacf3bbb3f4b9d20f63010fc24ac21d

--- core/clang/config.mk
+++ core/clang/config.mk
@@ -47,6 +47,11 @@ CLANG_CONFIG_EXTRA_CFLAGS += \
 CLANG_CONFIG_EXTRA_CPPFLAGS += \
   -Wno-inconsistent-missing-override
 
+# Force clang to always output color diagnostics.  Ninja will strip the ANSI
+# color codes if it is not running in a terminal.
+CLANG_CONFIG_EXTRA_CFLAGS += \
+  -fcolor-diagnostics
+
 CLANG_CONFIG_UNKNOWN_CFLAGS := \
   -finline-functions \
   -finline-limit=64 \
@@ -70,7 +75,8 @@ CLANG_CONFIG_UNKNOWN_CFLAGS := \
   -Wno-unused-but-set-variable \
   -Wno-unused-local-typedefs \
   -Wunused-but-set-parameter \
-  -Wunused-but-set-variable
+  -Wunused-but-set-variable \
+  -fdiagnostics-color
 
 # Clang flags for all host rules
 CLANG_CONFIG_HOST_EXTRA_ASFLAGS :=
