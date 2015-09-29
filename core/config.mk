d11c18a4b0c7093d3b6253fedc23566f495e7ed0
Stephen Hines <srhines@google.com>
2015-09-28 18:07:21 -0700

    Fix RS NDK usage so we always pull from "current" instead of NDK 8.
    The 8 in this definition refers to NDK release 8, not the API level 8.
    Setting it to "current" is better, since it still contains the relevant
    target API level (9 in our case) for using/building our RS support APIs.
    
    Change-Id: Ib216ea859e137643fba81c39c4b9d0077ec04455

--- core/config.mk
+++ core/config.mk
@@ -708,7 +708,7 @@ endif
 # Set up RS prebuilt variables for compatibility library
 
 RS_PREBUILT_CLCORE := prebuilts/sdk/renderscript/lib/$(TARGET_ARCH)/librsrt_$(TARGET_ARCH).bc
-RS_PREBUILT_LIBPATH := -L prebuilts/ndk/8/platforms/android-9/arch-$(TARGET_ARCH)/usr/lib
+RS_PREBUILT_LIBPATH := -L prebuilts/ndk/current/platforms/android-9/arch-$(TARGET_ARCH)/usr/lib
 RS_PREBUILT_COMPILER_RT := prebuilts/sdk/renderscript/lib/$(TARGET_ARCH)/libcompiler_rt.a
 
 # API Level lists for Renderscript Compat lib.
