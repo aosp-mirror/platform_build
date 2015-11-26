b15149cb2f53c86724f0bcacc464fa1ff034ea84
Colin Cross <ccross@android.com>
2015-11-26 14:08:34 -0800

    Use libstdc++ for ijar
    For some reason ijar won't build against libc++ for TARGET_BUILD_APPS
    builds, but does build with libstdc++.
    
    (cherry picked from commit 718bab6aecfe7e23a6afa1361da64c8e0c832705)
    
    Bug: 25904002
    Change-Id: I1de103918faa5bb574af6f12cc954e7fed44dc62

--- tools/ijar/Android.mk
+++ tools/ijar/Android.mk
@@ -6,9 +6,11 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_CPP_EXTENSION := cc
+LOCAL_CPP_EXTENSION := .cc
 LOCAL_SRC_FILES := classfile.cc ijar.cc zip.cc
 LOCAL_CFLAGS += -Wall
 LOCAL_SHARED_LIBRARIES := libz-host
 LOCAL_MODULE := ijar
+# libc++ is not supported for TARGET_BUILD_APPS builds
+LOCAL_CXX_STL := libstdc++
 include $(BUILD_HOST_EXECUTABLE)
