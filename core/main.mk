18a141cc8c06a30266adeb4c33c99b8c0b348a8e
Daniel Micay <danielmicay@gmail.com>
2015-12-16 16:42:49 -0500

    set ro.adb.secure=1 for user builds
    In user builds, adb without authentication is always disabled. However,
    it doesn't work with the defaults because authentication doesn't work
    without having ro.adb.secure=1 set. It makes sense to offer builds with
    no adb but not builds with adb present in an unusable state (especially
    as the default).
    
    Bug: 22220212
    Bug: 21862859
    Change-Id: Ic22300a70586fd57692ac59d6873ab16dde70919

--- core/main.mk
+++ core/main.mk
@@ -360,6 +360,10 @@ ifneq (,$(user_variant))
   # Target is secure in user builds.
   ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
 
+  ifeq ($(user_variant),user)
+    ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
+  endif
+
   ifeq ($(user_variant),userdebug)
     # Pick up some extra useful tools
     tags_to_install += debug
