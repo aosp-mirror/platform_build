ae74914ed9b3c894be63d4636c88a8cbb1360e7b
Lingfeng Yang <lfy@google.com>
2016-02-05 10:40:21 -0800

    Allow smooth upgrade of AVD's from older API's.
    If we are using an old API for the emulator
    (API 19 for example) and then upgrade the AVD
    to API 23 through the AVD settings, we get a bunch
    of crashes in the TelephonyProvider,
    as it is looking for a file that does not exist
    that concerns upgrading from old "apn"'s.
    
    This CL adds this file to the files to be copied
    to /etc in the system image.
    
    BUG: b.android.com/200390
    BUG=27273457
    
    RELEASE NOTE: Fixed telephony service crashes on startup when upgrading from an old API.
    
    Change-Id: I227713880bf20541c759aab588c00197ac7aebfd
    (cherry picked from commit e5083159401edf1df5109b56d1aedbbb739584e6)

--- target/product/sdk_base.mk
+++ target/product/sdk_base.mk
@@ -61,6 +61,7 @@ PRODUCT_PACKAGE_OVERLAYS := development/sdk_overlay
 
 PRODUCT_COPY_FILES := \
 	device/generic/goldfish/data/etc/apns-conf.xml:system/etc/apns-conf.xml \
+	device/sample/etc/old-apns-conf.xml:system/etc/old-apns-conf.xml \
 	frameworks/base/data/sounds/effects/camera_click.ogg:system/media/audio/ui/camera_click.ogg \
 	frameworks/base/data/sounds/effects/VideoRecord.ogg:system/media/audio/ui/VideoRecord.ogg \
 	frameworks/base/data/sounds/effects/VideoStop.ogg:system/media/audio/ui/VideoStop.ogg \
