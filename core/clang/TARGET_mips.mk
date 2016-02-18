cc1a0fdde16478a199e5385ca47023c026dc51e7
Duane Sand <duane.1.sand@gmail.com>
2016-02-18 13:35:12 -0800

    Enable asan/ubsan for mips
    Change-Id: I1f3c89d6ca08d109e7d2a6d32448ff248a98daac

--- core/clang/TARGET_mips.mk
+++ core/clang/TARGET_mips.mk
@@ -65,3 +65,7 @@ $(clang_2nd_arch_prefix)RS_TRIPLE_CFLAGS :=
 RS_COMPAT_TRIPLE := mipsel-linux-android
 
 $(clang_2nd_arch_prefix)TARGET_LIBPROFILE_RT := $(LLVM_RTLIB_PATH)/libclang_rt.profile-mipsel-android.a
+
+# Address sanitizer clang config
+$(clang_2nd_arch_prefix)ADDRESS_SANITIZER_RUNTIME_LIBRARY := libclang_rt.asan-mips-android
+$(clang_2nd_arch_prefix)ADDRESS_SANITIZER_LINKER := /system/bin/linker_asan
--- core/clang/TARGET_mips64.mk
+++ core/clang/TARGET_mips64.mk
@@ -64,3 +64,7 @@ RS_TRIPLE_CFLAGS :=
 RS_COMPAT_TRIPLE := mips64el-linux-android
 
 TARGET_LIBPROFILE_RT := $(LLVM_RTLIB_PATH)/libclang_rt.profile-mips64el-android.a
+
+# Address sanitizer clang config
+$(clang_2nd_arch_prefix)ADDRESS_SANITIZER_RUNTIME_LIBRARY := libclang_rt.asan-mips64-android
+$(clang_2nd_arch_prefix)ADDRESS_SANITIZER_LINKER := /system/bin/linker_asan64
