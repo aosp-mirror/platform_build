6c6c51aa794e88c32389c24589ded0ab5e378d6a
Andreas Gampe <agampe@google.com>
2016-03-10 15:07:27 -0800

    Revert "Don't preopt on eng builds."
    This reverts commit 7effde0048bcce6084e00bf29ff3bea4f34d1599.
    
    Bug: 25801231
    Bug: 26794212

--- core/dex_preopt.mk
+++ core/dex_preopt.mk
@@ -21,13 +21,9 @@ DEX_PREOPT_DEFAULT ?= true
 
 # The default values for pre-opting: always preopt PIC.
 # Conditional to building on linux, as dex2oat currently does not work on darwin.
-# Conditional to building a non-eng build, as it currently negatively affects
-# some development workflows.
-ifneq ($(TARGET_BUILD_VARIANT),eng)
-  ifeq ($(HOST_OS),linux)
-    WITH_DEXPREOPT_PIC ?= true
-    WITH_DEXPREOPT ?= true
-  endif
+ifeq ($(HOST_OS),linux)
+  WITH_DEXPREOPT_PIC ?= true
+  WITH_DEXPREOPT ?= true
 endif
 
 # $(1): the .jar or .apk to remove classes.dex
