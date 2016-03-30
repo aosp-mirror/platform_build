8e121663e6d2878e340be257c03c6060182117de
Andreas Gampe <agampe@google.com>
2016-03-30 12:18:34 -0700

    Build: Switch eng build boot dexopt to verify-at-runtime
    To have faster turnaround.
    
    Bug: 26794212
    Change-Id: I1d7877ec8c9cfff62bf14cbd23a790ee7a0b2734

--- target/product/core_minimal.mk
+++ target/product/core_minimal.mk
@@ -118,9 +118,17 @@ PRODUCT_COPY_FILES += \
     system/core/rootdir/etc/public.libraries.android.txt:system/etc/public.libraries.txt
 
 # Different dexopt types for different package update/install times.
+# On eng builds, make "boot" reasons do pure JIT for faster turnaround.
+ifeq (eng,$(TARGET_BUILD_VARIANT))
+    PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+        pm.dexopt.first-boot=verify-at-runtime \
+        pm.dexopt.boot=verify-at-runtime
+else
+    PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+        pm.dexopt.first-boot=interpret-only \
+        pm.dexopt.boot=verify-profile
+endif
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    pm.dexopt.first-boot=interpret-only \
-    pm.dexopt.boot=verify-profile \
     pm.dexopt.install=interpret-only \
     pm.dexopt.bg-dexopt=speed-profile \
     pm.dexopt.ab-ota=speed-profile \
