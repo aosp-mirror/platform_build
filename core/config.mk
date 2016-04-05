a3184dc2aecf79eb60658eea032e41a5cb091fa6
Shinichiro Hamaji <hamaji@google.com>
2016-04-05 17:34:51 +0900

    Fix dependencies of protoc with nanopb-c plugin
    Bug: 27954979
    Change-Id: Ic06b5288b092d8ea5225516069845bfc2ffa9cd1

--- core/config.mk
+++ core/config.mk
@@ -512,6 +512,10 @@ else
 BREAKPAD_GENERATE_SYMBOLS := false
 endif
 PROTOC := $(HOST_OUT_EXECUTABLES)/aprotoc$(HOST_EXECUTABLE_SUFFIX)
+NANOPB_SRCS := external/nanopb-c/generator/protoc-gen-nanopb \
+    $(wildcard external/nanopb-c/generator/*.py \
+               external/nanopb-c/generator/google/*.py \
+               external/nanopb-c/generator/proto/*.py)
 DBUS_GENERATOR := $(HOST_OUT_EXECUTABLES)/dbus-binding-generator
 MKBOOTFS := $(HOST_OUT_EXECUTABLES)/mkbootfs$(HOST_EXECUTABLE_SUFFIX)
 MINIGZIP := $(HOST_OUT_EXECUTABLES)/minigzip$(HOST_EXECUTABLE_SUFFIX)
