aeadf41e69cf2d2d4fe2c79fdf44e82cc6966d7d
Dan Willemsen <dwillemsen@google.com>
2016-05-09 14:53:22 -0700

    Remove -mcpu=cortex-a15 when adding -mcpu=krait
    This is not necessary, since the later -mcpu=krait will override the
    earlier -mcpu=cortex-a15, but is cleaner (and matches Soong).
    
    Change-Id: I18301faf8a5cb5364617b3008bc8af461b7884f8

--- core/clang/arm.mk
+++ core/clang/arm.mk
@@ -4,12 +4,6 @@ CLANG_CONFIG_arm_EXTRA_ASFLAGS :=
 
 CLANG_CONFIG_arm_EXTRA_CFLAGS :=
 
-ifneq (,$(filter krait,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
-  # Android's clang support's krait as a CPU whereas GCC doesn't. Specify
-  # -mcpu here rather than the more normal core/combo/arch/arm/armv7-a-neon.mk.
-  CLANG_CONFIG_arm_EXTRA_CFLAGS += -mcpu=krait -mfpu=neon-vfpv4
-endif
-
 CLANG_CONFIG_arm_EXTRA_CPPFLAGS :=
 
 CLANG_CONFIG_arm_EXTRA_LDFLAGS :=
@@ -30,6 +24,15 @@ CLANG_CONFIG_arm_UNKNOWN_CFLAGS := \
   -fno-strict-volatile-bitfields \
   -fno-tree-copy-prop \
   -fno-tree-loop-optimize
+
+ifneq (,$(filter krait,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
+  # Android's clang support's krait as a CPU whereas GCC doesn't. Specify
+  # -mcpu here rather than the more normal core/combo/arch/arm/armv7-a-neon.mk.
+  CLANG_CONFIG_arm_EXTRA_CFLAGS += -mcpu=krait -mfpu=neon-vfpv4
+
+  # This isn't really unknown, but allows us to only set -mcpu=krait
+  CLANG_CONFIG_arm_UNKNOWN_CFLAGS += -mcpu=cortex-a15
+endif
 
 define subst-clang-incompatible-arm-flags
   $(subst -march=armv5te,-march=armv5t,\
