ad61a3d01f1d2d26134764331019a44ac0ad659d
Daniel Micay <danielmicay@gmail.com>
2016-05-17 00:40:52 -0400

    disable unpriv perf by default in user{,debug} builds
    (Cherry picked from commit f2ac912b19ccbcc15d5176ec52c99744810adce7)
    
    Bug: 29054680
    
    Change-Id: If04bc63fc366006cde842a50b7c764f89d7449a6
    (cherry picked from commit c20df7057dbd386b37a668850e11059db5a49430)

--- core/main.mk
+++ core/main.mk
@@ -316,6 +316,7 @@ tags_to_install :=
 ifneq (,$(user_variant))
   # Target is secure in user builds.
   ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
+  ADDITIONAL_DEFAULT_PROPERTIES += security.perf_harden=1
 
   ifeq ($(user_variant),userdebug)
     # Pick up some extra useful tools
