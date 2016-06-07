8740e9df19a67e11c231596a9aa16f464679a414
Alex Klyubin <klyubin@google.com>
2016-06-07 13:18:57 -0700

    APK Signature Scheme v2 APK verifier.
    This adds the ApkVerifier class which verifies APKs using APK
    Signature Scheme v2 only. In a follow-up commit this class will be
    extended to verify APKs using JAR signature scheme when necessary.
    
    The APK verifier is designed to not just verify an APK, but also
    report errors, warnings, and information about signers in a structured
    way, to enable tools to surface this information to users in various
    ways.
    
    Bug: 27461702
    Change-Id: I10c6ba436021d86b6dbf6d3cf44494652adacb66

--- tools/apksigner/core/src/com/android/apksigner/core/internal/util/MessageDigestSink.java
+++ tools/apksigner/core/src/com/android/apksigner/core/internal/util/MessageDigestSink.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.apksigner.core.internal.apk.v2;
+package com.android.apksigner.core.internal.util;
 
 import com.android.apksigner.core.util.DataSink;
 
