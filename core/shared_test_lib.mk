e1c6deab102020c3212be38210759cc42b77f9da
Dan Willemsen <dwillemsen@google.com>
2016-06-22 00:27:54 -0700

    Finish refactoring tests to NATIVE_TESTS
    Now that the source trees all use NATIVE_TESTS for intermediate files
    and generated sources, make it a requirement.
    
    Change-Id: Id5718fabe63f6e8dde7981a6f0f5bd89e0ec7ee5

--- core/shared_test_lib.mk
+++ core/shared_test_lib.mk
@@ -1,10 +1,1 @@
-#############################################
-## A thin wrapper around BUILD_SHARED_LIBRARY
-## Common flags for native tests are added.
-#############################################
-
 $(error BUILD_SHARED_TEST_LIBRARY is obsolete)
-
-include $(BUILD_SYSTEM)/target_test_internal.mk
-
-include $(BUILD_SHARED_LIBRARY)
