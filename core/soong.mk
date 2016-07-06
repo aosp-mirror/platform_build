f7bc2e8f22a0a5847b17dc72b2dff124fe666436
Tim Becker <tjbecker@google.com>
2016-07-06 16:32:01 -0700

    Fix discrepancy in SANITIZE_TARGET format with soong.
    core/soong.mk expected SANITIZE_TARGET to be comma separated,
    while the rest of the build files expected it to be space
    separated.
    
    Change-Id: I179054118ed5c422f9ae2d69afb2f4ad3a71cb7b

--- core/soong.mk
+++ core/soong.mk
@@ -39,8 +39,8 @@ $(SOONG_VARIABLES): FORCE
 	echo '    "Brillo": $(if $(BRILLO),true,false),'; \
 	echo '    "Malloc_not_svelte": $(if $(filter true,$(MALLOC_SVELTE)),false,true),'; \
 	echo '    "Allow_missing_dependencies": $(if $(TARGET_BUILD_APPS)$(filter true,$(SOONG_ALLOW_MISSING_DEPENDENCIES)),true,false),'; \
-	echo '    "SanitizeHost": [$(if $(SANITIZE_HOST),"$(subst $(comma),"$(comma)",$(SANITIZE_HOST))")],'; \
-	echo '    "SanitizeDevice": [$(if $(SANITIZE_TARGET),"$(subst $(comma),"$(comma)",$(SANITIZE_TARGET))")],'; \
+	echo '    "SanitizeHost": [$(if $(SANITIZE_HOST),"$(subst $(space),"$(comma)",$(SANITIZE_HOST))")],'; \
+	echo '    "SanitizeDevice": [$(if $(SANITIZE_TARGET),"$(subst $(space),"$(comma)",$(SANITIZE_TARGET))")],'; \
 	echo '    "HostStaticBinaries": $(if $(strip $(BUILD_HOST_static)),true,false),'; \
 	echo ''; \
 	echo '    "DeviceName": "$(TARGET_DEVICE)",'; \
