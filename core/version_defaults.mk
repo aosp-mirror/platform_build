cf740e61c738e5a7b274cc9663cdc377987cbbf6
Zach Jang <zachjang@google.com>
2016-07-19 13:20:45 -0700

    Updating security string to 2016-09-01 to platform and CTS in preparation for 2016 September OTA
    b/30224278
    
    Change-Id: I8032d58d2965fcf2c412a098cfa1d1e4c0add01d
    
    Fixing Merge Conflict ag/1237712 directly into mnc-dr1.6-release

--- core/version_defaults.mk
+++ core/version_defaults.mk
@@ -73,7 +73,7 @@ ifeq "" "$(PLATFORM_SECURITY_PATCH)"
   # Can be an arbitrary string, but must be a single word.
   #
   # If there is no $PLATFORM_SECURITY_PATCH set, keep it empty.
-  PLATFORM_SECURITY_PATCH := 2016-08-05
+  PLATFORM_SECURITY_PATCH := 2016-09-01
 endif
 
 ifeq "" "$(PLATFORM_BASE_OS)"
@@ -95,22 +95,6 @@ ifeq "" "$(DEFAULT_APP_TARGET_SDK)"
   else
     DEFAULT_APP_TARGET_SDK := $(PLATFORM_VERSION_CODENAME)
   endif
-endif
-
-ifeq "" "$(PLATFORM_SECURITY_PATCH)"
-  # Used to indicate the security patch that has been applied to the device.
-  # Can be an arbitrary string, but must be a single word.
-  #
-  # If there is no $PLATFORM_SECURITY_PATCH set, keep it empty.
-  PLATFORM_SECURITY_PATCH := 2016-04-01
-endif
-
-ifeq "" "$(PLATFORM_BASE_OS)"
-  # Used to indicate the base os applied to the device.
-  # Can be an arbitrary string, but must be a single word.
-  #
-  # If there is no $PLATFORM_BASE_OS set, keep it empty.
-  PLATFORM_BASE_OS :=
 endif
 
 ifeq "" "$(BUILD_ID)"
