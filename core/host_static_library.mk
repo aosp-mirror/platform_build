70e0ac6bc1f5473f83af73f513e0ea8fe89f8ee8
Colin Cross <ccross@android.com>
2016-09-02 13:20:52 -0700

    Remove HOST_PREFER_32_BIT
    HOST_PREFER_32_BIT was used during the switch to 64-bit host tools to
    keep the SDK building as 32-bit, but is never set any more.
    
    Change-Id: I874f89c7d1e9cd8c9d4c879048d81a4362ce4dce

--- core/host_static_library.mk
+++ core/host_static_library.mk
@@ -6,12 +6,8 @@ include $(BUILD_SYSTEM)/multilib.mk
 
 ifndef LOCAL_MODULE_HOST_ARCH
 ifndef my_module_multilib
-ifeq ($(HOST_PREFER_32_BIT),true)
-my_module_multilib := 32
-else
 # libraries default to building for both architecturess
 my_module_multilib := both
-endif
 endif
 endif
 
--- core/soong.mk
+++ core/soong.mk
@@ -53,7 +53,6 @@ $(SOONG_VARIABLES): FORCE
 	echo '    "Schedboost": $(if $(strip $(ENABLE_SCHEDBOOST)),true,false),'; \
 	echo '    "Binder32bit": $(if $(BINDER32BIT),true,false),'; \
 	echo '    "DevicePrefer32BitExecutables": $(if $(filter true,$(TARGET_PREFER_32_BIT_EXECUTABLES)),true,false),'; \
-	echo '    "HostPrefer32BitExecutables": $(if $(filter true,$(HOST_PREFER_32_BIT)),true,false),'; \
 	echo '    "UseGoma": $(if $(filter-out false,$(USE_GOMA)),true,false),'; \
 	echo ''; \
 	echo '    "DeviceName": "$(TARGET_DEVICE)",'; \
