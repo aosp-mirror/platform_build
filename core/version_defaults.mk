d6340945436ba154a692f9c7656bdb9cc2ccbb90
Akshay Thakker <aksh@google.com>
2016-09-30 12:44:58 -0700

    Set N-MR1 to version 7.1.1.
    N-MR1 is based on NDR. It has the same API level (25) but includes a few
    more bug fixes and is going out to all devices. Hence 7.1.1.
    
    Bug: 31859390
    Change-Id: Iaf864b183e90e1ee1503e24cc99ead504c67f147

--- core/version_defaults.mk
+++ core/version_defaults.mk
@@ -43,7 +43,7 @@ ifeq "" "$(PLATFORM_VERSION)"
   # which is the version that we reveal to the end user.
   # Update this value when the platform version changes (rather
   # than overriding it somewhere else).  Can be an arbitrary string.
-  PLATFORM_VERSION := 7.1
+  PLATFORM_VERSION := 7.1.1
 endif
 
 ifeq "" "$(PLATFORM_SDK_VERSION)"
