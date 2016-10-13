3c51c2bc487e6dd68fe586aba01ceb5f55e44671
Jerry Zhang <zhangjerry@google.com>
2016-10-12 18:00:36 -0700

    Remove automatic adding of adb into default.prop
    Default.prop can interfere with UsbDeviceManager's management of usb state.
    Adb is now enabled for userdebug and eng builds directly from UsbDeviceManager.
    
    Bug: 31814300
    Test: Manually verified that logcat is not kicked
    Change-Id: I69c998c0dd184f9440cc236d82cc2f25f36e8734

--- tools/post_process_props.py
+++ tools/post_process_props.py
@@ -33,16 +33,6 @@ def mangle_build_prop(prop):
 # Put the modifications that you need to make into the /default.prop into this
 # function. The prop object has get(name) and put(name,value) methods.
 def mangle_default_prop(prop):
-  # If ro.debuggable is 1, then enable adb on USB by default
-  # (this is for userdebug builds)
-  if prop.get("ro.debuggable") == "1":
-    val = prop.get("persist.sys.usb.config")
-    if "adb" not in val:
-      if val == "":
-        val = "adb"
-      else:
-        val = val + ",adb"
-      prop.put("persist.sys.usb.config", val)
   # UsbDeviceManager expects a value here.  If it doesn't get it, it will
   # default to "adb". That might not the right policy there, but it's better
   # to be explicit.
