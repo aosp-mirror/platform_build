bab0fa6928438e3a0bdf916120296208bb191f4c
Dan Willemsen <dwillemsen@google.com>
2016-11-18 14:05:39 -0800

    Add basic VNDK support in Make
    Add BOARD_VNDK_VERSION and LOCAL_USE_VNDK to specify the version of the
    VNDK that will be used globally, and whether to use the VNDK on a module
    basis.
    
    If the board is using the VNDK:
    
    * LOCAL_COPY_HEADERS may only be used by modules defining LOCAL_USE_VNDK
    * LOCAL_USE_VNDK modules will compile against the NDK headers and stub
      libraries, but continue to use the platform libc++.
    * LOCAL_USE_VNDK modules will not have the global includes like
      system/core/include, but it will use device-specific kernel headers.
    
    This change does not attempt to enforce any linking constraints, that
    will come in a later patch.
    
    Test: out/build-aosp_arm.ninja is identical before/after
    Change-Id: Icce65d4974f085093d500b5b2516983788fe2905

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -47,6 +47,7 @@ $(SOONG_VARIABLES): FORCE
 	echo '    "DeviceCpuVariant": "$(TARGET_CPU_VARIANT)",'; \
 	echo '    "DeviceAbi": ["$(TARGET_CPU_ABI)", "$(TARGET_CPU_ABI2)"],'; \
 	echo '    "DeviceUsesClang": $(if $(USE_CLANG_PLATFORM_BUILD),$(USE_CLANG_PLATFORM_BUILD),false),'; \
+	echo '    "DeviceVndkVersion": "$(BOARD_VNDK_VERSION)",'; \
 	echo ''; \
 	echo '    "DeviceSecondaryArch": "$(TARGET_2ND_ARCH)",'; \
 	echo '    "DeviceSecondaryArchVariant": "$(TARGET_2ND_ARCH_VARIANT)",'; \
