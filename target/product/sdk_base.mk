b9a8e208b1467818956abca8f9b548da0ec8c10e
Grigory Dzhavadyan <grigoryj@google.com>
2016-12-02 17:50:35 -0800

    Add screenrecord to master (emulator)
    Change-Id: Idda25f9bd0195f608efb20a0b16e277ba09a18bd
    (cherry picked from commit c3de1a6cca058a67924f06be21bce092d502a846)

--- target/product/sdk_base.mk
+++ target/product/sdk_base.mk
@@ -38,6 +38,7 @@ PRODUCT_PACKAGES := \
 	OpenWnn \
 	Protips \
 	rild \
+	screenrecord \
 	SdkSetup \
 	SmokeTest \
 	SmokeTestApp \
@@ -56,6 +57,29 @@ PRODUCT_PACKAGES += \
 	audio.primary.goldfish \
 	audio.r_submix.default \
 	local_time.default
+
+# CDD mandates following codecs
+PRODUCT_PACKAGES += \
+    libstagefright_soft_aacdec \
+    libstagefright_soft_aacenc \
+    libstagefright_soft_amrdec \
+    libstagefright_soft_amrnbenc \
+    libstagefright_soft_amrwbenc \
+    libstagefright_soft_avcdec \
+    libstagefright_soft_avcenc \
+    libstagefright_soft_flacenc \
+    libstagefright_soft_g711dec \
+    libstagefright_soft_gsmdec \
+    libstagefright_soft_hevcdec \
+    libstagefright_soft_mp3dec \
+    libstagefright_soft_mpeg2dec \
+    libstagefright_soft_mpeg4dec \
+    libstagefright_soft_mpeg4enc \
+    libstagefright_soft_opusdec \
+    libstagefright_soft_rawdec \
+    libstagefright_soft_vorbisdec \
+    libstagefright_soft_vpxdec \
+    libstagefright_soft_vpxenc
 
 PRODUCT_PACKAGE_OVERLAYS := development/sdk_overlay
 
