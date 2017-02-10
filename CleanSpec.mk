0a0732aa57f541ae609bc93f81a7d2b40ad78e08
Simran Basi <sbasi@google.com>
2017-02-10 15:40:43 -0800

    Update CleanSpec.mk for the common testcase directories.
    Fix an issue with incremental builds and the new common
    testcase directories.
    
    Bug: None
    Test: Removed https://android-review.googlesource.com/#/c/234478/
          then ran `m cts-platform-version-check` then reapplied the
          change to verify the failure. Then applied this new change
          to verify the problem was resolved.
    
    Change-Id: I77529021e852af35571fb99537d87100010b47af
    (cherry picked from commit 7fe57cfdcdc086871300d37cf6e80bc94f85b5ee)
    Merged-In: I77529021e852af35571fb99537d87100010b47af

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -374,6 +374,9 @@ $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/APPS/*)
 # $(PRODUCT_OUT)/recovery/root/sdcard goes from symlink to folder.
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/recovery/root/sdcard)
 
+$(call add-clean-step, rm -rf $(HOST_OUT_TESTCASES))
+$(call add-clean-step, rm -rf $(TARGET_OUT_TESTCASES))
+
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
