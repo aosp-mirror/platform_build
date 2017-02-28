41408b0e1a9a8e38f040980dc1c3288d5a90ec09
Richard Uhler <ruhler@google.com>
2017-02-28 17:05:12 +0000

    Don't include patch information for dex preopt.
    Because we always compile pic, patch information is not needed.
    
    Test: phone boots.
    Bug: 33192586
    Change-Id: I46a6db713e0ce21300e3f4b86bd771a68eeed0c4

--- core/dex_preopt_libart_boot.mk
+++ core/dex_preopt_libart_boot.mk
@@ -72,7 +72,7 @@ $($(my_2nd_arch_prefix)DEFAULT_DEX_PREOPT_BUILT_IMAGE_FILENAME) : $(LIBART_TARGE
 		--instruction-set-variant=$($(PRIVATE_2ND_ARCH_VAR_PREFIX)DEX2OAT_TARGET_CPU_VARIANT) \
 		--instruction-set-features=$($(PRIVATE_2ND_ARCH_VAR_PREFIX)DEX2OAT_TARGET_INSTRUCTION_SET_FEATURES) \
 		--android-root=$(PRODUCT_OUT)/system \
-		--include-patch-information --runtime-arg -Xnorelocate --compile-pic \
+		--runtime-arg -Xnorelocate --compile-pic \
 		--no-generate-debug-info --generate-build-id \
 		--multi-image --no-inline-from=core-oj.jar \
 		$(PRODUCT_DEX_PREOPT_BOOT_FLAGS) $(GLOBAL_DEXPREOPT_FLAGS) $(COMPILED_CLASSES_FLAGS) $(ART_BOOT_IMAGE_EXTRA_ARGS)
