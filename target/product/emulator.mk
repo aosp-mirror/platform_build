318905fff4a8fa951b9a824ff08b8aa7878aa0dd
bohu <bohu@google.com>
2017-03-06 16:15:59 -0800

    Emulator: Enable sensors hal support for hidl passthrough mode.
    BUG: 35990227
    Change-Id: Ia534f87727d59e5497ce46ed413aff739e5cf701

--- target/product/emulator.mk
+++ target/product/emulator.mk
@@ -65,6 +65,10 @@ PRODUCT_PACKAGES += \
 	android.hardware.keymaster@3.0-impl \
 	android.hardware.keymaster@3.0-service
 
+PRODUCT_PACKAGES += \
+	android.hardware.sensors@1.0-impl \
+	android.hardware.sensors@1.0-service
+
 # camera service treble disable until all backwards compat is complete
 PRODUCT_PROPERTY_OVERRIDES += \
     camera.disable_treble=1
