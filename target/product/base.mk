7d4944da37eba0803def007d64377dabfded234e
Michael Wright <michaelwr@google.com>
2017-03-14 19:03:08 -0700

    Remove inputflinger from the builds.
    Some day... some day...
    
    Test: builds
    Change-Id: I1a0390c7bfa15f7590a869ba5e12e112ec510602

--- target/product/base.mk
+++ target/product/base.mk
@@ -45,9 +45,6 @@ PRODUCT_PACKAGES += \
     incident \
     incident_report \
     input \
-    input.evdev.default \
-    inputflinger \
-    inputflingerhost \
     javax.obex \
     libandroid \
     libandroid_runtime \
@@ -66,7 +63,6 @@ PRODUCT_PACKAGES += \
     libeffects \
     libinput \
     libinputflinger \
-    libinputflingerhost \
     libiprouteutil \
     libjnigraphics \
     libldnhncr \
