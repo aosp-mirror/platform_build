93cae68532962c00902a37ec342fab4a262447af
Jiyong Park <jiyong@google.com>
2017-04-13 11:29:24 +0900

    Change ld.config.txt to a module
    ld.config.txt is changed to a module. So, installing it via
    PRODUCT_PACKAGES intead of PRODUCT_COPY_FILES.
    
    Bug: 37138083
    Test: sailfish boots
    Change-Id: I8689b7c090901d4f157c5da8a67326c97373ed29

--- target/product/core_minimal.mk
+++ target/product/core_minimal.mk
@@ -59,6 +59,7 @@ PRODUCT_PACKAGES += \
     gatekeeperd \
     keystore \
     keystore.default \
+    ld.config.txt \
     ld.mc \
     libaaudio \
     libOpenMAXAL \
@@ -131,9 +132,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     system/core/rootdir/etc/public.libraries.android.txt:system/etc/public.libraries.txt
-
-PRODUCT_COPY_FILES += \
-    system/core/rootdir/etc/ld.config.txt:system/etc/ld.config.txt
 
 # Different dexopt types for different package update/install times.
 # On eng builds, make "boot" reasons do pure JIT for faster turnaround.
