0845cb5c9815ea504926404b8bbaebce6c19ab35
Jiyong Park <jiyong@google.com>
2017-08-22 10:23:59 +0900

    add llndk|vndksp.libraries.txt to vndk_package
    Bug: 64882323
    Test: build 2017 pixel devices. check the files are under /system/etc.
    
    Merged-In: I488427f796b770bb2d151cfa49f73d323cafbc9f
    Change-Id: I488427f796b770bb2d151cfa49f73d323cafbc9f
    (cherry picked from commit 686aff5b91207ff68b114a6a61840a48ad27fccb)

--- target/product/vndk/Android.mk
+++ target/product/vndk/Android.mk
@@ -100,7 +100,9 @@ LOCAL_MODULE := vndk_package
 LOCAL_REQUIRED_MODULES := \
     $(addsuffix .vendor,$(VNDK_CORE_LIBRARIES)) \
     $(addsuffix .vendor,$(VNDK_SAMEPROCESS_LIBRARIES)) \
-    $(LLNDK_LIBRARIES)
+    $(LLNDK_LIBRARIES) \
+    llndk.libraries.txt \
+    vndksp.libraries.txt
 
 include $(BUILD_PHONY_PACKAGE)
 endif # BOARD_VNDK_VERSION is set
