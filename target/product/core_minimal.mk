ab631023b912eaef8d7abdb746a9a63adeb28ed5
Przemyslaw Szczepaniak <pszczepaniak@google.com>
2018-01-17 17:12:46 +0000

    Revert "Remove org.apache.http.legacy from bootclasspath"
    This reverts commit fa7f2ad5b3807f1dac35162310bb3fbccf71f079.
    
    Reason for revert: b/72084844
    
    Change-Id: Ib89fee567f66a0cdf270a0b41e39d38d295ab7e0
    (cherry picked from commit a5fb762b0313eba6b67bc99087948f7be5b39a8b)

--- target/product/core_minimal.mk
+++ target/product/core_minimal.mk
@@ -115,6 +115,7 @@ PRODUCT_BOOT_JARS := \
     telephony-common \
     voip-common \
     ims-common \
+    org.apache.http.legacy.boot \
     android.hidl.base-V1.0-java \
     android.hidl.manager-V1.0-java
 
