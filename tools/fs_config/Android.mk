55d0104f1b729fa76c4beef4e1b0a1539b43daf7
Tom Cherry <tomcherry@google.com>
2018-02-08 13:15:21 -0800

    Place TARGET_FS_CONFIG_GEN passwd/group files in /vendor/etc
    These entries are vendor provided and belong on the /vendor partition.
    
    Bug: 27999086
    Test: end to end user/group check via config.fs and getpwnam, etc.
    Change-Id: I9a5d56da594bf0d04de2b9ce7fd7d9a8151d4682

--- tools/fs_config/Android.mk
+++ tools/fs_config/Android.mk
@@ -261,6 +261,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := passwd
 LOCAL_MODULE_CLASS := ETC
+LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_SYSTEM)/base_rules.mk
 
@@ -279,6 +280,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := group
 LOCAL_MODULE_CLASS := ETC
+LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_SYSTEM)/base_rules.mk
 
