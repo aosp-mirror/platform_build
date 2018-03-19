4619031efed19602ee33fe1691e84d4f0c79a5d1
jdesprez <jdesprez@google.com>
2018-03-19 16:47:22 -0700

    Add loganalysis to cts
    Tradefed depends on Loganalysis so it should also be part
    of cts. Luckily there was no overlap (cts never called a
    part of TF that use loganalysis) so it never failed
    with classNotFound before.
    
    Test: build cts
    Bug: 75979908
    Change-Id: I2542ecd077149f7165aff120d67054943df20b54

--- core/tasks/tools/compatibility.mk
+++ core/tasks/tools/compatibility.mk
@@ -28,6 +28,7 @@ out_dir := $(HOST_OUT)/$(test_suite_name)/android-$(test_suite_name)
 test_artifacts := $(COMPATIBILITY.$(test_suite_name).FILES)
 test_tools := $(HOST_OUT_JAVA_LIBRARIES)/hosttestlib.jar \
   $(HOST_OUT_JAVA_LIBRARIES)/tradefed.jar \
+  $(HOST_OUT_JAVA_LIBRARIES)/loganalysis.jar \
   $(HOST_OUT_JAVA_LIBRARIES)/compatibility-host-util.jar \
   $(HOST_OUT_JAVA_LIBRARIES)/compatibility-host-util-tests.jar \
   $(HOST_OUT_JAVA_LIBRARIES)/compatibility-common-util-tests.jar \
