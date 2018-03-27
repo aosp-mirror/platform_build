d9c8e956aca037ae4f140ab3bf09536396e3a661
Tri Vo <trong@google.com>
2018-03-27 13:49:19 -0700

    Add TOT_SEPOLICY_VERSION = 10000.0
    This variable is equal to the sepolicy version for tip-of-tree development.
    
    Bug: 74622750
    Test: ./build/soong/soong_ui.bash  --dumpvars-mode -vars PLATFORM_SEPOLICY_VERSION
    is unchanged.
    Change-Id: I2a62f7399fe245ec0c681e9a789da6b1b83dd9d8

--- core/config.mk
+++ core/config.mk
@@ -879,11 +879,13 @@ sepolicy_minor_vers := 0
 ifneq ($(sepolicy_major_vers), $(PLATFORM_SDK_VERSION))
 $(error sepolicy_major_version does not match PLATFORM_SDK_VERSION, please update.)
 endif
+
+TOT_SEPOLICY_VERSION := 10000.0
 ifneq (REL,$(PLATFORM_VERSION_CODENAME))
-    sepolicy_major_vers := 10000
-    sepolicy_minor_vers := 0
+    PLATFORM_SEPOLICY_VERSION := $(TOT_SEPOLICY_VERSION)
+else
+    PLATFORM_SEPOLICY_VERSION := $(join $(addsuffix .,$(sepolicy_major_vers)), $(sepolicy_minor_vers))
 endif
-PLATFORM_SEPOLICY_VERSION := $(join $(addsuffix .,$(sepolicy_major_vers)), $(sepolicy_minor_vers))
 sepolicy_major_vers :=
 sepolicy_minor_vers :=
 
@@ -891,6 +893,11 @@ sepolicy_minor_vers :=
 PLATFORM_SEPOLICY_COMPAT_VERSIONS := \
     26.0 \
     27.0
+
+.KATI_READONLY := \
+    PLATFORM_SEPOLICY_COMPAT_VERSIONS \
+    PLATFORM_SEPOLICY_VERSION \
+    TOT_SEPOLICY_VERSION \
 
 # ###############################################################
 # Set up final options.
