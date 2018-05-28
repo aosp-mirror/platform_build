f8d4dc57879f302b253268d61ec0e326d2d88a35
Vishwath Mohan <vishwath@google.com>
2018-05-28 13:38:24 -0700

    Apply CFI include paths to all product configs.
    The current VNDK ABI snapshot expects that CFI is disabled for
    components that either in the include paths (from cfi-common.mk) OR
    enabled directly in the makefile/blueprint. However, on non-arm64
    architectures, the build system was only disabling CFI for vendor
    components in the include paths.
    
    This topic corrects it by (a) removing the include paths check to disable
    CFI for vendor variants, and (b) inheriting the CFI include paths in
    all product configs to ensure that when we update the ABI we're able
    to move to relying exclusively on (PRODUCT_)CFI_INCLUDE_PATHS.
    
    Bug: 66301104
    Test: ABI matches for all architectures.
    Change-Id: I4d845cd9925354d565e06020c479cc3f7ab28f61

--- target/product/aosp_arm64_ab.mk
+++ target/product/aosp_arm64_ab.mk
@@ -27,10 +27,6 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier
 
-# Enable control-flow integrity (CFI) for security sensitive components
-$(call inherit-product, $(SRC_TARGET_DIR)/product/cfi-common.mk)
-$(call inherit-product-if-exists, vendor/google/products/cfi-vendor.mk)
-
 PRODUCT_NAME := aosp_arm64_ab
 PRODUCT_DEVICE := generic_arm64_ab
 PRODUCT_BRAND := Android
