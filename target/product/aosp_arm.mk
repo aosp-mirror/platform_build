45096206e4463ac97c5daba48dcc5a51d2ef0532
bohu <bohu@google.com>
2018-06-10 00:11:30 -0700

    emulator: factor vendor partition out of emulator.mk
    The emulator vendor paritions are better moved out of
    emulator.mk into device/generic/goldfish/vendor.mk
    so that changes in vendor.mk will just concentrate on
    vendor partition.
    
    this cl does not impact real devices, and it does not
    even impact emulator images (it does code refactoring only)
    
    BUG: 110030159
    
    Change-Id: Ic5778cda8ca6ccf7bc2bc2f9028f801cda970143
    Merged-In: Ic5778cda8ca6ccf7bc2bc2f9028f801cda970143
    (cherry picked from commit 667685301600040c0006752bbf7ec0335efa9a56)
    (cherry picked from commit ebbdea824f259eaa218fba6113f61b53a2ed627a)

--- target/product/aosp_arm.mk
+++ target/product/aosp_arm.mk
@@ -21,16 +21,7 @@
 # - VNDK enforcement
 # - compatible property override enabled
 
-PRODUCT_PROPERTY_OVERRIDES += \
-	vendor.rild.libpath=/vendor/lib/libreference-ril.so
-
-# Note: the following lines need to stay at the beginning so that it can
-# take priority  and override the rules it inherit from other mk files
-# see copy file rules in core/Makefile
-PRODUCT_COPY_FILES += \
-    development/sys-img/advancedFeatures.ini.arm:advancedFeatures.ini \
-    prebuilts/qemu-kernel/arm64/3.18/kernel-qemu2:kernel-ranchu-64 \
-    device/generic/goldfish/fstab.ranchu.arm:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.ranchu
+include device/generic/goldfish/arm32-vendor.mk
 
 include $(SRC_TARGET_DIR)/product/full.mk
 
