ebbdea824f259eaa218fba6113f61b53a2ed627a
bohu <bohu@google.com>
2018-06-10 00:11:30 -0700

    emulator: factor vendor partition out of emulator.mk
    The emulator vendor paritions are better moved out of
    emulator.mk into device/generic/goldfish/vendor.mk
    so that changes in vendor.mk will just concentrate on
    vendor partition.
    
    this cl does not impact real devices, and it does not
    even impact emulator images (it does code refactoring only)
    
    BUG: 110030159
    
    Change-Id: Ic5778cda8ca6ccf7bc2bc2f9028f801cda970143
    Merged-In: Ic5778cda8ca6ccf7bc2bc2f9028f801cda970143
    (cherry picked from commit 667685301600040c0006752bbf7ec0335efa9a56)

--- target/product/aosp_x86.mk
+++ target/product/aosp_x86.mk
@@ -21,17 +21,7 @@
 # - VNDK enforcement
 # - compatible property override enabled
 
-PRODUCT_PROPERTY_OVERRIDES += \
-	vendor.rild.libpath=/vendor/lib/libreference-ril.so
-
-# This is a build configuration for a full-featured build of the
-# Open-Source part of the tree. It's geared toward a US-centric
-# build quite specifically for the emulator, and might not be
-# entirely appropriate to inherit from for on-device configurations.
-PRODUCT_COPY_FILES += \
-    development/sys-img/advancedFeatures.ini:advancedFeatures.ini \
-    device/generic/goldfish/data/etc/encryptionkey.img:encryptionkey.img \
-    prebuilts/qemu-kernel/x86_64/4.9/kernel-qemu2:kernel-ranchu-64
+include device/generic/goldfish/x86-vendor.mk
 
 include $(SRC_TARGET_DIR)/product/full_x86.mk
 
