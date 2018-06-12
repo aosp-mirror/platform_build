abf60f5526f7946f914a58be3eb9d8ca49f4850f
Anton Hansson <hansson@google.com>
2018-06-12 15:19:07 +0100

    Dedupe packages in mk's that inherit core_minimal.mk
    The following packages are moved up one level, into core_minimal.mk
    as all its descendants in master share them:
        ContactsProvider
        DefaultContainerService
        libaudiopreprocessing
        libfilterpack_imageproc
        libgabi++
        libstagefright_soft_aacdec
        libstagefright_soft_aacenc
        libstagefright_soft_amrdec
        libstagefright_soft_amrnbenc
        libstagefright_soft_amrwbenc
        libstagefright_soft_avcdec
        libstagefright_soft_avcenc
        libstagefright_soft_flacdec
        libstagefright_soft_flacenc
        libstagefright_soft_g711dec
        libstagefright_soft_gsmdec
        libstagefright_soft_hevcdec
        libstagefright_soft_mp3dec
        libstagefright_soft_mpeg2dec
        libstagefright_soft_mpeg4dec
        libstagefright_soft_mpeg4enc
        libstagefright_soft_opusdec
        libstagefright_soft_rawdec
        libstagefright_soft_vorbisdec
        libstagefright_soft_vpxdec
        libstagefright_soft_vpxenc
        mdnsd
        requestsync
    
    Bug: 80410283
    Test: diffed output of multiproduct_kati with aosp/703385 patched
    Change-Id: Ic38f7aea3e865dbd9f170925214e3dea5233e61e

--- target/product/core_base.mk
+++ target/product/core_base.mk
@@ -21,44 +21,16 @@ PRODUCT_PROPERTY_OVERRIDES := \
     ro.config.alarm_alert=Alarm_Classic.ogg
 
 PRODUCT_PACKAGES += \
-    ContactsProvider \
-    DefaultContainerService \
     Home \
     TelephonyProvider \
     UserDictionaryProvider \
     libandroidfw \
-    libaudiopreprocessing \
     libaudioutils \
-    libfilterpack_imageproc \
-    libgabi++ \
     libmdnssd \
     libnfc_ndef \
     libpowermanager \
     libspeexresampler \
-    libstagefright_soft_aacdec \
-    libstagefright_soft_aacenc \
-    libstagefright_soft_amrdec \
-    libstagefright_soft_amrnbenc \
-    libstagefright_soft_amrwbenc \
-    libstagefright_soft_avcdec \
-    libstagefright_soft_avcenc \
-    libstagefright_soft_flacdec \
-    libstagefright_soft_flacenc \
-    libstagefright_soft_g711dec \
-    libstagefright_soft_gsmdec \
-    libstagefright_soft_hevcdec \
-    libstagefright_soft_mp3dec \
-    libstagefright_soft_mpeg2dec \
-    libstagefright_soft_mpeg4dec \
-    libstagefright_soft_mpeg4enc \
-    libstagefright_soft_opusdec \
-    libstagefright_soft_rawdec \
-    libstagefright_soft_vorbisdec \
-    libstagefright_soft_vpxdec \
-    libstagefright_soft_vpxenc \
     libvariablespeed \
     libwebrtc_audio_preprocessing \
-    mdnsd \
-    requestsync \
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
--- target/product/core_minimal.mk
+++ target/product/core_minimal.mk
@@ -28,22 +28,50 @@ PRODUCT_PACKAGES += \
     com.android.media.remotedisplay \
     com.android.media.remotedisplay.xml \
     CompanionDeviceManager \
+    ContactsProvider \
+    DefaultContainerService \
     DownloadProvider \
     drmserver \
     ethernet-service \
     HTMLViewer \
     idmap \
+    libaudiopreprocessing \
     libdownmix \
     libdrmframework \
     libdrmframework_jni \
     libfilterfw \
+    libfilterpack_imageproc \
+    libgabi++ \
     libgatekeeper \
     libkeystore \
     libneuralnetworks \
+    libstagefright_soft_aacdec \
+    libstagefright_soft_aacenc \
+    libstagefright_soft_amrdec \
+    libstagefright_soft_amrnbenc \
+    libstagefright_soft_amrwbenc \
+    libstagefright_soft_avcdec \
+    libstagefright_soft_avcenc \
+    libstagefright_soft_flacdec \
+    libstagefright_soft_flacenc \
+    libstagefright_soft_g711dec \
+    libstagefright_soft_gsmdec \
+    libstagefright_soft_hevcdec \
+    libstagefright_soft_mp3dec \
+    libstagefright_soft_mpeg2dec \
+    libstagefright_soft_mpeg4dec \
+    libstagefright_soft_mpeg4enc \
+    libstagefright_soft_opusdec \
+    libstagefright_soft_rawdec \
+    libstagefright_soft_vorbisdec \
+    libstagefright_soft_vpxdec \
+    libstagefright_soft_vpxenc \
     libwebviewchromium_loader \
     libwebviewchromium_plat_support \
+    mdnsd \
     MediaProvider \
     PackageInstaller \
+    requestsync \
     StatementService \
     vndk_snapshot_package \
     webview \
