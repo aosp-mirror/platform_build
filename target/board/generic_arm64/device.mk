c2effa871227c5762ac24c1cc60de817eddd1fdd
Kevin Rocard <krocard@google.com>
2018-06-18 15:23:38 -0700

    Add emulator audio policy config in /vendor not /system
    audio_policy_configuration_generic.xml is pushed in /system as the default
    configuration (used if the vendor does not provide one in /vendor or /odm)
    and for the emulator.
    
    As a result it can be parsed by 2.0 and 4.0 audio HALs. The format
    is retro-compatible, but not forward-compatible.
    
    Additionally, the generic config pushed in /system should never be used
    as the vendor should always provide a configuration file describing its
    target architecture. Thus it should not be part of the GSI.
    
    Those files were originally pushed in /system the emulator
    target does not provide its own configuration file.
    The emulator should provide config file instead in /vendor instead.
    
    Bug: 78543061
    Test: sdk_gphone_x86 and aosp_x86_64 emulator has audio
    Test: adb shell cat /vendor/etc/audio_policy_configuration.xml | diff - \
              frameworks/av/services/audiopolicy/config/audio_policy_configuration_generic.xml
    Change-Id: Ice43cb4670a82f4e60fcccc58a636d224f240b5a
    Merged-In: Ice43cb4670a82f4e60fcccc58a636d224f240b5a
    Signed-off-by: Kevin Rocard <krocard@google.com>
    (cherry picked from commit dad3ad6126f0d5a5e5e658a8fa0c146fb94e3f3f)
    (cherry picked from commit b4150586c5ce4ce0d8eedc98d8df7ccd3edd0f59)

--- target/board/generic_arm64/device.mk
+++ target/board/generic_arm64/device.mk
@@ -24,14 +24,6 @@ PRODUCT_COPY_FILES := \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     device/generic/goldfish/camera/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml
 
-# minimal configuration for audio policy.
-PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/audio_policy_configuration_generic.xml:system/etc/audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/primary_audio_policy_configuration.xml:system/etc/primary_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:system/etc/r_submix_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:system/etc/audio_policy_volumes.xml \
-    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:system/etc/default_volume_tables.xml \
-
 # NFC:
 #   Provide default libnfc-nci.conf file for devices that does not have one in
 #   vendor/etc because aosp system image (of aosp_$arch products) is going to
