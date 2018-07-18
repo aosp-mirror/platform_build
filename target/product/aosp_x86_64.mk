a2da9a33614515f0894c0c2785e095f6944cb3ef
SzuWei Lin <szuweilin@google.com>
2018-07-18 18:06:25 +0800

    Enable dyanmic image size for GSI
    The original size number of GSI are fixed size. Change to give
    size by the content with appending 64M bytes space.
    
    Bug: 71970853
    Test: `make systemimage` and check the out system.img
    Change-Id: I345f5e22c2fee7b553fc3337e9bff0ba28f579e1
    Merged-In: I345f5e22c2fee7b553fc3337e9bff0ba28f579e1

--- target/product/aosp_x86_64.mk
+++ target/product/aosp_x86_64.mk
@@ -47,6 +47,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/board/generic_x86_64/device.mk)
 
+# Enable dynamic partition size
+PRODUCT_USE_DYNAMIC_PARTITION_SIZE := true
+
 # Enable A/B update
 AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS := system
