a42e5f976d6d9211b75f5adb216ac02ee5dec7ca
Yifan Hong <elsk@google.com>
2018-07-31 13:30:38 -0700

    Remove USE_LOGICAL_PARTITIONS override
    ... so that its requirements does not fallback
    on the overridden USE_LOGICAL_PARTITIONS, but
    PRODUCT_USE_LOGICAL_PARTITIONS.
    
    Test: m superimage -j
    Change-Id: Ib14aaf83cc8bd4336125d503d11c69a60566530e

--- core/main.mk
+++ core/main.mk
@@ -240,7 +240,7 @@ else
 ADDITIONAL_DEFAULT_PROPERTIES += ro.actionable_compatible_property.enabled=${PRODUCT_COMPATIBLE_PROPERTY}
 endif
 
-ADDITIONAL_PRODUCT_PROPERTIES += ro.boot.logical_partitions=$(USE_LOGICAL_PARTITIONS)
+ADDITIONAL_PRODUCT_PROPERTIES += ro.boot.logical_partitions=$(PRODUCT_USE_LOGICAL_PARTITIONS)
 
 # -----------------------------------------------------------------
 ###
