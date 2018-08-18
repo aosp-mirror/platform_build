50d1c22efb31bdadf52d026eac3cefb74e807190
Colin Cross <ccross@android.com>
2018-08-17 22:52:19 -0700

    Rename general-tests-* variables to general_tests_*
    Make supports dashes in variable names, but underscores are more
    common.  Also use a private variable for general_tests_list_zip,
    and clear local variables at the end of the file.
    
    Test: m checkbuild
    Change-Id: I8fd968d9e674dbc630bdb71ddd375448b6ada5ed

--- core/tasks/general-tests.mk
+++ core/tasks/general-tests.mk
@@ -14,13 +14,13 @@
 
 .PHONY: general-tests
 
-general-tests-zip := $(PRODUCT_OUT)/general-tests.zip
+general_tests_zip := $(PRODUCT_OUT)/general-tests.zip
 # Create an artifact to include a list of test config files in general-tests.
-general-tests-list-zip := $(PRODUCT_OUT)/general-tests_list.zip
-$(general-tests-zip) : .KATI_IMPLICIT_OUTPUTS := $(general-tests-list-zip)
-$(general-tests-zip) : PRIVATE_general_tests_list := $(PRODUCT_OUT)/general-tests_list
-
-$(general-tests-zip) : $(COMPATIBILITY.general-tests.FILES) $(SOONG_ZIP)
+general_tests_list_zip := $(PRODUCT_OUT)/general-tests_list.zip
+$(general_tests_zip) : PRIVATE_general_tests_list_zip := $(general_tests_list_zip)
+$(general_tests_zip) : .KATI_IMPLICIT_OUTPUTS := $(general_tests_list_zip)
+$(general_tests_zip) : PRIVATE_general_tests_list := $(PRODUCT_OUT)/general-tests_list
+$(general_tests_zip) : $(COMPATIBILITY.general-tests.FILES) $(SOONG_ZIP)
 	echo $(sort $(COMPATIBILITY.general-tests.FILES)) | tr " " "\n" > $@.list
 	grep $(HOST_OUT_TESTCASES) $@.list > $@-host.list || true
 	grep $(TARGET_OUT_TESTCASES) $@.list > $@-target.list || true
@@ -28,8 +28,11 @@ $(general-tests-zip) : $(COMPATIBILITY.general-tests.FILES) $(SOONG_ZIP)
 	rm -f $(PRIVATE_general_tests_list)
 	$(hide) grep -e .*.config$$ $@-host.list | sed s%$(HOST_OUT)%host%g > $(PRIVATE_general_tests_list)
 	$(hide) grep -e .*.config$$ $@-target.list | sed s%$(PRODUCT_OUT)%target%g >> $(PRIVATE_general_tests_list)
-	$(hide) $(SOONG_ZIP) -d -o $(general-tests-list-zip) -C $(dir $@) -f $(PRIVATE_general_tests_list)
+	$(hide) $(SOONG_ZIP) -d -o $(PRIVATE_general_tests_list_zip) -C $(dir $@) -f $(PRIVATE_general_tests_list)
 	rm -f $@.list $@-host.list $@-target.list $(PRIVATE_general_tests_list)
 
-general-tests: $(general-tests-zip)
-$(call dist-for-goals, general-tests, $(general-tests-zip) $(general-tests-list-zip))
+general-tests: $(general_tests_zip)
+$(call dist-for-goals, general-tests, $(general_tests_zip) $(general_tests_list_zip))
+
+general_tests_zip :=
+general_tests_list_zip :=
