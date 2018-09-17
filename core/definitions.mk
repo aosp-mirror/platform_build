b73ccb7a25a175396fe451db1ecb33793ad877d9
Dan Willemsen <dwillemsen@google.com>
2018-09-17 14:36:37 -0700

    Fix installclean incremental builds w/veridex
    $(HOST_OUT)/obj/PACKAGING is cleaned via `installclean`, so regular
    built modules should not be depending on it. Explicitly call out the
    dependencies instead.
    
    Test: m; m installclean; m
    Change-Id: I0f318257e4742d6b7bd099dc06958ca8aba0cfb8

--- core/definitions.mk
+++ core/definitions.mk
@@ -2479,10 +2479,21 @@ $(hide) \
   echo "appcompat.sh output:" >> $(PRODUCT_OUT)/appcompat/$(PRIVATE_MODULE).log && \
   art/tools/veridex/appcompat.sh --dex-file=$@ 2>&1 >> $(PRODUCT_OUT)/appcompat/$(PRIVATE_MODULE).log
 endef
+appcompat-files = \
+  art/tools/veridex/appcompat.sh \
+  $(INTERNAL_PLATFORM_HIDDENAPI_WHITELIST) \
+  $(INTERNAL_PLATFORM_HIDDENAPI_LIGHT_GREYLIST) \
+  $(INTERNAL_PLATFORM_HIDDENAPI_DARK_GREYLIST) \
+  $(INTERNAL_PLATFORM_HIDDENAPI_BLACKLIST) \
+  $(HOST_OUT_EXECUTABLES)/veridex \
+  $(TARGET_OUT_COMMON_INTERMEDIATES)/PACKAGING/core_dex_intermediates/classes.dex \
+  $(TARGET_OUT_COMMON_INTERMEDIATES)/PACKAGING/oahl_dex_intermediates/classes.dex
 else
 appcompat-header =
 run-appcompat =
+appcompat-files =
 endif  # HOST_OS == linux
+.KATI_READONLY: appcompat-header run-appcompat appcompat-files
 
 # Remove dynamic timestamps from packages
 #
