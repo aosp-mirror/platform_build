2929cadb3b873583b25d970508883dff59cb97d9
Tom Cherry <tomcherry@google.com>
2018-09-20 11:04:37 -0700

    Allow BOARD_USES_RECOVERY_AS_BOOT without BOARD_BUILD_SYSTEM_ROOT_IMAGE
    The recovery image already contains a copy of first stage init, so we
    can boot unconditionally to the recovery image and instruct first
    stage init whether or not to boot to Android or to recovery.  In this
    case, we need neither the kernel to mount /system as / nor a separate
    partition for recovery, so this change modifies the build scripts to
    allow this combination.
    
    Bug: 114062208
    Test: Boot pixel from recovery to Android with BOARD_USES_RECOVERY_AS_BOOT
          But without BOARD_BUILD_SYSTEM_ROOT_IMAGE
    Change-Id: Icd047afb7f22d2724b3bcaca1aa0c837426dcce7

--- tools/releasetools/sign_target_files_apks.py
+++ tools/releasetools/sign_target_files_apks.py
@@ -597,8 +597,7 @@ def ReplaceOtaKeys(input_tf_zip, output_tf_zip, misc_info):
   if p.returncode != 0:
     raise common.ExternalError("failed to run dumpkeys")
 
-  if (misc_info.get("system_root_image") == "true" and
-      misc_info.get("recovery_as_boot") == "true"):
+  if misc_info.get("recovery_as_boot") == "true":
     recovery_keys_location = "BOOT/RAMDISK/res/keys"
   else:
     recovery_keys_location = "RECOVERY/RAMDISK/res/keys"
