2eea59973b695c77ce1ab85e324a381c3e5bc0be
Dan Willemsen <dwillemsen@google.com>
2018-10-21 19:46:36 -0700

    Remove USE_CLANG_LLD[=false]
    Test: treehugger
    Change-Id: I6dd632e9501f82b22f3f0c084039808e37b37742

--- core/use_lld_setup.mk
+++ core/use_lld_setup.mk
@@ -1,19 +1,14 @@
 #############################################################
-## Set up flags based on USE_CLANG_LLD and LOCAL_USE_CLANG_LLD.
-## Input variables: USE_CLANG_LLD,LOCAL_USE_CLANG_LLD.
+## Set up flags based on LOCAL_USE_CLANG_LLD.
+## Input variables: LOCAL_USE_CLANG_LLD
 ## Output variables: my_use_clang_lld
 #############################################################
 
 # Use LLD by default.
-# Do not use LLD if LOCAL_USE_CLANG_LLD is false or 0,
-# of if LOCAL_USE_CLANG_LLD is not set and USE_CLANG_LLD is 0 or false.
+# Do not use LLD if LOCAL_USE_CLANG_LLD is false or 0
 my_use_clang_lld := true
 ifneq (,$(LOCAL_USE_CLANG_LLD))
   ifneq (,$(filter 0 false,$(LOCAL_USE_CLANG_LLD)))
-    my_use_clang_lld := false
-  endif
-else
-  ifneq (,$(filter 0 false,$(USE_CLANG_LLD)))
     my_use_clang_lld := false
   endif
 endif
