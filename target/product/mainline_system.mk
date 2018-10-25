d994b0dc986fdc98dc3f934a1c3ab836e5436f7f
Anton Hansson <hansson@google.com>
2018-10-25 11:52:54 +0100

    Add list of locales to mainline_system
    Locales not in this list will get stripped.
    
    Bug: 80410283
    Test: lunch mainline_arm64; m
    Change-Id: I90a63e234e2f2ee32da8f9f257248f5ee6354151

--- target/product/mainline_system.mk
+++ target/product/mainline_system.mk
@@ -67,6 +67,8 @@ PRODUCT_PACKAGES_DEBUG += \
 # Enable dynamic partition size
 PRODUCT_USE_DYNAMIC_PARTITION_SIZE := true
 
+PRODUCT_LOCALES := en_US af_ZA am_ET ar_EG as_IN az_AZ be_BY bg_BG bn_BD bs_BA ca_ES cs_CZ da_DK de_DE el_GR en_AU en_CA en_GB en_IN es_ES es_US et_EE eu_ES fa_IR fi_FI fr_CA fr_FR gl_ES gu_IN hi_IN hr_HR hu_HU hy_AM in_ID is_IS it_IT iw_IL ja_JP ka_GE kk_KZ km_KH ko_KR ky_KG lo_LA lt_LT lv_LV km_MH kn_IN mn_MN ml_IN mk_MK mr_IN ms_MY my_MM ne_NP nb_NO nl_NL or_IN pa_IN pl_PL pt_BR pt_PT ro_RO ru_RU si_LK sk_SK sl_SI sq_AL sr_Latn_RS sr_RS sv_SE sw_TZ ta_IN te_IN th_TH tl_PH tr_TR uk_UA ur_PK uz_UZ vi_VN zh_CN zh_HK zh_TW zu_ZA en_XA ar_XB
+
 PRODUCT_NAME := mainline_system
 PRODUCT_BRAND := generic
 
