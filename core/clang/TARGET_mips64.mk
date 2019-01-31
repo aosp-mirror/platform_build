c6d2cf86d1fb79aa545b764818a0bfe217280dec
Logan Chien <loganchien@google.com>
2019-01-31 17:07:50 +0800

    Fix linker_asan[64] apex bootstrap build error
    This commit fixes `linker_asan[64]` apex bootstrap build errors.
    Without this change, `make -j SANITIZE_TARGET=address` results in:
    
      FAILED: ninja: 'out/target/product/walleye/system/bin/linker_asan64',
      needed by 'out/target/product/walleye/system/bin/app_process64',
      missing and no known rule to make it
    
    Test: lunch aosp_walleye-userdebug && make SANITIZE_TARGET=address
    Change-Id: I980a36499cd327db307321fc8e4548925e7d56bf

--- core/clang/TARGET_mips64.mk
+++ core/clang/TARGET_mips64.mk
@@ -6,3 +6,4 @@ TARGET_LIBPROFILE_RT := $(LLVM_RTLIB_PATH)/libclang_rt.profile-mips64el-android.
 
 # Address sanitizer clang config
 $(clang_2nd_arch_prefix)ADDRESS_SANITIZER_LINKER := /system/bin/linker_asan64
+$(clang_2nd_arch_prefix)ADDRESS_SANITIZER_LINKER_FILE := /system/bin/bootstrap/linker_asan64
