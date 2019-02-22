a1bd07977954cca4cb47cbb314aa9920b1508881
Sundong Ahn <sundongahn@google.com>
2019-02-22 12:04:53 +0900

    Change to configstore 1.1
    The configstore 1.2 will be deleted. So changed to configstore 1.1 in
    PRODUCT_PACKAGES and remove configstore 1.2 in vndk.
    
    Bug: 124531214
    Test: build & boot
    Test: adb shell lshal | grep configstore & check configstore 1.2
    Change-Id: I2c9e1f8b5dc7616112d2602cac22059a05e59901

--- target/product/base_vendor.mk
+++ target/product/base_vendor.mk
@@ -29,7 +29,7 @@ PRODUCT_PACKAGES += \
 # Base modules and settings for the vendor partition.
 PRODUCT_PACKAGES += \
     android.hardware.cas@1.1-service \
-    android.hardware.configstore@1.2-service \
+    android.hardware.configstore@1.1-service \
     android.hardware.media.omx@1.0-service \
     dumpsys_vendor \
     fs_config_files_nonsystem \
