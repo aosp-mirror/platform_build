eee1046b5063980f6a69f331704ab9b7419e3797
Jiwen 'Steve' Cai <jwcai@google.com>
2019-03-19 15:59:08 -0700

    Remove android.frameworks.bufferhub@1.0-service
    Disable this for Q. We will add this back once Q branches.
    
    Bug: 128320950
    Bug: 123429755
    Test: Build system
    Change-Id: I1d360c7a8890148be6197092ba149203a72a019d

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -613,6 +613,10 @@ $(call add-clean-step, rm -rf $(HOST_OUT)/framework/vts-tradefed.jar)
 
 # Clean up old location of system_other.avbpubkey
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/etc/security/avb/)
+
+# Clean up bufferhub files
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/bin/hw/android.frameworks.bufferhub@1.0-service)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/etc/init/android.frameworks.bufferhub@1.0-service.rc)
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
