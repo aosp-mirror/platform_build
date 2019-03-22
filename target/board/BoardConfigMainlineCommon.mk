cd2f94407cfa854822325a83bc4643804ab317fb
Anton Hansson <hansson@google.com>
2019-03-22 15:02:08 +0000

    Unset system-as-root for mainline
    This flag is not allowed to be true when dynamic partitions are
    enabled, which mainline devices are expected to do.
    
    Bug: 80410283
    Test: make
    Change-Id: I50c9fe9332ea1490a84484f9311cfe001a083754

--- target/board/BoardConfigMainlineCommon.mk
+++ target/board/BoardConfigMainlineCommon.mk
@@ -5,6 +5,7 @@
 # The generic product target doesn't have any hardware-specific pieces.
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_KERNEL := true
+TARGET_NO_RECOVERY := true
 
 TARGET_USERIMAGES_USE_EXT4 := true
 
@@ -14,10 +15,6 @@ BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 
 TARGET_COPY_OUT_PRODUCT := product
 BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
-
-# system-as-root is mandatory from Android P
-TARGET_NO_RECOVERY := true
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 
 BOARD_VNDK_VERSION := current
 
