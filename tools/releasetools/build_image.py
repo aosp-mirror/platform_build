0d8e89b5f8b8df6a781e6060936ff9ee164999b0
Yifan Hong <elsk@google.com>
2019-04-09 17:03:57 +0000

    Reland "Only assert-max-image-size for static partitions."
    assert-max-image-size doesn't make sense for
    dynamic partitions, as build_image.py always find the
    right size for the output image. Hence:
    
    - build_image.py no longer need to write generated_*_info.txt
      (which contains the size of the image).
    - assert-max-image-size on the static BOARD_*IMAGE_PARTITION_SIZE. If
      a partition is dynamic, that variable isn't set, and
      assert-max-image-size becomes a no-op. If the partition is static,
      assert-max-image-size checks the static partition size as it used
      to be.
    - Fix read-size-of-partitions to use the size of the partition by
      reading the image directly (instead of using generated_*_info.txt).
    
    For devices without AVB, with DAP enabled, and does not have
    RESERVED_SIZE for partitions, because of right sizing, the original
    code always warns about approaching size limits. Since such checks
    doesn't make sense for dynamic partitions, remove them.
    
    Test: builds on device with dynamic partitions
    Test: builds on cuttlefish with DAP enabled (without AVB), no
          more size limit warnings:
    WARNING: out/target/product/vsoc_x86/vendor.img approaching size limit (X now; limit X)
    
    This reverts commit 6e099095d13b02223d224d4ca7cf16b9ed10b83b.
    
    Reason for revert: reland the CL
    
    Bug: 122377935
    Test: build blueline_mainline
    
    Change-Id: Iee594b64e687decff186c0fa60f82b88608febe9
    Merged-In: Iee594b64e687decff186c0fa60f82b88608febe9

--- tools/releasetools/build_image.py
+++ tools/releasetools/build_image.py
@@ -18,10 +18,8 @@
 Builds output_image from the given input_directory, properties_file,
 and writes the image to target_output_directory.
 
-If argument generated_prop_file exists, write additional properties to the file.
-
 Usage:  build_image.py input_directory properties_file output_image \\
-            target_output_directory [generated_prop_file]
+            target_output_directory
 """
 
 from __future__ import print_function
@@ -735,13 +733,8 @@ def GlobalDictFromImageProp(image_prop, mount_point):
   return d
 
 
-def SaveGlobalDict(filename, glob_dict):
-  with open(filename, "w") as f:
-    f.writelines(["%s=%s" % (key, value) for (key, value) in glob_dict.items()])
-
-
 def main(argv):
-  if len(argv) < 4 or len(argv) > 5:
+  if len(argv) != 4:
     print(__doc__)
     sys.exit(1)
 
@@ -751,7 +744,6 @@ def main(argv):
   glob_dict_file = argv[1]
   out_file = argv[2]
   target_out = argv[3]
-  prop_file_out = argv[4] if len(argv) >= 5 else None
 
   glob_dict = LoadGlobalDict(glob_dict_file)
   if "mount_point" in glob_dict:
@@ -790,10 +782,6 @@ def main(argv):
   except:
     logger.error("Failed to build %s from %s", out_file, in_dir)
     raise
-
-  if prop_file_out:
-    glob_dict_out = GlobalDictFromImageProp(image_properties, mount_point)
-    SaveGlobalDict(prop_file_out, glob_dict_out)
 
 
 if __name__ == '__main__':
