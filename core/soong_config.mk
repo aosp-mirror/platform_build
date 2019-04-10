d32e6d13149c3fdfb364885f5c5e602d19a94e5f
Dan Willemsen <dwillemsen@google.com>
2019-04-10 12:25:07 -0700

    Convert oemaids_headers / passwd / group to Soong
    Bug: 118089258
    Test: m oemaids_header_gen oemaids_headers passwd group
    Change-Id: Ie3b92a499b44391e0692da5d9ad067089a62aabb

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -165,6 +165,8 @@ $(call add_json_list, ProductHiddenAPIStubs,             $(PRODUCT_HIDDENAPI_STU
 $(call add_json_list, ProductHiddenAPIStubsSystem,       $(PRODUCT_HIDDENAPI_STUBS_SYSTEM))
 $(call add_json_list, ProductHiddenAPIStubsTest,         $(PRODUCT_HIDDENAPI_STUBS_TEST))
 
+$(call add_json_str,  TargetFSConfigGen,                 $(TARGET_FS_CONFIG_GEN))
+
 $(call add_json_map, VendorVars)
 $(foreach namespace,$(SOONG_CONFIG_NAMESPACES),\
   $(call add_json_map, $(namespace))\
