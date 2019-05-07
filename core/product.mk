google-mirror/build_240522

Add BUILD_RECOVERY_IMAGE and BUILD_BOOT_IMAGE flags
Bug: 123428770
Test: Built system-only image and checked that no boot.img or
recovery.img files where created. Booted the resulting merged build on
device.

Change-Id: I760476502775e68125907c39e66b8665e789a798

core/product.mk
@@ -342,6 +342,8 @@ _product_var_list += PRODUCT_BUILD_ODM_IMAGE
 _product_var_list += PRODUCT_BUILD_CACHE_IMAGE
 _product_var_list += PRODUCT_BUILD_RAMDISK_IMAGE
 _product_var_list += PRODUCT_BUILD_USERDATA_IMAGE
+_product_var_list += PRODUCT_BUILD_RECOVERY_IMAGE
+_product_var_list += PRODUCT_BUILD_BOOT_IMAGE
 _product_var_list += PRODUCT_UPDATABLE_BOOT_MODULES
 _product_var_list += PRODUCT_UPDATABLE_BOOT_LOCATIONS
 

aosp-mirror/platform_build

core/product.mk
@@ -355,9 +355,12 @@ _product_single_value_vars += PRODUCT_BUILD_ODM_IMAGE
 _product_single_value_vars += PRODUCT_BUILD_CACHE_IMAGE
 _product_single_value_vars += PRODUCT_BUILD_RAMDISK_IMAGE
 _product_single_value_vars += PRODUCT_BUILD_USERDATA_IMAGE
+_product_single_value_vars += PRODUCT_BUILD_RECOVERY_IMAGE
+_product_single_value_vars += PRODUCT_BUILD_BOOT_IMAGE
 
 _product_list_vars += PRODUCT_UPDATABLE_BOOT_MODULES
 _product_list_vars += PRODUCT_UPDATABLE_BOOT_LOCATIONS
+
 # Whether the product would like to check prebuilt ELF files.
 _product_single_value_vars += PRODUCT_CHECK_ELF_FILES
 