e08363da378d1f9a5133850a570e4fa2f0cbf74a
Anton Hansson <hansson@google.com>
2019-06-07 13:36:17 +0100

    Re-submit "Make mainline device use apex"
    The build system default was changed to not support apex, but
    we want the mainline device to enable it.
    
    Test: make mainline_system
    Change-Id: I9f29e8354acffb1856dfd8a173b80a3f9324630c

--- target/board/mainline_arm64/BoardConfig.mk
+++ target/board/mainline_arm64/BoardConfig.mk
@@ -34,3 +34,8 @@ AB_OTA_PARTITIONS := system
 
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# Mainline devices support apex
+# TODO: move this to BoardConfigMainlineCommon. Currently, GSI wants flattened
+#       apexes, but emulator wants .apex files, preventing this.
+TARGET_FLATTEN_APEX := false
