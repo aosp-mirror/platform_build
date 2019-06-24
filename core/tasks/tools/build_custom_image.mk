cf821fc963fa74e4875d5d305e24492d9c945044
Tao Bao <tbao@google.com>
2019-06-24 12:39:10 -0700

    Fix an issue in setting up PATH.
    `PATH=$(foreach p,$(INTERNAL_USERIMAGES_BINARY_PATHS),$(p):)$$PATH` will
    produce a space separated string due to `foreach`, if
    $(INTERNAL_USERIMAGES_BINARY_PATHS) has more than one string. We didn't
    hit the issue in the past because $(INTERNAL_USERIMAGES_BINARY_PATHS)
    had contained only one path in practice.
    
    Test: TreeHugger
    Change-Id: I74ef4356668af63d871a81f6bfd4c9324aa1d956

--- core/tasks/tools/build_custom_image.mk
+++ core/tasks/tools/build_custom_image.mk
@@ -151,7 +151,7 @@ $(my_built_custom_image): $(INTERNAL_USERIMAGES_DEPS) $(my_built_modules) $(my_i
 	# Generate the image.
 	$(if $(filter oem,$(PRIVATE_MOUNT_POINT)), \
 	  $(hide) echo "oem.buildnumber=$(BUILD_NUMBER_FROM_FILE)" >> $(PRIVATE_STAGING_DIR)/oem.prop)
-	$(hide) PATH=$(foreach p,$(INTERNAL_USERIMAGES_BINARY_PATHS),$(p):)$$PATH \
+	$(hide) PATH=$(INTERNAL_USERIMAGES_BINARY_PATHS):$$PATH \
 	  build/make/tools/releasetools/build_image.py \
 	  $(PRIVATE_STAGING_DIR) $(PRIVATE_INTERMEDIATES)/image_info.txt $@ $(TARGET_OUT)
 
