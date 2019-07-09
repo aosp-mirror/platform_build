google-mirror/build_240522

Rename product_services to system_ext
Bug: 134359158
Test: build and check if system_ext.img created
Change-Id: I67f2e95dd29eac6a28e07e24ea973d3a134c3bfc

tools/releasetools/sign_target_files_apks.py
@@ -500,8 +500,8 @@ def ProcessTargetFiles(input_tf_zip, output_tf_zip, misc_info,
         "PRODUCT/build.prop",
         "SYSTEM/product/build.prop",
 
-        "PRODUCT_SERVICES/build.prop",
-        "SYSTEM/product_services/build.prop",
+        "SYSTEM_EXT/build.prop",
+        "SYSTEM/ext/build.prop",
 
         "SYSTEM/etc/prop.default",
         "BOOT/RAMDISK/prop.default",

aosp-mirror/platform_build

Test: build and check if system_ext.img is created

tools/releasetools/sign_target_files_apks.py
@@ -501,8 +501,8 @@ def ProcessTargetFiles(input_tf_zip, output_tf_zip, misc_info,
         "PRODUCT/build.prop",
         "SYSTEM/product/build.prop",
 
-        "PRODUCT_SERVICES/build.prop",
-        "SYSTEM/product_services/build.prop",
+        "SYSTEM_EXT/build.prop",
+        "SYSTEM/system_ext/build.prop",
 
         "SYSTEM/etc/prop.default",
         "BOOT/RAMDISK/prop.default",