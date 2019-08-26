bfe4a1c0969cd60a3084b36ba8c600cbade7d9d7
Justin Yun <justinyun@google.com>
2019-08-26 17:09:27 +0900

    Add platform hidl interfaces to mainline_system
    Core variant of some hidl interfaces must be included in
    mainline_system explicitly because they have dependency with external
    modules.
    
    Bug: 138284120
    Test: build mainline_system_arm64
    Change-Id: I1893bb807429dbcacac3db3dcab2a830456775ea

--- target/product/mainline_system.mk
+++ target/product/mainline_system.mk
@@ -73,6 +73,8 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.0 \
     android.hardware.radio@1.1 \
     android.hardware.radio@1.2 \
+    android.hardware.radio@1.3 \
+    android.hardware.radio@1.4 \
     android.hardware.radio.config@1.0 \
     android.hardware.radio.deprecated@1.0 \
     android.hardware.secure_element@1.0 \
