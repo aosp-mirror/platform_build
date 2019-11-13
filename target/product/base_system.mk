8ac6463155a7e26af4212c83e2b20752a97fabca
evitayan <evitayan@google.com>
2019-11-12 17:21:11 -0800

    Add ike.jar to bootclasspath to expose IKE API
    IKE will be a mainline module. This commit adds ike.jar to
    bootpathclass so that IKE API is accessible to apps.
    
    Bug: 143983419
    Test: make update-api && make
    Change-Id: I1dbb249f3109f45ce32c34bcb398108d61bc06cc

--- target/product/base_system.mk
+++ target/product/base_system.mk
@@ -322,10 +322,12 @@ PRODUCT_BOOT_JARS := \
     telephony-common \
     voip-common \
     ims-common \
+    ike \
     updatable-media
-PRODUCT_UPDATABLE_BOOT_MODULES := conscrypt updatable-media
+PRODUCT_UPDATABLE_BOOT_MODULES := conscrypt ike updatable-media
 PRODUCT_UPDATABLE_BOOT_LOCATIONS := \
     /apex/com.android.conscrypt/javalib/conscrypt.jar \
+    /apex/com.android.ipsec/javalib/ike.jar \
     /apex/com.android.media/javalib/updatable-media.jar
 
 
