723ff8274d2d09edb5746969de9357fc766dd5fe
Luke Huang <huangluke@google.com>
2019-12-10 17:55:58 +0800

    Add com.android.cronet APEX and org.chromium.net.cronet to base_system.
    com.android.cronet will be shipped as a mainline
    module in APEX format.
    
    org.chromium.net.cronet is a java_sdk_library used to provide
    Cronet API to apps.
    
    Bug: 139397529
    Test: Built and installed apex on device
    Change-Id: Icff7ded02e360f2df9dfe3ac7185cf7f0034d014

--- target/product/base_system.mk
+++ target/product/base_system.mk
@@ -50,6 +50,7 @@ PRODUCT_PACKAGES += \
     com.android.adbd \
     com.android.apex.cts.shim.v1_prebuilt \
     com.android.conscrypt \
+    com.android.cronet \
     com.android.i18n \
     com.android.ipsec \
     com.android.location.provider \
