5e57a778cdf9ac33ed06ba3b24e3ae662afa309d
Yifan Hong <elsk@google.com>
2020-01-09 16:15:11 -0800

    Disallow VINTF metadata for PRODUCT_COPY_FILES
    VINTF metadata must be checked by defining
    DEVICE_MANIFEST_FILE and friends, instead of being
    copied with PRODUCT_COPY_FILES directly.
    
    This can be worked arond with BUILD_BROKEN_VINTF_PRODUCT_COPY_FILES.
    
    Test: Add the following and see errors:
    
    PRODUCT_COPY_FILES += \
        device/XXX/manifest.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/vintf/manifest.xml \
        device/XXX/manifest.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/vintf/manifest/foo.xml \
        device/XXX/manifest.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/vintf/compatibility_matrix.xml \
        device/XXX/manifest.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/vintf/manifest.xml \
        device/XXX/manifest.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/vintf/manifest/foo.xml \
        device/XXX/manifest.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/vintf/compatibility_matrix.xml \
        device/XXX/manifest.xml:$(TARGET_COPY_OUT_VENDOR)/etc/vintf/manifest.xml \
        device/XXX/manifest.xml:$(TARGET_COPY_OUT_VENDOR)/manifest.xml \
        device/XXX/manifest.xml:$(TARGET_COPY_OUT_VENDOR)/etc/vintf/compatibility_matrix.xml \
        device/XXX/manifest.xml:$(TARGET_COPY_OUT_VENDOR)/compatibility_matrix.xml \
        device/XXX/manifest.xml:$(TARGET_COPY_OUT_ODM)/etc/manifest.xml \
        device/XXX/manifest.xml:$(TARGET_COPY_OUT_ODM)/etc/vintf/manifest.xml \
    
    Test: test again with BUILD_BROKEN_VINTF_PRODUCT_COPY_FILES := true
    
    Bug: 147506249
    
    Change-Id: Id75a9690b3c71fdc9a4e1b4e9be2caa63183cf7c

--- core/board_config.mk
+++ core/board_config.mk
@@ -86,10 +86,11 @@ _board_strip_readonly_list += $(_dynamic_partitions_var_list)
 
 _build_broken_var_list := \
   BUILD_BROKEN_DUP_RULES \
+  BUILD_BROKEN_OUTSIDE_INCLUDE_DIRS \
   BUILD_BROKEN_PREBUILT_ELF_FILES \
   BUILD_BROKEN_TREBLE_SYSPROP_NEVERALLOW \
   BUILD_BROKEN_USES_NETWORK \
-  BUILD_BROKEN_OUTSIDE_INCLUDE_DIRS \
+  BUILD_BROKEN_VINTF_PRODUCT_COPY_FILES \
 
 _build_broken_var_list += \
   $(foreach m,$(AVAILABLE_BUILD_MODULE_TYPES) \
