0b49c03727cab24cb89d3da6b89bc79fdcbef45f
Yo Chiang <yochiang@google.com>
2020-03-17 17:49:49 +0800

    Check ELF prebuilts in PRODUCT_COPY_FILES
    Add a check to ban ELF prebuilts in PRODUCT_COPY_FILES.
    ELF prebuilts should be defined properly by cc_prebuilt_library_shared
    and cc_prebuilt_binary so that the build system can keep track of the
    file dependencies.
    
    To opt in this check, set this option in BoardConfig.mk:
      BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := false
    
    After all devices that have ELF prebuilts in PRODUCT_COPY_FILES are
    tagged with BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true flip
    BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES to default false.
    
    After a device is cleaned up, remove its
    BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true setting.
    
    Bug: 140560012
    Test: Set BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := false
    Test: m # Should report any ELF prebuilt error in PRODUCT_COPY_FILES
    Change-Id: Id75dac9417aed9baa7c5e9388430ff9369cf9b4f

--- core/board_config.mk
+++ core/board_config.mk
@@ -87,6 +87,7 @@ _board_strip_readonly_list += $(_dynamic_partitions_var_list)
 
 _build_broken_var_list := \
   BUILD_BROKEN_DUP_RULES \
+  BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES \
   BUILD_BROKEN_OUTSIDE_INCLUDE_DIRS \
   BUILD_BROKEN_PREBUILT_ELF_FILES \
   BUILD_BROKEN_TREBLE_SYSPROP_NEVERALLOW \
