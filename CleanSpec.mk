09153e244e8e1681b5083c102dec47934c19583d
Jiyong Park <jiyong@google.com>
2020-05-11 16:37:06 +0900

    Remove /system/etc/prop.default
    Contents that were in /system/etc/prop.default are now in
    /system/build.prop.
    
    The content is placed at the top of build.prop to keep the existing
    order between the two files.
    
    Caveat: /prop.default still remains for non-Treble devices.
    
    Bug: 117892318
    Test: TH passes
    Test: inspect  /system/build.prop and check if it has contents from
    the old /system/etc/prop.default file
    
    Change-Id: Ibc74edb88904f953a1e5e6ceef44c461f381c9cf

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -723,6 +723,7 @@ $(call add-clean-step, rm -rf $(HOST_OUT)/vts/*)
 $(call add-clean-step, rm -rf $(HOST_OUT)/framework/vts-tradefed.jar)
 
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/vendor/default.prop)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/etc/prop.default)
 
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
