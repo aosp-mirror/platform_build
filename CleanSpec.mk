b894d4bb6840457da78f562bb4128ba2a4d5018f
Jiyong Park <jiyong@google.com>
2020-05-11 15:53:57 +0900

    Remove /vendor/default.prop
    Contents that were in /vendor/default.prop are now in /vendor/build.prop
    
    The content is placed at the top of build.prop to keep the existing
    order between default.prop and builld.prop.
    
    Bug: 117892318
    Test: TH passes
    Test: inspect  /vendor/build.prop and check if it has contents from
    the old /vendor/default.prop file
    
    Change-Id: Ifa1f1dca3b4f4a5fa87d26b8ee058ee706dc54bb

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -722,6 +722,8 @@ $(call add-clean-step, rm -rf $(HOST_OUT)/framework/vts10-tradefed.jar)
 $(call add-clean-step, rm -rf $(HOST_OUT)/vts/*)
 $(call add-clean-step, rm -rf $(HOST_OUT)/framework/vts-tradefed.jar)
 
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/vendor/default.prop)
+
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
