eac6ed644cafbc592bf1b0e5164b6eb823e1ed76
Dan Shi <dshi@google.com>
2020-05-19 15:32:25 -0700

    Remove vts10 dependencies
    Bug: 149249068
    Test: build
    Change-Id: I56119259add1435215958b51a58bc1b971f27067

--- core/tasks/general-tests.mk
+++ core/tasks/general-tests.mk
@@ -14,12 +14,10 @@
 
 .PHONY: general-tests
 
-# TODO(b/149249068): Remove vts10-tradefed.jar after all VTS tests are converted
 general_tests_tools := \
     $(HOST_OUT_JAVA_LIBRARIES)/cts-tradefed.jar \
     $(HOST_OUT_JAVA_LIBRARIES)/compatibility-host-util.jar \
     $(HOST_OUT_JAVA_LIBRARIES)/vts-tradefed.jar \
-    $(HOST_OUT_JAVA_LIBRARIES)/vts10-tradefed.jar
 
 intermediates_dir := $(call intermediates-dir-for,PACKAGING,general-tests)
 general_tests_zip := $(PRODUCT_OUT)/general-tests.zip
