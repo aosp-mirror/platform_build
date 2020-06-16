google-mirror/build_240522

Mark jni libs as REQUIRED by their app
This makes the dependencies of the jni lib .so appear in
product_target_FILES. Previously, only the jni lib .so
itself appeared, as an effect of being added to the
INSTALLED files of the app.

Bug: 129323707
Test: not yet
Change-Id: Ie391bb45ad7758e51368cabb1ecba5caeae2f09c

core/install_jni_libs_internal.mk
@@ -52,7 +52,8 @@ else ifneq ($(my_jni_shared_libraries),) # not my_embed_jni
   # Do not use order-only dependency, because we want to rebuild the image if an jni is updated.
   $(LOCAL_INSTALLED_MODULE) : $(my_installed_library)
 
-  ALL_MODULES.$(LOCAL_MODULE).INSTALLED += $(my_installed_library)
+  bit_suffix := $(if $(filter %64,$(TARGET_$(my_2nd_arch_prefix)ARCH)),:64,:32)
+  ALL_MODULES.$(LOCAL_MODULE).REQUIRED += $(addsuffix $(bit_suffix),$(LOCAL_JNI_SHARED_LIBRARIES))
 
   # Create symlink in the app specific lib path
   # Skip creating this symlink when running the second part of a target sanitization build.

aosp-mirror/platform_build

Test: compare the output between `m dump-files` and installed-files.txt
Test: noop in terms of build artifacts

core/install_jni_libs_internal.mk
@@ -57,9 +57,9 @@ else ifneq ($(my_jni_shared_libraries),) # not my_embed_jni
   # Make sure the JNI libraries get installed
   my_shared_library_path := $(call get_non_asan_path,\
       $($(my_2nd_arch_prefix)TARGET_OUT$(partition_tag)_SHARED_LIBRARIES))
-  my_installed_library := $(addprefix $(my_shared_library_path)/, $(my_jni_filenames))
 
-  ALL_MODULES.$(my_register_name).INSTALLED += $(my_installed_library)
+  bit_suffix := $(if $(filter %64,$(TARGET_$(my_2nd_arch_prefix)ARCH)),:64,:32)
+  ALL_MODULES.$(my_register_name).REQUIRED_FROM_TARGET += $(addsuffix $(bit_suffix),$(LOCAL_JNI_SHARED_LIBRARIES))
 
   # Create symlink in the app specific lib path
   # Skip creating this symlink when running the second part of a target sanitization build.