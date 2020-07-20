cf1a1894470688f33671c9c1dd3425f575923b1b
Hung-ying Tyan <tyanh@google.com>
2020-07-20 10:15:53 +0800

    Revert "Enable vbmeta_system for GSI targets"
    This reverts commit eb15451b47719411817c05e28d34163b9633ff3e.
    
    Bug: 161503491
    Test: build and check vbmeta_system
    Test: boot GSI on CF Q with aosp/1366956
    Change-Id: I3be4a2d7786903f8d950f45d9b809255876e95ae

--- target/board/generic_x86_64/BoardConfig.mk
+++ target/board/generic_x86_64/BoardConfig.mk
@@ -24,7 +24,6 @@ TARGET_2ND_ARCH_VARIANT := x86_64
 
 TARGET_PRELINK_MODULE := false
 include build/make/target/board/BoardConfigGsiCommon.mk
-ifndef BUILDING_GSI
 include build/make/target/board/BoardConfigEmuCommon.mk
 
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 576716800
@@ -41,4 +40,3 @@ WPA_SUPPLICANT_VERSION      := VER_0_8_X
 WIFI_DRIVER_FW_PATH_PARAM   := "/dev/null"
 WIFI_DRIVER_FW_PATH_STA     := "/dev/null"
 WIFI_DRIVER_FW_PATH_AP      := "/dev/null"
-endif
