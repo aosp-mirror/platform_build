48ae71a6f0ccca27c92568f5e6f10eaf08ef4723
Bill Peckham <bpeckham@google.com>
2020-07-22 17:59:34 -0700

    Export TARGET_ENABLE_MEDIADRM_64 to Soong.
    The change enables conversion of
    hardware/interfaces/drm/1.0/default from make to Soong
    by exporting a variable used in a conditional check by
    the modules in that directory.
    
    Bug: 161571531
    Bug: 123557988
    Test: Build with TARGET_ENABLE_MEDIADRM_64 :=
    Test: Build with TARGET_ENABLE_MEDIADRM_64 := true
    Test: Insepction of out/soong/soong.variables for each
    Test: Inspection of target files package for each
    Test: Verify inclusion of modules in vendor snapshot
    Change-Id: I5737d9d4687e182595a213be000b2e56c7536b8a

--- core/config.mk
+++ core/config.mk
@@ -1227,6 +1227,7 @@ dont_bother_goals := out \
     product-graph dump-products
 
 ifeq ($(CALLED_FROM_SETUP),true)
+include $(BUILD_SYSTEM)/android_soong_config_vars.mk
 include $(BUILD_SYSTEM)/ninja_config.mk
 include $(BUILD_SYSTEM)/soong_config.mk
 endif
