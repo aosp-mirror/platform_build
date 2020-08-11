0116b6cb0e15bcc31b5f38a7224c050ebd17a871
JaeMan <jaeman@google.com>
2020-08-11 02:37:01 +0000

    Rename mainline_system(.*).mk to generic_system(.*).mk
    Rename mainline_system to generic_system in mk files
    to avoid confusion with mainline module.
    
    First, move mainline_system(.*).mk to generic_system(.*).mk
    and make previous mainline_system(.*).mk to just inherit
    generic_system(.*).mk to make alias except mainline_system.mk.
    is just symlink to generic_system.mk.
    
    These aliasing file will be kept remain during migration
    for preventing build breakage and making changes possible
    during migration.
    
    Bug: 160649682
    Test: lunch generic_system_arm64 && m nothing
    Test: lunch generic_system_x86 && m nothing
    Test: lunch generic_system_x86_64 && m nothing
    Test: lunch generic_system_x86_arm && m nothing
    Test: lunch mainline_system_arm64 && m nothing
    Test: lunch mainline_system_x86 && m nothing
    Test: lunch mainline_system_x86_64 && m nothing
    Test: lunch mainline_system_x86_arm && m nothing
    Change-Id: I3609ec2d91b0f08859a36cac75c25876b1db6d6a
    Merged-In: I3609ec2d91b0f08859a36cac75c25876b1db6d6a
    (cherry picked from commit 6f16b5764b14823bbceadb6b1870f3adef4df3c7)

--- target/product/sdk_phone_arm64.mk
+++ target/product/sdk_phone_arm64.mk
@@ -30,7 +30,7 @@ endif
 # All components inherited here go to system image
 #
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/mainline_system.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_system.mk)
 
 #
 # All components inherited here go to system_ext image
