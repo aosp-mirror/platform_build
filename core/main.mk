a9b9e6e5ff978bca000de4bb7ad915420b11a76d
Colin Cross <ccross@android.com>
2020-08-14 17:43:35 -0700

    Merged and dist lists of unused methods from R8
    Merge all the proguard_usage.zip files produced by the R8 rules and
    dist the result.
    
    Bug: 151857441
    Test: m TARGET_BUILD_APPS=DocumentsUI dist
    Change-Id: I7e6d73241478016093a203dc7bd86407ab86a4ac

--- core/main.mk
+++ core/main.mk
@@ -1666,6 +1666,9 @@ ifneq ($(TARGET_BUILD_APPS),)
   $(PROGUARD_DICT_ZIP) : $(apps_only_installed_files)
   $(call dist-for-goals,apps_only, $(PROGUARD_DICT_ZIP))
 
+  $(PROGUARD_USAGE_ZIP) : $(apps_only_installed_files)
+  $(call dist-for-goals,apps_only, $(PROGUARD_USAGE_ZIP))
+
   $(SYMBOLS_ZIP) : $(apps_only_installed_files)
   $(call dist-for-goals,apps_only, $(SYMBOLS_ZIP))
 
@@ -1694,6 +1697,7 @@ else ifeq (,$(TARGET_BUILD_UNBUNDLED))
     $(BUILT_OTATOOLS_PACKAGE) \
     $(SYMBOLS_ZIP) \
     $(PROGUARD_DICT_ZIP) \
+    $(PROGUARD_USAGE_ZIP) \
     $(COVERAGE_ZIP) \
     $(APPCOMPAT_ZIP) \
     $(INSTALLED_FILES_FILE) \
