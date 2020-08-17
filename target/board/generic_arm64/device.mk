2c5f3a7d701186548767f1cfcabebe28672d36a0
Steve Muckle <smuckle@google.com>
2020-08-17 15:06:40 -0700

    add kernels with untrimmed symbols to aosp_arm64
    For development and debugging it is useful to have boot images with all
    kernel symbols exported.
    
    Bug: 163613927
    Merged-In: I6118e5f0fff6e9cbc20ecca6bd362a26a79544b1
    Change-Id: I6118e5f0fff6e9cbc20ecca6bd362a26a79544b1

--- target/board/generic_arm64/device.mk
+++ target/board/generic_arm64/device.mk
@@ -20,6 +20,13 @@ PRODUCT_COPY_FILES += \
     device/google/cuttlefish_kernel/5.4-arm64/kernel-5.4-gz:kernel-5.4-gz \
     device/google/cuttlefish_kernel/5.4-arm64/kernel-5.4-lz4:kernel-5.4-lz4
 
+ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
+PRODUCT_COPY_FILES += \
+    device/google/cuttlefish_kernel/5.4-arm64/kernel-5.4:kernel-5.4-allsyms \
+    device/google/cuttlefish_kernel/5.4-arm64/kernel-5.4-gz:kernel-5.4-gz-allsyms \
+    device/google/cuttlefish_kernel/5.4-arm64/kernel-5.4-lz4:kernel-5.4-lz4-allsyms
+endif
+
 # Adjust the Dalvik heap to be appropriate for a tablet.
 $(call inherit-product-if-exists, frameworks/base/build/tablet-dalvik-heap.mk)
 $(call inherit-product-if-exists, frameworks/native/build/tablet-dalvik-heap.mk)
