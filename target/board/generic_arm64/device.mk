4efd3fa0469b3c1b2b35059ca9e0e03f78577c62
Alistair Delva <adelva@google.com>
2020-09-09 11:25:16 -0700

    Fix copy of allsyms kernels
    Copy the correct source files to outputs. Should fix allsyms boot.img
    files so they have the right kernels.
    
    Bug: 168052462
    Change-Id: I0b5e030eb900c7b1373d2ce22ecb1e7a96fcf552

--- target/board/generic_arm64/device.mk
+++ target/board/generic_arm64/device.mk
@@ -22,9 +22,9 @@ PRODUCT_COPY_FILES += \
 
 ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
 PRODUCT_COPY_FILES += \
-    device/google/cuttlefish_kernel/5.4-arm64/kernel-5.4:kernel-5.4-allsyms \
-    device/google/cuttlefish_kernel/5.4-arm64/kernel-5.4-gz:kernel-5.4-gz-allsyms \
-    device/google/cuttlefish_kernel/5.4-arm64/kernel-5.4-lz4:kernel-5.4-lz4-allsyms
+    device/google/cuttlefish_kernel/5.4-arm64/kernel-5.4-allsyms:kernel-5.4-allsyms \
+    device/google/cuttlefish_kernel/5.4-arm64/kernel-5.4-gz-allsyms:kernel-5.4-gz-allsyms \
+    device/google/cuttlefish_kernel/5.4-arm64/kernel-5.4-lz4-allsyms:kernel-5.4-lz4-allsyms
 endif
 
 # Adjust the Dalvik heap to be appropriate for a tablet.
