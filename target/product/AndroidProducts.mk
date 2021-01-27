29c5d1a4f1aa96d0f8bb989b0994c8fd8f179b5c
Martin Stjernholm <mast@google.com>
2021-01-27 20:21:45 +0000

    Add top level product definitions for module_* devices.
    Test: lunch module_{arm,arm64,x86,x86_64}
      Inspect target arches in the banner
    Bug: 176171716
    Change-Id: Iba7818cfc1385d3f84dce1ef95c2bb8d279926bb

--- target/product/AndroidProducts.mk
+++ target/product/AndroidProducts.mk
@@ -72,7 +72,12 @@ PRODUCT_MAKEFILES := \
 
 endif
 
-PRODUCT_MAKEFILES += $(LOCAL_DIR)/mainline_sdk.mk
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/mainline_sdk.mk \
+    $(LOCAL_DIR)/module_arm.mk \
+    $(LOCAL_DIR)/module_arm64.mk \
+    $(LOCAL_DIR)/module_x86.mk \
+    $(LOCAL_DIR)/module_x86_64.mk \
 
 COMMON_LUNCH_CHOICES := \
     aosp_arm64-eng \
