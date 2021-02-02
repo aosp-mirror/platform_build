4ca749cb1188c63ffb49cb13800a30f483d73942
Martin Stjernholm <mast@google.com>
2021-02-02 11:23:53 +0000

    Merge the module_* device.mk files into their corresponding product mk files.
    To clean up an unnecessary indirection. Instead introduce a common file
    for module products.
    
    Test: lunch module_{arm,arm64,x86,x86_64}
          inspect the build banners
    Change-Id: Ia312431a664e731f5d801ee2671f62f5cd23bd51

--- target/product/module_x86.mk
+++ target/product/module_x86.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-$(call inherit-product, $(SRC_TARGET_DIR)/board/module_x86/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/module_common.mk)
 
 PRODUCT_NAME := module_x86
 PRODUCT_BRAND := Android
