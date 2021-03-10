cf8a05bbfab8b875056c779f5adefde0389d2695
Janis Danisevskis <jdanis@google.com>
2021-03-10 11:16:38 -0800

    Keystore 2.0: Remove keystore and keystore2.enable property.
    * Remove keystore as build target.
    * Remove the default setting of the keystore2.enable property.
    
    Bug: 171563717
    Test: N/A
    Change-Id: I2f29d18650a5e2b819985d202cb0c4002bbe94de

--- target/product/base_system.mk
+++ target/product/base_system.mk
@@ -116,7 +116,6 @@ PRODUCT_PACKAGES += \
     iptables \
     ip-up-vpn \
     javax.obex \
-    keystore \
     keystore2 \
     credstore \
     ld.mc \
@@ -400,9 +399,5 @@ PRODUCT_COPY_FILES += $(call add-to-product-copy-files-if-exists,\
 #       for known dirty objects in the image will be empty.
 PRODUCT_COPY_FILES += $(call add-to-product-copy-files-if-exists,\
     frameworks/base/config/dirty-image-objects:system/etc/dirty-image-objects)
-
-# This property allows enabling Keystore 2.0 selectively for testing.
-# TODO Remove when Keystore 2.0 migration is complete. b/171563717
-PRODUCT_SYSTEM_PROPERTIES += persist.android.security.keystore2.enable=true
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/runtime_libart.mk)
