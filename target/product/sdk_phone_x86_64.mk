918f71acf72132791601819837115bf31d84a42e
Ulya Trafimovich <skvadrik@google.com>
2021-03-24 15:40:02 +0000

    Disable <uses-library> checks for SDK products.
    They lack some libraries (e.g. RadioConfigLib), which makes it
    impossible to translate their module names to library name, so the check
    fails.
    
    Bug: 183339664
    Bug: 132357300
    Bug: 183616134
    Test: treehugger
    Change-Id: I2f5f646f0fba054f9ef6a0e45afa2ecc70d5fcf0

--- target/product/sdk_phone_x86_64.mk
+++ target/product/sdk_phone_x86_64.mk
@@ -59,3 +59,7 @@ PRODUCT_BRAND := Android
 PRODUCT_NAME := sdk_phone_x86_64
 PRODUCT_DEVICE := emulator_x86_64
 PRODUCT_MODEL := Android SDK built for x86_64
+# Disable <uses-library> checks for SDK product. It lacks some libraries (e.g.
+# RadioConfigLib), which makes it impossible to translate their module names to
+# library name, so the check fails.
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
