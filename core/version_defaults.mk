25d421446d63e300b36847ef42414390b94f9084
Jeff Sharkey <jsharkey@android.com>
2021-06-07 13:09:36 -0600

    [DO NOT MERGE] platform/build/make - S is now REL
    Bug: 171506470
    Test: Build
    Change-Id: I98536ff83aab23cfaf5553a57a8a3b8264878847

--- core/version_defaults.mk
+++ core/version_defaults.mk
@@ -93,7 +93,7 @@ PLATFORM_VERSION_LAST_STABLE := 12
 
 # These are the current development codenames, if the build is not a final
 # release build.  If this is a final release build, it is simply "REL".
-PLATFORM_VERSION_CODENAME.SP1A := S
+PLATFORM_VERSION_CODENAME.SP1A := REL
 
 ifndef PLATFORM_VERSION_CODENAME
   PLATFORM_VERSION_CODENAME := $(PLATFORM_VERSION_CODENAME.$(TARGET_PLATFORM_VERSION))
