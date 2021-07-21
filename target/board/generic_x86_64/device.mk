73ac42f9aa517b8b7f83f45f389e8139d0d05bd1
SzuWei Lin <szuweilin@google.com>
2021-07-21 19:16:24 +0800

    Export kernel manifest.xml to dist folder
    Applies on target with generic_arm64 or generic_x86_64.
    
    Bug: 192833360
    Test: lunch gsi_arm64-user; make -j40 dist; check the dist folder
    Test: lunch gsi_x86_64-user; make -j40 dist; check the dist folder
    Change-Id: I58e720f257f31d538066100b9bdcfe793ffb7389

--- target/board/generic_x86_64/device.mk
+++ target/board/generic_x86_64/device.mk
@@ -18,8 +18,15 @@ PRODUCT_COPY_FILES += \
     kernel/prebuilts/5.4/x86_64/kernel-5.4:kernel-5.4 \
     kernel/prebuilts/5.10/x86_64/kernel-5.10:kernel-5.10 \
 
-$(call dist-for-goals, dist_files, kernel/prebuilts/5.4/x86_64/prebuilt-info.txt:kernel/5.4/prebuilt-info.txt)
-$(call dist-for-goals, dist_files, kernel/prebuilts/5.10/x86_64/prebuilt-info.txt:kernel/5.10/prebuilt-info.txt)
+define _output-kernel-info
+$(call dist-for-goals,dist_files,$(1)/prebuilt-info.txt:$(2)/prebuilt-info.txt) \
+$(eval _kernel_manifest_xml := $(word 1,$(wildcard $(1)/manifest_*.xml))) \
+$(if $(_kernel_manifest_xml), \
+  $(call dist-for-goals,dist_files,$(_kernel_manifest_xml):$(2)/manifest.xml))
+endef
+
+$(call _output-kernel-info,kernel/prebuilts/5.4/x86_64,kernel/5.4)
+$(call _output-kernel-info,kernel/prebuilts/5.10/x86_64,kernel/5.10)
 
 ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
 PRODUCT_COPY_FILES += \
