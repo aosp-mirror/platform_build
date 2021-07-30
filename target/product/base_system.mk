4ae4381fcc62913627a5394bc4569be7bcc4a323
Roshan Pius <rpius@google.com>
2021-07-29 21:34:53 -0700

    Add uwb apex/jars to top level makefiles
    Bug: 194906542
    Test: Compiles
    Change-Id: I214f9487fd8cb5da4df24b32d3378fb083b79281

--- target/product/base_system.mk
+++ target/product/base_system.mk
@@ -69,6 +69,7 @@ PRODUCT_PACKAGES += \
     com.android.sdkext \
     com.android.tethering \
     com.android.tzdata \
+    com.android.uwb \
     com.android.wifi \
     ContactsProvider \
     content \
