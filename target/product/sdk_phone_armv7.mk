39b81cdc7a8633424f562416bbd5084034b0104f
Dan Willemsen <dwillemsen@google.com>
2021-09-16 14:23:07 -0700

    Remove old build-tools/platform-tools SDK implementation
    Since those were the only components of the Windows SDK, this removes a
    lot of special casing around that as well.
    
    Bug: 187222815
    Change-Id: I2690aadcf78e9ccad43314c8475893e81a2e2735

--- target/product/sdk_phone_armv7.mk
+++ target/product/sdk_phone_armv7.mk
@@ -49,10 +49,6 @@ $(call inherit-product-if-exists, device/generic/goldfish/arm32-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulator_vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/board/emulator_arm/device.mk)
 
-# Define the host tools and libs that are parts of the SDK.
-$(call inherit-product, sdk/build/product_sdk.mk)
-$(call inherit-product, development/build/product_sdk.mk)
-
 # keep this apk for sdk targets for now
 PRODUCT_PACKAGES += \
     EmulatorSmokeTests
