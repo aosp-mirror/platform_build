f52a20dfc31a348fb2aa0d30095ecaeeabd547c6
Yahan Zhou <yahan@google.com>
2021-10-01 00:18:39 +0000

    Revert "Port sdk_slim_x86_64 from rvc-emu-dev."
    This reverts commit c8dbfb795fb2012183941f458ca17164f15ee94c.
    
    Reason for revert: moved to goldfish
    
    Change-Id: I1e0cd5a04b5a2a3d584f806c4c3ce290725017c7

--- target/product/AndroidProducts.mk
+++ target/product/AndroidProducts.mk
@@ -67,7 +67,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/sdk_phone_armv7.mk \
     $(LOCAL_DIR)/sdk_phone_x86_64.mk \
     $(LOCAL_DIR)/sdk_phone_x86.mk \
-    $(LOCAL_DIR)/sdk_slim_x86_64.mk \
     $(LOCAL_DIR)/sdk_x86_64.mk \
     $(LOCAL_DIR)/sdk_x86.mk \
 
