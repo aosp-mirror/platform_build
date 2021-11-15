a7e1611118bc498ff2ecc49d911a3415f269f7df
Martin Stjernholm <mast@google.com>
2021-11-15 17:15:05 +0000

    Revert "Configure boot image profiles for platform and unbundled ART module"
    This reverts commit 1f925ed13eb0bdec4817e67620adef118e82f28a.
    
    Reason for revert: SH regressions in boot and app startup: b/206152027
    
    Bug: 174746397
    Bug: 206152027
    Change-Id: I60f642b8e4dd8ea04248b835e5b76ffcf01ac300

--- target/product/runtime_libart.mk
+++ target/product/runtime_libart.mk
@@ -75,14 +75,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     hiddenapi-package-whitelist.xml \
 
-ifeq (,$(TARGET_BUILD_UNBUNDLED))
-  # Don't depend on the framework boot image profile in unbundled builds where
-  # frameworks/base may not be present.
-  # TODO(b/179900989): We may not need this check once we stop using full
-  # platform products on the thin ART manifest branch.
-  PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION += frameworks/base/boot/boot-image-profile.txt
-endif
-
 # The dalvik.vm.dexopt.thermal-cutoff property must contain one of the values
 # listed here:
 #
