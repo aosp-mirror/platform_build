57e8549480d6ea1c823529a42e90ec7c7bb09c5a
Remi NGUYEN VAN <reminv@google.com>
2022-01-07 13:07:49 +0900

    Move nearby apex to tethering
    Nearby will be merged together with the tethering/connectivity APEX, so
    that module maintenance efforts can be reduced, and the two codebases
    can evolve the interfaces that they expose to each other over time.
    
    Bug: 189355156
    Test: m
    Change-Id: I51ba3fe8bee34fa226fa5ed31ff9f87ee61fdd63

--- target/product/base_system.mk
+++ target/product/base_system.mk
@@ -61,7 +61,6 @@ PRODUCT_PACKAGES += \
     com.android.media \
     com.android.media.swcodec \
     com.android.mediaprovider \
-    com.android.nearby \
     com.android.os.statsd \
     com.android.permission \
     com.android.resolv \
