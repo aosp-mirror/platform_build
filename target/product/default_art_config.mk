dede1b9f7761967f973c4761093d03ed03c8b95a
Anna Zhuravleva <azhura@google.com>
2022-09-08 13:25:48 +0000

    Add Health Connect library to system server jars.
    Bug: 245722029
    Test: build
    Change-Id: Ice145e15d876b5f37ffb6a071afbb36b106be26a

--- target/product/default_art_config.mk
+++ target/product/default_art_config.mk
@@ -81,6 +81,7 @@ PRODUCT_APEX_SYSTEM_SERVER_JARS := \
     com.android.adservices:service-sdksandbox \
     com.android.appsearch:service-appsearch \
     com.android.art:service-art \
+    com.android.healthconnect:service-healthconnect \
     com.android.media:service-media-s \
     com.android.permission:service-permission \
 
