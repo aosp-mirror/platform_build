12af09d9873be52945aa51a1d657a204d8ecd51a
Sandeep Dhavale <dhavale@google.com>
2023-01-31 18:48:31 +0000

    Add support for different .so alignment based on lunch target
    This allows for us to use different alignement (PAGE_SIZE) for
    .so files with signapk based on lunch target.
    
    Bug: 266136830
    Test: build and boot
    
    Change-Id: I62ca49dda1691bb53532b3e659c384dcf404a5f7
    Signed-off-by: Sandeep Dhavale <dhavale@google.com>

--- tools/signapk/src/com/android/signapk/SignApk.java
+++ tools/signapk/src/com/android/signapk/SignApk.java
@@ -675,6 +675,15 @@ class SignApk {
         }
     }
 
+    private static int getStoredEntryDataAlignmentForSo(String targetProduct) {
+
+        /* Special TARGET_PRODUCT with 16K page size */
+        if (targetProduct != null && targetProduct.toLowerCase().contains("_16k")) {
+            return 16384;
+        }
+        /* default alignment otherwise */
+        return 4096;
+    }
     /**
      * Returns the multiple (in bytes) at which the provided {@code STORED} entry's data must start
      * relative to start of file or {@code 0} if alignment of this entry's data is not important.
@@ -687,7 +696,7 @@ class SignApk {
         if (entryName.endsWith(".so")) {
             // Align .so contents to memory page boundary to enable memory-mapped
             // execution.
-            return 16384;
+            return getStoredEntryDataAlignmentForSo(System.getenv("TARGET_PRODUCT"));
         } else {
             return defaultAlignment;
         }
