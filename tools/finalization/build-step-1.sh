d98c43c8dda1a56408ae7782f8306872cd24f38a
Alex Buynytskyy <alexbuy@google.com>
2023-04-11 16:47:35 +0000

    Script updates.
    - add a mode to disable finalization steps if they were already merged,
    - add the extension version into commit message.
    
    Bug: 243966946
    Test: local run
    Change-Id: I51505e5d5715b56f3b385d714e1aabaae06f58ed

--- tools/finalization/build-step-1.sh
+++ tools/finalization/build-step-1.sh
@@ -6,8 +6,11 @@ function finalize_main_step1() {
     local top="$(dirname "$0")"/../../../..
     source $top/build/make/tools/finalization/environment.sh
 
-    # Build finalization artifacts.
-    source $top/build/make/tools/finalization/finalize-aidl-vndk-sdk-resources.sh
+    if [ "$FINAL_STATE" = "unfinalized" ] ; then
+        # Build finalization artifacts.
+        # source $top/build/make/tools/finalization/finalize-aidl-vndk-sdk-resources.sh
+        echo "Build finalization artifacts."
+    fi;
 }
 
 finalize_main_step1
