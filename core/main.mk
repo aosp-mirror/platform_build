54403316c0a0aaac8f3e8a7355112217a260c296
Jeongik Cha <jeongik@google.com>
2023-05-23 06:39:37 +0900

    Replace touch -a with comment
    Bug: 278060169
    Test: m
    Change-Id: Ie5ad1b5c6cf335cc3de641fd76a76bfa5f3f79cb

--- core/main.mk
+++ core/main.mk
@@ -47,9 +47,9 @@ $(KATI_obsolete_var BUILD_HOSTNAME,Use BUILD_HOSTNAME_FROM_FILE instead)
 $(KATI_obsolete_var FILE_NAME_TAG,https://android.googlesource.com/platform/build/+/master/Changes.md#FILE_NAME_TAG)
 
 $(BUILD_NUMBER_FILE):
-	touch -a $@
+	# empty rule to prevent dangling rule error for a file that is written by soong_ui
 $(BUILD_HOSTNAME_FILE):
-	touch -a $@
+	# empty rule to prevent dangling rule error for a file that is written by soong_ui
 
 .KATI_RESTAT: $(BUILD_NUMBER_FILE)
 .KATI_RESTAT: $(BUILD_HOSTNAME_FILE)
