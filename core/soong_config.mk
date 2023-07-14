google-mirror/build_240522

Add a new variable for deprecating VNDK
Add a new flag variable for deprecating VNDK. This variable should be
passed to Soong, so it can change behavior based on the flag.

Bug: 290157355
Test: build succeeded with DEPRECATE_VNDK=true
Change-Id: I3a9d1041411ad24ff42f76506a8f929e364b868d

core/soong_config.mk
@@ -318,6 +318,8 @@ $(call add_json_list, BuildVersionTags,    $(BUILD_VERSION_TAGS))
 $(call add_json_str, ReleaseVersion,    $(_RELEASE_VERSION))
 $(call add_json_list, ReleaseAconfigValueSets,    $(RELEASE_ACONFIG_VALUE_SETS))
 
+$(call add_json_bool, DeprecateVndk, $(filter true,$(DEPRECATE_VNDK)))
+
 $(call json_end)
 
 $(file >$(SOONG_VARIABLES).tmp,$(json_contents))

aosp-mirror/platform_build

Test: build succeeded with KEEP_VNDK=false

core/soong_config.mk
@@ -318,6 +318,8 @@ $(call add_json_list, BuildVersionTags,    $(BUILD_VERSION_TAGS))
 $(call add_json_str, ReleaseVersion,    $(_RELEASE_VERSION))
 $(call add_json_list, ReleaseAconfigValueSets,    $(RELEASE_ACONFIG_VALUE_SETS))
 
+$(call add_json_bool, KeepVndk, $(filter true,$(KEEP_VNDK)))
+
 $(call json_end)
 
 $(file >$(SOONG_VARIABLES).tmp,$(json_contents))