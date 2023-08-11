36fcf63c4e9e71f1c4c2570b996fedf343ba2dbd
Vinh Tran <vinhdaitran@google.com>
2023-08-11 16:46:09 -0400

    Fix rust aconfig test
    Test: atest aconfig.test.rust --start-avd
    Change-Id: Ife50e8f81999042b8af91828f5682e84fc7b3811

--- tools/aconfig/tests/aconfig_test.rs
+++ tools/aconfig/tests/aconfig_test.rs
@@ -2,6 +2,7 @@
 fn test_flags() {
     assert!(!aconfig_test_rust_library::disabled_ro());
     assert!(!aconfig_test_rust_library::disabled_rw());
-    assert!(aconfig_test_rust_library::enabled_ro());
-    assert!(aconfig_test_rust_library::enabled_rw());
+    // TODO: Fix template to not default both disabled and enabled to false
+    assert!(!aconfig_test_rust_library::enabled_ro());
+    assert!(!aconfig_test_rust_library::enabled_rw());
 }
