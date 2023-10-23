d9199eb8edd464ca056dc0a14e60dd79cd5a8103
Harshit Mahajan <harshitmahajan@google.com>
2023-10-23 20:23:14 +0000

    Revert "[CrashRecovery] Add module to BCP and SSCP"
    Revert submission 24444827-crashrecovery
    
    Reason for revert: Since the module is not behind any flags, it is getting included in 24Q1 Beta release.
    
    Reverted changes: /q/submissionid:24444827-crashrecovery
    Bug: b/305826937
    Change-Id: I39dfab62897d907023720b594d6929397403d314

--- target/product/base_system.mk
+++ target/product/base_system.mk
@@ -53,7 +53,6 @@ PRODUCT_PACKAGES += \
     com.android.btservices \
     com.android.configinfrastructure \
     com.android.conscrypt \
-    com.android.crashrecovery \
     com.android.devicelock \
     com.android.extservices \
     com.android.healthfitness \
