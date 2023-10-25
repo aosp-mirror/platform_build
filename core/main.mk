f71c221f402a419da493efb6bba6e89b0c8ace47
Justin Yun <justinyun@google.com>
2023-10-25 13:18:48 +0900

    Remove ro.product.vndk.version property
    Product modules no longer use vndk libraries but install them to
    /product/lib[64].
    
    Test: lunch cf_x86_64_phone-next-userdebug; m
    Test: atest GtsVndkDependencyTestCases
    Bug: 299867815
    Bug: 302255959
    Change-Id: I67614457c83f0f18eba7dc1d8259efeb5795807b

--- core/main.mk
+++ core/main.mk
@@ -335,19 +335,6 @@ ADDITIONAL_VENDOR_PROPERTIES += \
     ro.build.ab_update=$(AB_OTA_UPDATER)
 endif
 
-# Set ro.product.vndk.version to know the VNDK version required by product
-# modules. It uses the version in PRODUCT_PRODUCT_VNDK_VERSION. If the value
-# is "current", use PLATFORM_VNDK_VERSION.
-ifdef PRODUCT_PRODUCT_VNDK_VERSION
-ifeq ($(KEEP_VNDK),true)
-ifeq ($(PRODUCT_PRODUCT_VNDK_VERSION),current)
-ADDITIONAL_PRODUCT_PROPERTIES += ro.product.vndk.version=$(PLATFORM_VNDK_VERSION)
-else
-ADDITIONAL_PRODUCT_PROPERTIES += ro.product.vndk.version=$(PRODUCT_PRODUCT_VNDK_VERSION)
-endif
-endif
-endif
-
 ADDITIONAL_PRODUCT_PROPERTIES += ro.build.characteristics=$(TARGET_AAPT_CHARACTERISTICS)
 
 ifeq ($(AB_OTA_UPDATER),true)
