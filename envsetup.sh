6d10bbddf92887f3535fef2fc9bd4b701e9088d0
Ronald Braunstein <rbraunstein@google.com>
2023-10-30 12:03:40 -0700

    Add banner announcing adevice when running builds.
    Test: ANDROID_QUIET_BUILD=true mm
    Test: mm
    Change-Id: I513591137d4866bcb824b3021269c29dec32f972

--- envsetup.sh
+++ envsetup.sh
@@ -1878,6 +1878,10 @@ function _trigger_build()
       >&2 echo "Couldn't locate the top of the tree. Try setting TOP."
       return 1
     fi
+
+    if [[ -z "${ANDROID_QUIET_BUILD:-}" && -n "${ANDROID_BUILD_BANNER}" ]]; then
+      echo "$ANDROID_BUILD_BANNER"
+    fi
 )
 
 function m()
