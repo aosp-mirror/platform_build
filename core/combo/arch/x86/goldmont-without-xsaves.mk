88fc7ad3a880482c58113b0ba1097fb4e0a97f01
Satoshi Niwa <niwa@google.com>
2023-12-14 17:47:56 +0900

    Add a new x86(_64) arch variant "goldmont-without-xsaves"
    Its target arch is goldmont, but without xsaves support.
    It can build images that run on old Intel/AMD CPUs as well as new CPUs.
    
    Bug: 314243939
    Test: Build an image with TARGET_ARCH_VARIANT:=goldmont-without-xsaves
    Change-Id: I9c4bfd3928511f5b4a075f9a9337db6e1270a765

---
+++ core/combo/arch/x86/goldmont-without-xsaves.mk
@@ -0,0 +1,7 @@
+# This file contains feature macro definitions specific to the
+# goldmont-without-xsaves arch variant.
+#
+# See build/make/core/combo/arch/x86/x86-atom.mk for differences.
+#
+
+ARCH_X86_HAVE_SSE4_1 := true
