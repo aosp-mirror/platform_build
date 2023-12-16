0f84e4b21cc50e209e313d2c6b4d6ef61248d68b
Steven Moreland <smoreland@google.com>
2023-12-16 02:51:26 +0000

    Split finalization scripts between vFRC and sFRC.
    This splits out the AIDL/VNDK/sepolicy stuff from the SDK
    finalization. However, in CI, we still do the vFRC step
    as part of the first step.
    
    We'll probably want to do some of the following:
    - (maybe) add CI for the first step - a 'fina-0' branch
    - add a way to do SDK finalizatoin without VINTF finalization
      because after VINTF finalization is submitted, people will
      develop in-progress interfaces that aren't done yet
    - a lot of VINTF finalization is still not automated, so things
      like sepolicy and LL-NDK need to be added here.
    
    Ignore-AOSP-First: b/304316873#comment10
    
    Bug: 314010935
    Test: N/A
    Change-Id: I4831a14f5c5c3d757bd1d10a775d8cf4940d9a4a

--- tools/finalization/build-step-1-and-2.sh
+++ tools/finalization/build-step-1-and-2.sh
@@ -8,7 +8,8 @@ function finalize_main_step12() {
 
     if [ "$FINAL_STATE" = "unfinalized" ] ; then
         # SDK codename -> int
-        source $top/build/make/tools/finalization/finalize-aidl-vndk-sdk-resources.sh
+        source $top/build/make/tools/finalization/finalize-vintf-resources.sh
+        source $top/build/make/tools/finalization/finalize-sdk-resources.sh
     fi;
 
     if [ "$FINAL_STATE" = "unfinalized" ] || [ "$FINAL_STATE" = "sdk" ] ; then
