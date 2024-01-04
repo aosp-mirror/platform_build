bae4c57b7b3144aa094417330ff977813aa14e21
Greg Kaiser <gkaiser@google.com>
2024-01-04 15:57:54 -0700

    Change default lunch target
    We keep this as "trunk_staging" and "eng".  However, we switch
    from "aosp_arm" to "aosp_cf_x86_64_phone".  This switches us
    away from 32-bit development, and gives us a project (the
    cuttlefish ("cf") emulator) at the end of the build which is
    runnable with reasonable performance, and able to run all 32-bit
    and 64-bit tests.
    
    Bug: 318749634
    Test: lunch
    
    Change-Id: I920cdd413cbcecd4300bc46c719e5788244a924e

--- envsetup.sh
+++ envsetup.sh
@@ -775,7 +775,7 @@ function lunch()
         answer=$1
     else
         print_lunch_menu
-        echo "Which would you like? [aosp_arm-trunk_staging-eng]"
+        echo "Which would you like? [aosp_cf_x86_64_phone-trunk_staging-eng]"
         echo -n "Pick from common choices above (e.g. 13) or specify your own (e.g. aosp_barbet-trunk_staging-eng): "
         read answer
         used_lunch_menu=1
@@ -785,7 +785,7 @@ function lunch()
 
     if [ -z "$answer" ]
     then
-        selection=aosp_arm-trunk_staging-eng
+        selection=aosp_cf_x86_64_phone-trunk_staging-eng
     elif (echo -n $answer | grep -q -e "^[0-9][0-9]*$")
     then
         local choices=($(TARGET_BUILD_APPS= get_build_var COMMON_LUNCH_CHOICES))
