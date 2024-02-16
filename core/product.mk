8804a8e2bd2a47b318a0041efbc6718934bbf8be
Jihoon Kang <jihoonkang@google.com>
2024-02-16 23:25:06 +0000

    Revert "Create a product variable for skipping apex contribution..."
    Revert submission 2964509-skip-apex-contributions
    
    Reason for revert: 325666427
    
    Reverted changes: /q/submissionid:2964509-skip-apex-contributions
    
    Change-Id: I0172bdf99adce7b0eef42161c79cc7c1fdf977eb

--- core/product.mk
+++ core/product.mk
@@ -454,8 +454,6 @@ _product_list_vars += PRODUCT_VALIDATION_CHECKS
 
 _product_single_value_vars += PRODUCT_BUILD_FROM_SOURCE_STUB
 
-_product_list_vars += PRODUCT_BUILD_IGNORE_APEX_CONTRIBUTION_CONTENTS
-
 .KATI_READONLY := _product_single_value_vars _product_list_vars
 _product_var_list :=$= $(_product_single_value_vars) $(_product_list_vars)
 
