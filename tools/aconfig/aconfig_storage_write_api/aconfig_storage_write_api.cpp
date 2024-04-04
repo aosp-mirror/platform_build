455dc608f3bf590ff568c1ee2b1893b9331a62f4
Dennis Shen <dzshen@google.com>
2024-04-04 20:45:58 +0000

    aconfig: move create flag info file api to aconfig_storage_write_api
    rust_ffi_static right now will export excessive symbols, this lead to a
    duplication of symbols when aconfig links against both
    libaconfig_storage_file_cc and libaconfig_storage_read_api_cc. This is
    because aconfig_storage_read_api crate depends on aconfig_storage_file
    crate. To solve this problem, move create flag info file api to
    aconfig_storage_write_api crate which is parallel to
    aconfig_storage_read_api crate.
    
    Bug: b/321077378
    Test: atest aconfig_storage_file.test; atest
    aconfig_storage_read_api.test; atest aconfig_storage_write_api.test
    
    Change-Id: Ibbb50193a2da82d52ccbb4087c8e3fb9f320805f

--- tools/aconfig/aconfig_storage_write_api/aconfig_storage_write_api.cpp
+++ tools/aconfig/aconfig_storage_write_api/aconfig_storage_write_api.cpp
@@ -126,4 +126,18 @@ Result<void> set_boolean_flag_value(
   return {};
 }
 
+Result<void> create_flag_info(
+    std::string const& package_map,
+    std::string const& flag_map,
+    std::string const& flag_info_out) {
+  auto creation_cxx = create_flag_info_cxx(
+      rust::Str(package_map.c_str()),
+      rust::Str(flag_map.c_str()),
+      rust::Str(flag_info_out.c_str()));
+  if (creation_cxx.success) {
+    return {};
+  } else {
+    return android::base::Error() << creation_cxx.error_message;
+  }
+}
 } // namespace aconfig_storage
