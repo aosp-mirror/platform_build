a2440a3e4a26447a080ca710042faf20a96b8db2
Hsin-Yi Chen <hsinyichen@google.com>
2024-05-15 15:49:34 +0800

    Create APEX ABI dumps with next release configuration
    Next release configuration maps the codename to the finalized API
    level, but fina_2 doesn't.
    
    Ignore-AOSP-First: Finalization scripts are not on AOSP
    Test: build/make/tools/finalization/finalize-sdk-rel.sh
    Bug: 340507697
    Change-Id: I00e55bf350ff5b5f5b10f9e74ceff38b0660dde3

--- tools/finalization/finalize-sdk-rel.sh
+++ tools/finalization/finalize-sdk-rel.sh
@@ -34,8 +34,8 @@ function finalize_sdk_rel() {
     git -C "$top/cts" mv hostsidetests/theme/assets/${FINAL_PLATFORM_CODENAME} hostsidetests/theme/assets/${FINAL_PLATFORM_SDK_VERSION}
 
     # prebuilts/abi-dumps/platform
-    "$top/build/soong/soong_ui.bash" --make-mode TARGET_RELEASE=fina_2 TARGET_PRODUCT=aosp_arm64 TARGET_BUILD_VARIANT=userdebug create_reference_dumps
-    ANDROID_BUILD_TOP="$top" "$top/out/host/linux-x86/bin/create_reference_dumps" -release fina_2 --build-variant userdebug --lib-variant APEX
+    "$top/build/soong/soong_ui.bash" --make-mode TARGET_RELEASE=next TARGET_PRODUCT=aosp_arm64 TARGET_BUILD_VARIANT=userdebug create_reference_dumps
+    ANDROID_BUILD_TOP="$top" "$top/out/host/linux-x86/bin/create_reference_dumps" -release next --build-variant userdebug --lib-variant APEX
 }
 
 finalize_sdk_rel
