d3c6cbfe7cec0e165e5ad32863b37ae6bbcbfc3d
SzuWei Lin <szuweilin@google.com>
2024-06-20 07:54:31 +0000

    Do not output kernel 4.19 and 5.4
    These kernels are not used by Android 12.
    
    Ignore-AOSP-First: CP to AOSP later.
    Bug: 347440780
    Test: TH
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:c31c3a571f53431b4cdf8112f4d30d7dc1ed8388)
    Merged-In: I1d6fb183ef762c0ab6d92129651b79df0337d3e7
    Change-Id: I1d6fb183ef762c0ab6d92129651b79df0337d3e7

--- target/board/generic_arm64/device.mk
+++ target/board/generic_arm64/device.mk
@@ -20,25 +20,17 @@ ifdef BUILD_WITH_KERNEL
 
 # Keep prebuilt
 PRODUCT_COPY_FILES += \
-    kernel/prebuilts/4.19/arm64/kernel-4.19-gz:kernel-4.19-gz \
     kernel/prebuilts/mainline/arm64/kernel-mainline-allsyms:kernel-mainline \
     kernel/prebuilts/mainline/arm64/kernel-mainline-gz-allsyms:kernel-mainline-gz \
     kernel/prebuilts/mainline/arm64/kernel-mainline-lz4-allsyms:kernel-mainline-lz4 \
 
 PRODUCT_COPY_FILES += \
-    kernel/prebuilts/5.4/arm64/kernel-5.4:kernel-5.4 \
-    kernel/prebuilts/5.4/arm64/kernel-5.4-gz:kernel-5.4-gz \
-    kernel/prebuilts/5.4/arm64/kernel-5.4-lz4:kernel-5.4-lz4 \
     $(OUT_DIR)/target/kernel/5.10/arm64/kernel-5.10:kernel-5.10 \
     $(OUT_DIR)/target/kernel/5.10/arm64/kernel-5.10-gz:kernel-5.10-gz \
     $(OUT_DIR)/target/kernel/5.10/arm64/kernel-5.10-lz4:kernel-5.10-lz4 \
 
 ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
 PRODUCT_COPY_FILES += \
-    kernel/prebuilts/4.19/arm64/kernel-4.19-gz:kernel-4.19-gz-allsyms \
-    kernel/prebuilts/5.4/arm64/kernel-5.4:kernel-5.4-allsyms \
-    kernel/prebuilts/5.4/arm64/kernel-5.4-gz:kernel-5.4-gz-allsyms \
-    kernel/prebuilts/5.4/arm64/kernel-5.4-lz4:kernel-5.4-lz4-allsyms \
     $(OUT_DIR)/target/kernel/5.10/arm64/kernel-5.10-allsyms:kernel-5.10-allsyms \
     $(OUT_DIR)/target/kernel/5.10/arm64/kernel-5.10-gz-allsyms:kernel-5.10-gz-allsyms \
     $(OUT_DIR)/target/kernel/5.10/arm64/kernel-5.10-lz4-allsyms:kernel-5.10-lz4-allsyms \
@@ -48,30 +40,18 @@ endif
 else # BUILD_WITH_KERNEL
 
 PRODUCT_COPY_FILES += \
-    kernel/prebuilts/4.19/arm64/kernel-4.19-gz:kernel-4.19-gz \
-    kernel/prebuilts/5.4/arm64/kernel-5.4:kernel-5.4 \
-    kernel/prebuilts/5.4/arm64/kernel-5.4-gz:kernel-5.4-gz \
-    kernel/prebuilts/5.4/arm64/kernel-5.4-lz4:kernel-5.4-lz4 \
     kernel/prebuilts/5.10/arm64/kernel-5.10:kernel-5.10 \
     kernel/prebuilts/5.10/arm64/kernel-5.10-gz:kernel-5.10-gz \
     kernel/prebuilts/5.10/arm64/kernel-5.10-lz4:kernel-5.10-lz4 \
 
-$(call _output-kernel-info,kernel/prebuilts/4.19/arm64,kernel/4.19)
-$(call _output-kernel-info,kernel/prebuilts/5.4/arm64,kernel/5.4)
 $(call _output-kernel-info,kernel/prebuilts/5.10/arm64,kernel/5.10)
 
 ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
 PRODUCT_COPY_FILES += \
-    kernel/prebuilts/4.19/arm64/kernel-4.19-gz-allsyms:kernel-4.19-gz-allsyms \
-    kernel/prebuilts/5.4/arm64/kernel-5.4-allsyms:kernel-5.4-allsyms \
-    kernel/prebuilts/5.4/arm64/kernel-5.4-gz-allsyms:kernel-5.4-gz-allsyms \
-    kernel/prebuilts/5.4/arm64/kernel-5.4-lz4-allsyms:kernel-5.4-lz4-allsyms \
     kernel/prebuilts/5.10/arm64/kernel-5.10-allsyms:kernel-5.10-allsyms \
     kernel/prebuilts/5.10/arm64/kernel-5.10-gz-allsyms:kernel-5.10-gz-allsyms \
     kernel/prebuilts/5.10/arm64/kernel-5.10-lz4-allsyms:kernel-5.10-lz4-allsyms \
 
-$(call _output-kernel-info-debug,kernel/prebuilts/4.19/arm64,kernel/4.19-debug)
-$(call _output-kernel-info-debug,kernel/prebuilts/5.4/arm64,kernel/5.4-debug)
 $(call _output-kernel-info-debug,kernel/prebuilts/5.10/arm64,kernel/5.10-debug)
 
 endif
