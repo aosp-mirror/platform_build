bionic-linux/android_build_make

Use -target-feature for MTE
This combines with other features.

Change-Id: I74bab7e725eaa856d183987fa682f2ab0903ce70

core/config_sanitizers.mk
@@ -284,9 +284,9 @@ endif
 ifneq ($(filter memtag_stack,$(my_sanitize)),)
   my_cflags += -fsanitize=memtag-stack
   my_ldflags += -fsanitize=memtag-stack
-  my_cflags += -march=armv8a+memtag
-  my_ldflags += -march=armv8a+memtag
-  my_asflags += -march=armv8a+memtag
+  my_cflags += -Xclang -target-feature -Xclang +memtag
+  my_ldflags += -Xclang -target-feature -Xclang +memtag
+  my_asflags += -Xclang -target-feature -Xclang +memtag
   my_sanitize := $(filter-out memtag_stack,$(my_sanitize))
 endif
 

aosp-mirror/platform_build

Bug: 366222162

core/config_sanitizers.mk
@@ -284,9 +284,9 @@ endif
 ifneq ($(filter memtag_stack,$(my_sanitize)),)
   my_cflags += -fsanitize=memtag-stack
   my_ldflags += -fsanitize=memtag-stack
-  my_cflags += -march=armv8a+memtag
-  my_ldflags += -march=armv8a+memtag
-  my_asflags += -march=armv8a+memtag
+  my_cflags += -Xclang -target-feature -Xclang +mte
+  my_ldflags += -Xclang -target-feature -Xclang +mte
+  my_asflags += -Xclang -target-feature -Xclang +mte
   my_sanitize := $(filter-out memtag_stack,$(my_sanitize))
 endif
 