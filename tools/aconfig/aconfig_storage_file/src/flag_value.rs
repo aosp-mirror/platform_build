13b64308e8e9c20e7c811a19f54bfab00bd72eeb
Marybeth Fair <marybethfair@google.com>
2024-09-23 19:37:21 +0000

    Revert "Add fingerprint to packages.map."
    This reverts commit 769d8eed378d468be6c8d840343772510752e4f1.
    
    Reason for revert: Caused crashed in places where old version files were present.
    (cherry picked from https://android-review.googlesource.com/q/commit:a80e298d34a3cfcaf1f89bc9c808c8b175095c4f)
    Merged-In: I7d529773226cd834979400aa018c47bbf6891b72
    Change-Id: I7d529773226cd834979400aa018c47bbf6891b72

--- tools/aconfig/aconfig_storage_file/src/flag_value.rs
+++ tools/aconfig/aconfig_storage_file/src/flag_value.rs
@@ -159,7 +159,7 @@ mod tests {
         let bytes = &flag_value_list.into_bytes();
         let mut head = 0;
         let version = read_u32_from_bytes(bytes, &mut head).unwrap();
-        assert_eq!(version, 2);
+        assert_eq!(version, 1);
     }
 
     #[test]
