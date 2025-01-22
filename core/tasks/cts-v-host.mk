e27c10dbd81559b2886679e6c91d2faf633fafdf
Ziwei Zhang <zhangxh@google.com>
2025-01-22 17:32:40 +0800

    Rename cts-interactive to cts-v-host.
    The suite will contains both interactive tests and multi-device tests,
    so rename it to cts-v-host to avoid confusion.
    
    Bug: 391490924
    Flag: TEST_ONLY
    Test: m cts-v-host
    Change-Id: I9fdb96660415990e3b297ae9a2c3e1f6b75f6ab7
    Ignore-AOSP-First: target B release

--- core/tasks/cts-v-host.mk
+++ core/tasks/cts-v-host.mk
@@ -12,18 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# cts-interactive includes interactive and multi-device CTS tests that
-# cannot be automated. It is part of CTS Verifier.
-ifneq ($(wildcard cts/tools/cts-interactive/README),)
-test_suite_name := cts-interactive
-test_suite_tradefed := cts-interactive-tradefed
-test_suite_readme := cts/tools/cts-interactive/README
+# cts-v-host includes host-side interactive and multi-device CTS tests that
+# cannot be fully automated. It is part of CTS Verifier.
+ifneq ($(wildcard cts/tools/cts-v-host/README),)
+test_suite_name := cts-v-host
+test_suite_tradefed := cts-v-host-tradefed
+test_suite_readme := cts/tools/cts-v-host/README
 test_suite_tools := $(HOST_OUT_JAVA_LIBRARIES)/ats_console_deploy.jar \
   $(HOST_OUT_JAVA_LIBRARIES)/ats_olc_server_local_mode_deploy.jar
 
 include $(BUILD_SYSTEM)/tasks/tools/compatibility.mk
 
-.PHONY: cts-interactive
-cts-interactive: $(compatibility_zip) $(compatibility_tests_list_zip)
-$(call dist-for-goals, cts-interactive, $(compatibility_zip) $(compatibility_tests_list_zip))
+.PHONY: cts-v-host
+cts-v-host: $(compatibility_zip) $(compatibility_tests_list_zip)
+$(call dist-for-goals, cts-v-host, $(compatibility_zip) $(compatibility_tests_list_zip))
 endif
