8a1a1858ce8f1e20dd2588c4b71b0eaca703435d
Inseob Kim <inseob@google.com>
2025-02-09 20:17:15 -0800

    Add soong module only for aosp_{arch}
    aosp_{arch} build GSIs for release, whereas gsi_{arch} build GSIs for
    compilance test. System images of aosp_{arch} are supersets of system
    images of gsi_{arch}, with a few exceptions (userdebug sepolicy).
    
    This adds a Soong module for aosp_{arch} targets, named
    aosp_system_image, enabled only when building GSI.
    
    This change is a reland of ag/31654647 where the build is broken on
    targets of downstream branches which inherit aosp_{arch} targets.
    
    Bug: 381019944
    Test: build, TH
    Change-Id: I390d59cb4168eee8c422a4c1b257ade465d17921

--- target/product/aosp_arm.mk
+++ target/product/aosp_arm.mk
@@ -60,8 +60,12 @@ ifeq (aosp_arm,$(TARGET_PRODUCT))
 MODULE_BUILD_FROM_SOURCE ?= true
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_release.mk)
-endif
 
+PRODUCT_SOONG_DEFINED_SYSTEM_IMAGE := aosp_system_image
+USE_SOONG_DEFINED_SYSTEM_IMAGE := true
+PRODUCT_USE_SOONG_NOTICE_XML := true
+
+endif
 
 PRODUCT_NAME := aosp_arm
 PRODUCT_DEVICE := generic
