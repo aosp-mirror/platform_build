50a3b126ef75e5ed298d04a3d9e492579950ed1b
Jared Duke <jdduke@google.com>
2025-03-18 20:38:56 +0000

    Add a system_server_trace_refs soong var
    When fully optimizing system_server, we need to make sure that any
    references from downstream system_server jars are kept. As an initial
    step, generate this list from SYSTEM_SERVER_JARS and route it into a
    new soong config var: system_server_trace_refs.
    
    Note that this CL does not integrate usage of the new var. That will be
    done separately after refactoring system server targets to allow
    breaking any resulting dependency cycles.
    
    Bug: 212737576
    Test: m
    Flag: EXEMPT build refactor
    Change-Id: I19a3531f230025f1b10eb8ac4339ba3102d74fd6

--- core/android_soong_config_vars.mk
+++ core/android_soong_config_vars.mk
@@ -176,6 +176,7 @@ ifeq (true,$(PRODUCT_BROKEN_SUBOPTIMAL_ORDER_OF_SYSTEM_SERVER_JARS))
 else ifneq (platform:services,$(lastword $(PRODUCT_SYSTEM_SERVER_JARS)))
   # If services is not the final jar in the dependency ordering, don't assume
   # it can be safely optimized in isolation, as there may be dependent jars.
+  # TODO(b/212737576): Remove this exception after integrating use of `$(system_server_trace_refs)`.
   SYSTEM_OPTIMIZE_JAVA ?= false
 else
   SYSTEM_OPTIMIZE_JAVA ?= true
@@ -187,6 +188,20 @@ endif
 
 $(call add_soong_config_var,ANDROID,SYSTEM_OPTIMIZE_JAVA)
 $(call add_soong_config_var,ANDROID,FULL_SYSTEM_OPTIMIZE_JAVA)
+
+ifeq (true, $(SYSTEM_OPTIMIZE_JAVA))
+  # Create a list of (non-prefixed) system server jars that follow `services` in
+  # the classpath. This can be used when optimizing `services` to trace any
+  # downstream references that need keeping.
+  # Example: "foo:service1 platform:services bar:services2" -> "services2"
+  system_server_jars_dependent_on_services := $(shell \
+      echo "$(PRODUCT_SYSTEM_SERVER_JARS)" | \
+      awk '{found=0; for(i=1;i<=NF;i++){if($$i=="platform:services"){found=1; continue} if(found){split($$i,a,":"); print a[2]}}}' | \
+      xargs)
+  ifneq ($(strip $(system_server_jars_dependent_on_services)),)
+    $(call soong_config_set_string_list,ANDROID,system_server_trace_refs,$(system_server_jars_dependent_on_services))
+  endif
+endif
 
 # TODO(b/319697968): Remove this build flag support when metalava fully supports flagged api
 $(call soong_config_set,ANDROID,release_hidden_api_exportable_stubs,$(RELEASE_HIDDEN_API_EXPORTABLE_STUBS))
